f0b6df84fe0d75998f4ed086797e5eadef5e0831
Taras <tarassmakula@gmail.com>
2022-02-16 19:36:36 +0200

    Integrate log in function

--- app/src/main/java/com/futo/circles/ui/log_in/LogInViewModel.kt
+++ app/src/main/java/com/futo/circles/ui/log_in/LogInViewModel.kt
@@ -1,10 +1,15 @@
 package com.futo.circles.ui.log_in
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.futo.circles.ui.log_in.data_source.LoginDataSource
+import kotlinx.coroutines.launch
 
-class LogInViewModel : ViewModel() {
+class LogInViewModel(
+    private val loginDataSource: LoginDataSource
+) : ViewModel() {
 
     fun logIn(name: String, password: String, secondPassword: String?) {
-
+        viewModelScope.launch { loginDataSource.logIn(name, password, secondPassword) }
     }
 }
--- app/src/main/java/com/futo/circles/ui/log_in/data_source/LoginDataSource.kt
+++ app/src/main/java/com/futo/circles/ui/log_in/data_source/LoginDataSource.kt
@@ -1,4 +1,25 @@
 package com.futo.circles.ui.log_in.data_source
 
-class LoginDataSource {
+import com.futo.circles.provider.MatrixHomeServerProvider
+import com.futo.circles.provider.MatrixProvider
+import com.futo.circles.provider.MatrixSessionProvider
+import java.util.*
+
+class LoginDataSource(private val matrixSessionProvider: MatrixSessionProvider) {
+
+    suspend fun logIn(name: String, password: String, secondPassword: String?) {
+        try {
+            val homeServerConnectionConfig = MatrixHomeServerProvider().createHomeServerConfig()
+
+            MatrixProvider.matrix.authenticationService().directAuthentication(
+                homeServerConnectionConfig = homeServerConnectionConfig,
+                matrixId = name,
+                password = password,
+                deviceId = secondPassword,
+                initialDeviceName = UUID.randomUUID().toString()
+            )
+        } catch (failure: Throwable) {
+            null
+        }?.let { matrixSessionProvider.startSession(it) }
+    }
 }
