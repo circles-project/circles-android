1b58ae98d1277c24efa533db0f806c15a99db34a
Taras <tarassmakula@gmail.com>
2022-02-16 16:49:32 +0200

    Init matrix session

---
+++ app/src/main/java/com/futo/circles/provider/MatrixSessionProvider.kt
@@ -0,0 +1,28 @@
+package com.futo.circles.provider
+
+import android.content.Context
+import org.matrix.android.sdk.api.Matrix
+import org.matrix.android.sdk.api.MatrixConfiguration
+import org.matrix.android.sdk.api.session.Session
+
+class MatrixSessionProvider(private val context: Context) {
+
+    var currentSession: Session? = null
+        private set
+
+    fun initSession() {
+        Matrix.initialize(
+            context = context, matrixConfiguration = MatrixConfiguration(
+                roomDisplayNameFallbackProvider = RoomDisplayNameFallbackProviderImpl()
+            )
+        )
+        val lastSession =
+            Matrix.getInstance(context).authenticationService().getLastAuthenticatedSession()
+
+        lastSession?.let { startSession(it) }
+    }
+
+    private fun startSession(session: Session) {
+        currentSession = session.apply { open(); startSync(true) }
+    }
+}
