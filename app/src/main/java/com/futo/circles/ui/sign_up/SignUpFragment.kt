857a45168adef5003acd196a42c20872957c04f8
Taras <tarassmakula@gmail.com>
2022-02-16 10:46:04 +0200

    Setup base navigation

---
+++ app/src/main/java/com/futo/circles/ui/sign_up/SignUpFragment.kt
@@ -0,0 +1,19 @@
+package com.futo.circles.ui.sign_up
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.futo.circles.R
+
+class SignUpFragment : Fragment(R.layout.sign_up_fragment) {
+
+    companion object {
+        fun newInstance() = SignUpFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
