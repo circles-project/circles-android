c9ad5535c208d3815464e75b9b47875bf2928d5e
Taras <tarassmakula@gmail.com>
2022-02-16 16:51:43 +0200

    Set start destination based on active sesion

--- app/src/main/java/com/futo/circles/MainActivity.kt
+++ app/src/main/java/com/futo/circles/MainActivity.kt
@@ -1,6 +1,33 @@
 package com.futo.circles
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.Navigation.findNavController
+import com.futo.circles.provider.MatrixSessionProvider
+import org.koin.android.ext.android.get
 
 
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setInitialFragment()
+    }
+
+    private fun setInitialFragment() {
+        val navController: NavController = findNavController(this, R.id.nav_host_fragment)
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph_start_host)
+        setStartDestination(navGraph)
+        navController.setGraph(navGraph, null)
+    }
+
+    private fun setStartDestination(navGraph: NavGraph) {
+        val startDestinationId = get<MatrixSessionProvider>().currentSession?.let {
+            R.id.bottomNavigationFragment
+        } ?: R.id.logInFragment
+
+        navGraph.setStartDestination(startDestinationId)
+    }
+}
