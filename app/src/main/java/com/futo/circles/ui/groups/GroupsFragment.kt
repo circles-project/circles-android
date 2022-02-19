a539da6e4403afdb46c07bc6717a71eecde66a01
Taras <tarassmakula@gmail.com>
2022-02-19 02:09:41 +0200

    Setup grup list

--- app/src/main/java/com/futo/circles/ui/groups/GroupsFragment.kt
+++ app/src/main/java/com/futo/circles/ui/groups/GroupsFragment.kt
@@ -7,6 +7,7 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.futo.circles.R
 import com.futo.circles.databinding.GroupsFragmentBinding
 import com.futo.circles.extensions.observeData
+import com.futo.circles.ui.groups.list.GroupsListAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.matrix.android.sdk.api.session.group.model.GroupSummary
 
@@ -14,14 +15,21 @@ class GroupsFragment : Fragment(R.layout.groups_fragment) {
 
     private val viewModel by viewModel<GroupsViewModel>()
     private val binding by viewBinding(GroupsFragmentBinding::bind)
+    private val listAdapter by lazy { GroupsListAdapter(::onGroupListItemClicked) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.rvGroups.adapter = listAdapter
 
         viewModel.groupsLiveData?.observeData(this, ::setGroupsList)
     }
 
     private fun setGroupsList(list: List<GroupSummary>) {
+        listAdapter.submitList(list)
+    }
+
+    private fun onGroupListItemClicked(group: GroupSummary) {
 
     }
 }
