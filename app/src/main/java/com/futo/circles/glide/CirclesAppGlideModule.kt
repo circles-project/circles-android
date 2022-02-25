0db132f7e50d2dbd2c88fb9c03ef03dab194cabc
Taras <tarassmakula@gmail.com>
2022-02-26 01:10:04 +0200

    Create app glide module

---
+++ app/src/main/java/com/futo/circles/glide/CirclesAppGlideModule.kt
@@ -0,0 +1,27 @@
+package com.futo.circles.glide
+
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.futo.circles.model.ImageContent
+import java.io.InputStream
+
+@GlideModule
+class CirclesAppGlideModule : AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setLogLevel(Log.ERROR)
+    }
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry.append(
+            ImageContent::class.java,
+            InputStream::class.java,
+            CirclesGlideModelLoaderFactory(context)
+        )
+    }
+}
