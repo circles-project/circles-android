0430eabba425e13592f1b18c40f8b4542ab8441c
Taras <tarassmakula@gmail.com>
2022-02-26 01:11:33 +0200

    Add load encrypted image extension

--- app/src/main/java/com/futo/circles/extensions/ImageViewExtensions.kt
+++ app/src/main/java/com/futo/circles/extensions/ImageViewExtensions.kt
@@ -2,23 +2,42 @@ package com.futo.circles.extensions
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
 import com.futo.circles.R
-import org.matrix.android.sdk.api.session.content.ContentUrlResolver
+import com.futo.circles.glide.GlideApp
+import com.futo.circles.model.ImageContent
+import com.futo.circles.provider.MatrixSessionProvider
 
-const val THUMBNAIL_SIZE = 250
 
-fun ImageView.loadMatrixThumbnail(
-    avatarUrl: String?,
-    resolver: ContentUrlResolver?,
-    size: Int = THUMBNAIL_SIZE
+fun ImageView.loadImage(
+    url: String?,
+    size: Int = Target.SIZE_ORIGINAL
 ) {
-    val resolvedUrl = resolver?.resolveThumbnail(
-        avatarUrl,
-        size, size,
-        ContentUrlResolver.ThumbnailMethod.SCALE
-    )
-    Glide.with(this).load(resolvedUrl).into(this)
+    val resolvedUrl = MatrixSessionProvider.currentSession?.resolveUrl(url, size)
+    Glide.with(this)
+        .load(resolvedUrl)
+        .override(size, size)
+        .fitCenter()
+        .into(this)
 }
+
+
+fun ImageView.loadEncryptedImage(content: ImageContent, size: Int = Target.SIZE_ORIGINAL) {
+    val request = content.elementToDecrypt?.let {
+        GlideApp
+            .with(context)
+            .load(content)
+    } ?: run {
+        val resolvedUrl = MatrixSessionProvider.currentSession?.resolveUrl(content.fileUrl, size)
+        GlideApp
+            .with(context)
+            .load(resolvedUrl)
+    }
+    request.override(size, size)
+        .fitCenter()
+        .into(this)
+}
+
 
 fun ImageView.setIsEncryptedIcon(isEncrypted: Boolean) {
     setImageResource(if (isEncrypted) R.drawable.ic_lock else R.drawable.ic_lock_open)
--- app/src/main/java/com/futo/circles/ui/view/GroupPostHeaderView.kt
+++ app/src/main/java/com/futo/circles/ui/view/GroupPostHeaderView.kt
@@ -5,8 +5,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.futo.circles.databinding.GroupPostHeaderViewBinding
-import com.futo.circles.extensions.loadMatrixThumbnail
-import org.matrix.android.sdk.api.session.content.ContentUrlResolver
+import com.futo.circles.extensions.loadImage
 import org.matrix.android.sdk.api.session.room.sender.SenderInfo
 
 class GroupPostHeaderView(
@@ -17,12 +16,8 @@ class GroupPostHeaderView(
     private val binding =
         GroupPostHeaderViewBinding.inflate(LayoutInflater.from(context), this)
 
-    fun setData(sender: SenderInfo, urlResolver: ContentUrlResolver?) {
-        binding.ivSenderImage.loadMatrixThumbnail(
-            sender.avatarUrl,
-            urlResolver,
-            binding.ivSenderImage.height
-        )
+    fun setData(sender: SenderInfo) {
+        binding.ivSenderImage.loadImage(sender.avatarUrl, binding.ivSenderImage.height)
         binding.tvUserName.text = sender.disambiguatedDisplayName
         binding.tvUserId.text = sender.userId
     }
