5df49d30209238592944b490c95e778c1259bae4
Taras <tarassmakula@gmail.com>
2022-03-04 14:00:57 +0200

    Fetch members with roles data

--- app/src/main/java/com/futo/circles/feature/manage_group_members/data_source/ManageGroupMembersDataSource.kt
+++ app/src/main/java/com/futo/circles/feature/manage_group_members/data_source/ManageGroupMembersDataSource.kt
@@ -2,9 +2,22 @@ package com.futo.circles.feature.manage_group_members.data_source
 
 
 import android.content.Context
+import androidx.lifecycle.asFlow
 import com.futo.circles.R
 import com.futo.circles.extensions.nameOrId
+import com.futo.circles.mapping.toGroupMemberListItem
+import com.futo.circles.model.GroupMemberListItem
 import com.futo.circles.provider.MatrixSessionProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import org.matrix.android.sdk.api.query.QueryStringValue
+import org.matrix.android.sdk.api.session.events.model.EventType
+import org.matrix.android.sdk.api.session.events.model.toModel
+import org.matrix.android.sdk.api.session.room.members.roomMemberQueryParams
+import org.matrix.android.sdk.api.session.room.model.Membership
+import org.matrix.android.sdk.api.session.room.model.PowerLevelsContent
+import org.matrix.android.sdk.api.session.room.model.RoomMemberSummary
+import org.matrix.android.sdk.api.session.room.powerlevels.PowerLevelsHelper
 
 class ManageGroupMembersDataSource(
     private val roomId: String,
@@ -20,4 +33,39 @@ class ManageGroupMembersDataSource(
         room?.roomSummary()?.nameOrId() ?: roomId
     )
 
+    fun getRoomMembersFlow(): Flow<List<GroupMemberListItem>> {
+        return combine(
+            getRoomMembersSummaryFlow(), getRoomMembersRoleFlow()
+        ) { members, powerLevel ->
+            buildList(members, powerLevel)
+        }.flowOn(Dispatchers.IO).distinctUntilChanged()
+    }
+
+    private fun getRoomMembersSummaryFlow(): Flow<List<RoomMemberSummary>> {
+        val roomMemberQueryParams = roomMemberQueryParams {
+            displayName = QueryStringValue.IsNotEmpty
+            memberships = Membership.activeMemberships()
+        }
+        return room?.getRoomMembersLive(roomMemberQueryParams)?.asFlow() ?: flowOf()
+    }
+
+    private fun getRoomMembersRoleFlow(): Flow<PowerLevelsContent> {
+        return room?.getStateEventLive(EventType.STATE_ROOM_POWER_LEVELS)?.asFlow()
+            ?.mapNotNull { it.getOrNull()?.content.toModel<PowerLevelsContent>() } ?: flowOf()
+    }
+
+    private fun buildList(
+        members: List<RoomMemberSummary>,
+        powerLevelsContent: PowerLevelsContent
+    ): List<GroupMemberListItem> {
+        val roleHelper = PowerLevelsHelper(powerLevelsContent)
+
+        return members.map { member ->
+            val role = roleHelper.getUserRole(member.userId)
+            val hasInvite = member.membership == Membership.INVITE
+
+            member.toGroupMemberListItem(role, hasInvite)
+
+        }.sortedByDescending { it.role.value }
+    }
 }
