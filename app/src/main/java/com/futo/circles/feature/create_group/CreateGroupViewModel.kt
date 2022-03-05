ef2d06dabf3900d5ffebefa9025518646753b8ca
Taras <tarassmakula@gmail.com>
2022-03-05 15:48:44 +0200

    Add create group view model

---
+++ app/src/main/java/com/futo/circles/feature/create_group/CreateGroupViewModel.kt
@@ -0,0 +1,11 @@
+package com.futo.circles.feature.create_group
+
+import androidx.lifecycle.ViewModel
+import com.futo.circles.feature.create_group.data_source.CreateGroupDataSource
+
+class CreateGroupViewModel(
+    private val dataSource: CreateGroupDataSource
+) : ViewModel() {
+
+
+}
