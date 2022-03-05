ab3a3aca07b22ccf3425e49c4ccc37b1f64918c1
Taras <tarassmakula@gmail.com>
2022-03-05 15:48:25 +0200

    Add create group data source

---
+++ app/src/main/java/com/futo/circles/feature/create_group/data_source/CreateGroupDataSource.kt
@@ -0,0 +1,8 @@
+package com.futo.circles.feature.create_group.data_source
+
+import com.futo.circles.provider.MatrixSessionProvider
+
+class CreateGroupDataSource() {
+
+    private val session = MatrixSessionProvider.currentSession
+}
