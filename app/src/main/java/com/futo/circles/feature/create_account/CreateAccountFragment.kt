d82c6553df7351e3e1fa9f62824c8568f489cb86
Taras <tarassmakula@gmail.com>
2022-03-10 21:47:39 +0200

    Create empty create account fragment

---
+++ app/src/main/java/com/futo/circles/feature/create_account/CreateAccountFragment.kt
@@ -0,0 +1,7 @@
+package com.futo.circles.feature.create_account
+
+import androidx.fragment.app.Fragment
+import com.futo.circles.R
+
+class CreateAccountFragment : Fragment(R.layout.create_account_fragment) {
+}
