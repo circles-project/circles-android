d6aee04eeda0b9758d4604ca4b793c2777058e8a
Taras <tarassmakula@gmail.com>
2022-03-10 15:47:24 +0200

    Create sign up fragment

--- app/src/main/java/com/futo/circles/feature/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/futo/circles/feature/sign_up/SignUpFragment.kt
@@ -3,17 +3,19 @@ package com.futo.circles.feature.sign_up
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.futo.circles.R
+import com.futo.circles.databinding.SignUpFragmentBinding
+import com.futo.circles.feature.log_in.LogInViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignUpFragment : Fragment(R.layout.sign_up_fragment) {
 
-    companion object {
-        fun newInstance() = SignUpFragment()
-    }
+    private val viewModel by viewModel<SignUpViewModel>()
+    private val binding by viewBinding(SignUpFragmentBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
 }
