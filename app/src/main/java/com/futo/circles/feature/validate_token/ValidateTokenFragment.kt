977b5136165bd1c09fbcdca5c7fc30f531de7638
Taras <tarassmakula@gmail.com>
2022-03-10 21:46:55 +0200

    Create empty validate token fragment

---
+++ app/src/main/java/com/futo/circles/feature/validate_token/ValidateTokenFragment.kt
@@ -0,0 +1,7 @@
+package com.futo.circles.feature.validate_token
+
+import androidx.fragment.app.Fragment
+import com.futo.circles.R
+
+class ValidateTokenFragment : Fragment(R.layout.validate_token_fragment) {
+}
