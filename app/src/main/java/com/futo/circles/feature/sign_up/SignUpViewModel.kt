755d99e4bc6dd943c238ed1e210cfd402d3ea9a4
Taras <tarassmakula@gmail.com>
2022-03-10 15:47:36 +0200

    Create sign up view model

---
+++ app/src/main/java/com/futo/circles/feature/sign_up/SignUpViewModel.kt
@@ -0,0 +1,11 @@
+package com.futo.circles.feature.sign_up
+
+import androidx.lifecycle.ViewModel
+import com.futo.circles.feature.sign_up.data_source.SignUpDataSource
+
+class SignUpViewModel(
+    datasource: SignUpDataSource
+) : ViewModel() {
+
+
+}
