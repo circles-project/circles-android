6f7a9583ffe4911e0d8df253c6985e9acee39832
Taras <tarassmakula@gmail.com>
2022-03-10 21:47:13 +0200

    Create empty accept terms fragment

---
+++ app/src/main/java/com/futo/circles/feature/terms/AcceptTermsFragment.kt
@@ -0,0 +1,7 @@
+package com.futo.circles.feature.terms
+
+import androidx.fragment.app.Fragment
+import com.futo.circles.R
+
+class AcceptTermsFragment : Fragment(R.layout.accept_terms_fragment) {
+}
