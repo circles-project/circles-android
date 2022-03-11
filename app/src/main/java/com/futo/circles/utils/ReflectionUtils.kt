09ad2437d55a5ae6cb7b9cc6a3c6dda6440baaf8
Taras <tarassmakula@gmail.com>
2022-03-11 17:14:49 +0200

    Add reflections for private fields

---
+++ app/src/main/java/com/futo/circles/utils/ReflectionUtils.kt
@@ -0,0 +1,8 @@
+package com.futo.circles.utils
+
+fun <T : Any> T.getPrivateProperty(variableName: String): Any? {
+    return javaClass.getDeclaredField(variableName).let { field ->
+        field.isAccessible = true
+        return@let field.get(this)
+    }
+}
