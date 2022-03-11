5beaec9361415e1aded3feaeeb1f31942a053f63
Taras <tarassmakula@gmail.com>
2022-03-11 18:05:56 +0200

    Fix retrofit di definition

--- app/src/main/java/com/futo/circles/di/RetrofitModule.kt
+++ app/src/main/java/com/futo/circles/di/RetrofitModule.kt
@@ -1,17 +1,14 @@
 package com.futo.circles.di
 
 import com.futo.circles.BuildConfig
-import com.futo.circles.feature.log_in.data_source.LoginDataSource
 import com.futo.circles.io.service.CirclesSignUpService
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val retrofitModule = module {
-    single{
+    single {
         Retrofit.Builder()
             .baseUrl(BuildConfig.MATRIX_HOME_SERVER_URL)
-            .addConverterFactory(get())
             .build()
     }
 
