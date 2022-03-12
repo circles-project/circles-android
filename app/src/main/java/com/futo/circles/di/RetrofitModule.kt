1a32601951b29b017418900998323c3a1faa6512
Taras <tarassmakula@gmail.com>
2022-03-12 18:45:19 +0200

    Add retrofit converter factory

--- app/src/main/java/com/futo/circles/di/RetrofitModule.kt
+++ app/src/main/java/com/futo/circles/di/RetrofitModule.kt
@@ -4,11 +4,13 @@ import com.futo.circles.BuildConfig
 import com.futo.circles.io.service.CirclesSignUpService
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val retrofitModule = module {
     single {
         Retrofit.Builder()
             .baseUrl(BuildConfig.MATRIX_HOME_SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
