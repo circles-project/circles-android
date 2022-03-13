4e16262d64222ffe4738747ba49d5343d6fe2e34
Taras <tarassmakula@gmail.com>
2022-03-13 16:08:38 +0200

    Add terms screen logic

--- app/src/main/java/com/futo/circles/feature/terms/AcceptTermsFragment.kt
+++ app/src/main/java/com/futo/circles/feature/terms/AcceptTermsFragment.kt
@@ -1,7 +1,58 @@
 package com.futo.circles.feature.terms
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.futo.circles.R
+import com.futo.circles.core.HasLoadingState
+import com.futo.circles.databinding.AcceptTermsFragmentBinding
+import com.futo.circles.extensions.observeData
+import com.futo.circles.extensions.observeResponse
+import com.futo.circles.extensions.openUrl
+import com.futo.circles.feature.terms.list.TermsListAdapter
+import com.futo.circles.model.TermsListItem
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class AcceptTermsFragment : Fragment(R.layout.accept_terms_fragment) {
+class AcceptTermsFragment : Fragment(R.layout.accept_terms_fragment), HasLoadingState {
+
+    override val fragment: Fragment = this
+    private val binding by viewBinding(AcceptTermsFragmentBinding::bind)
+    private val viewModel by viewModel<AcceptTermsViewModel>()
+    private val listAdapter by lazy {
+        TermsListAdapter(
+            onCheckChanged = { item -> viewModel.changeTermCheck(item) },
+            onViewTerms = { item -> openTermsUrl(item) },
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObservers()
+    }
+
+    private fun setupViews() {
+        with(binding) {
+            btnAccept.setOnClickListener {
+                startLoading(btnAccept)
+                viewModel.acceptTerms()
+            }
+
+            rvTerms.adapter = listAdapter
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.acceptTermsLiveData.observeResponse(this)
+
+        viewModel.termsListLiveData.observeData(this) {
+            listAdapter.submitList(it)
+            binding.btnAccept.setButtonEnabled(viewModel.isAllTermsAccepted(it))
+        }
+    }
+
+    private fun openTermsUrl(item: TermsListItem) {
+        openUrl(item.url)
+    }
 }
--- app/src/main/java/com/futo/circles/feature/terms/data_source/AcceptTermsDataSource.kt
+++ app/src/main/java/com/futo/circles/feature/terms/data_source/AcceptTermsDataSource.kt
@@ -1,15 +1,21 @@
 package com.futo.circles.feature.terms.data_source
 
 
+import androidx.lifecycle.MutableLiveData
 import com.futo.circles.extensions.Response
 import com.futo.circles.extensions.createResult
+import com.futo.circles.extensions.toTermsListItems
 import com.futo.circles.feature.sign_up.data_source.SignUpDataSource
+import com.futo.circles.model.TermsListItem
 import com.futo.circles.provider.MatrixInstanceProvider
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
+import org.matrix.android.sdk.api.auth.registration.Stage
 
 class AcceptTermsDataSource(
     private val signUpDataSource: SignUpDataSource
 ) {
+
+    val termsListLiveData = MutableLiveData<List<TermsListItem>>(getTermsList())
 
     private val wizard by lazy {
         MatrixInstanceProvider.matrix.authenticationService().getRegistrationWizard()
@@ -22,4 +28,13 @@ class AcceptTermsDataSource(
 
         return result
     }
+
+    fun changeTermCheck(item: TermsListItem) {
+        termsListLiveData.value = termsListLiveData.value
+            ?.map { if (it.id == item.id) it.copy(isChecked = !it.isChecked) else it }
+    }
+
+    private fun getTermsList() =
+        (signUpDataSource.currentStage as? Stage.Terms)?.policies?.toTermsListItems()
+
 }
