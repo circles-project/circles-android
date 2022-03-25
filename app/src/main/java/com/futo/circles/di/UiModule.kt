76ff66804bcb48becc2113cacba0dd16fe473cd2
Taras <tarassmakula@gmail.com>
2022-03-25 17:28:18 +0200

    Setup di for new components

--- app/src/main/java/com/futo/circles/di/UiModule.kt
+++ app/src/main/java/com/futo/circles/di/UiModule.kt
@@ -7,6 +7,7 @@ import com.futo.circles.feature.groups.GroupsViewModel
 import com.futo.circles.feature.log_in.LogInViewModel
 import com.futo.circles.feature.manage_group_members.ManageGroupMembersViewModel
 import com.futo.circles.feature.select_users.SelectUsersViewModel
+import com.futo.circles.feature.setup_profile.SetupProfileViewModel
 import com.futo.circles.feature.sign_up.SignUpViewModel
 import com.futo.circles.feature.sign_up_type.SelectSignUpTypeViewModel
 import com.futo.circles.feature.terms.AcceptTermsViewModel
@@ -29,4 +30,5 @@ val uiModule = module {
     viewModel { SelectSignUpTypeViewModel(get()) }
     viewModel { AcceptTermsViewModel(get()) }
     viewModel { ValidateEmailViewModel(get()) }
+    viewModel { SetupProfileViewModel(get()) }
 }
