e99a4146ef58c760428653d4411e4f2a4eec16bf
Taras <tarassmakula@gmail.com>
2022-03-31 13:04:31 +0300

    Change group filter to include type

--- app/src/main/java/com/futo/circles/extensions/MatrixRoomExtensions.kt
+++ app/src/main/java/com/futo/circles/extensions/MatrixRoomExtensions.kt
@@ -1,12 +1,5 @@
 package com.futo.circles.extensions
 
-import com.futo.circles.core.GROUP_TYPE
-import com.futo.circles.mapping.toGroupListItem
 import org.matrix.android.sdk.api.session.room.model.RoomSummary
-
-fun List<RoomSummary>.toGroupsList() = mapNotNull { room ->
-    val isGroup = room.roomType == GROUP_TYPE
-    if (isGroup) room.toGroupListItem() else null
-}
 
 fun RoomSummary.nameOrId() = displayName.takeIf { it.isNotEmpty() } ?: roomId
--- app/src/main/java/com/futo/circles/feature/groups/GroupsViewModel.kt
+++ app/src/main/java/com/futo/circles/feature/groups/GroupsViewModel.kt
@@ -2,7 +2,8 @@ package com.futo.circles.feature.groups
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import com.futo.circles.extensions.toGroupsList
+import com.futo.circles.core.GROUP_TYPE
+import com.futo.circles.mapping.toGroupListItem
 import com.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
@@ -10,7 +11,8 @@ import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 class GroupsViewModel : ViewModel() {
 
     val groupsLiveData =
-        MatrixSessionProvider.currentSession?.getRoomSummariesLive(roomSummaryQueryParams())
-            ?.map { list -> list.toGroupsList() }
+        MatrixSessionProvider.currentSession?.getRoomSummariesLive(roomSummaryQueryParams {
+            includeType = listOf(GROUP_TYPE)
+        })?.map { list -> list.map { it.toGroupListItem() } }
 
 }
