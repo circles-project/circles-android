b1479bedaab6842fd8b24911a2a9055f2c3518f0
Taras <tarassmakula@gmail.com>
2022-04-04 16:41:05 +0300

    Create constants for new types

--- app/src/main/java/com/futo/circles/core/Constants.kt
+++ app/src/main/java/com/futo/circles/core/Constants.kt
@@ -1,8 +1,17 @@
 package com.futo.circles.core
 
-const val SPACE_TYPE = "m.space"
+const val ROOT_SPACE_TYPE = "m.space.root"
+const val CIRCLES_SPACE_TYPE = "m.space.circles"
+const val GROUPS_SPACE_TYPE = "m.space.groups"
+const val PHOTOS_SPACE_TYPE = "m.space.photos"
+const val CIRCLE_TYPE = "m.social.circle"
 const val GROUP_TYPE = "m.social.group"
 const val TIMELINE_SPACE_TYPE = "m.social.timeline"
+
+const val ROOT_SPACE_NAME = "Circles"
+const val CIRCLES_SPACE_NAME = "My Circles"
+const val GROUPS_SPACE_NAME = "My Groups"
+const val PHOTOS_SPACE_NAME = "My Photo Galleries"
 
 const val REGISTRATION_TOKEN_KEY = "org.matrix.msc3231.login.registration_token"
 
