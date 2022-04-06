571462c495319641010d0507866b9408560da69a
Taras <tarassmakula@gmail.com>
2022-04-06 13:15:13 +0300

    Create home data source

---
+++ app/src/main/java/com/futo/circles/feature/home/data_source/HomeDataSource.kt
@@ -0,0 +1,12 @@
+package com.futo.circles.feature.home.data_source
+
+import com.futo.circles.extensions.createResult
+import com.futo.circles.provider.MatrixSessionProvider
+
+class HomeDataSource {
+
+    private val session = MatrixSessionProvider.currentSession
+
+    suspend fun logOut() = createResult { session?.signOut(true) }
+
+}
