359965bd49fabfb4f69e3590d1e3a2acebf69c3c
Taras <tarassmakula@gmail.com>
2022-04-06 13:14:55 +0300

    Create home fragment

--- app/src/main/java/com/futo/circles/feature/home/HomeFragment.kt
+++ app/src/main/java/com/futo/circles/feature/home/HomeFragment.kt
@@ -1,6 +1,68 @@
 package com.futo.circles.feature.home
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.futo.circles.R
+import com.futo.circles.databinding.HomeFragmentBinding
+import com.futo.circles.extensions.findParentNavController
+import com.futo.circles.extensions.observeResponse
+import com.futo.circles.extensions.showDialog
+import com.futo.circles.feature.bottom_navigation.BottomNavigationFragmentDirections
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class HomeFragment:Fragment(R.layout.home_fragment)
+class HomeFragment : Fragment(R.layout.home_fragment) {
+
+    private val binding by viewBinding(HomeFragmentBinding::bind)
+    private val viewModel by viewModel<HomeViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.logOutLiveData.observeResponse(this,
+            success = { navigateToLogin() }
+        )
+    }
+
+    private fun navigateToLogin() {
+        findParentNavController()?.navigate(BottomNavigationFragmentDirections.toLogInFragment())
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        (menu as? MenuBuilder)?.setOptionalIconsVisible(true)
+        inflater.inflate(R.menu.home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logOut -> {
+                showLogoutDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showLogoutDialog() {
+        showDialog(
+            titleResIdRes = R.string.log_out,
+            messageResId = R.string.log_out_message,
+            positiveButtonRes = R.string.log_out,
+            negativeButtonVisible = true,
+            positiveAction = { viewModel.logOut() })
+    }
+
+}
