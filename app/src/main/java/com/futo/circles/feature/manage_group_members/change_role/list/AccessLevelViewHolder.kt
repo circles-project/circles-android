8cbec56031dd487c94e8b3baeeba0f1511771812
Taras <tarassmakula@gmail.com>
2022-04-17 15:23:21 +0300

    Create view holder

---
+++ app/src/main/java/com/futo/circles/feature/manage_group_members/change_role/list/AccessLevelViewHolder.kt
@@ -0,0 +1,27 @@
+package com.futo.circles.feature.manage_group_members.change_role.list
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.futo.circles.core.list.ViewBindingHolder
+import com.futo.circles.databinding.AccessLevelListItemBinding
+import com.futo.circles.extensions.onClick
+import com.futo.circles.model.AccessLevelListItem
+import com.futo.circles.model.GroupListItem
+
+class AccessLevelViewHolder(
+    parent: ViewGroup,
+    onRoleClicked: (Int) -> Unit
+) : RecyclerView.ViewHolder(inflate(parent, AccessLevelListItemBinding::inflate)) {
+
+    private companion object : ViewBindingHolder
+
+    private val binding = baseBinding as AccessLevelListItemBinding
+
+    init {
+        onClick(itemView) { position -> onRoleClicked(position) }
+    }
+
+    fun bind(data: AccessLevelListItem) {
+
+    }
+}
