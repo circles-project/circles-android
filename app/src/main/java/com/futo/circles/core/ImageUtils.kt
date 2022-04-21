5c3ba6b6fe962ba7055d43f2808ef9f2752e9187
Taras <tarassmakula@gmail.com>
2022-04-21 16:59:48 +0300

    Create image utils

---
+++ app/src/main/java/com/futo/circles/core/ImageUtils.kt
@@ -0,0 +1,39 @@
+package com.futo.circles.core
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import androidx.exifinterface.media.ExifInterface
+
+object ImageUtils {
+
+    fun getBitmap(context: Context, uri: Uri): Bitmap? {
+        return try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
+            } else {
+                context.contentResolver.openInputStream(uri)?.use { inputStream ->
+                    BitmapFactory.decodeStream(inputStream)
+                }
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun getOrientation(context: Context, uri: Uri): Int {
+        var orientation = 0
+        context.contentResolver.openInputStream(uri)?.use { inputStream ->
+            try {
+                ExifInterface(inputStream).let {
+                    orientation = it.rotationDegrees
+                }
+            } catch (ignore: Exception) {
+            }
+        }
+        return orientation
+    }
+}
