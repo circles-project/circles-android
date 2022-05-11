cb1663625ea021a42cffab045bf8b7c4c53efa19
Taras <tarassmakula@gmail.com>
2022-05-11 18:36:48 +0300

    Setup di

--- app/src/main/java/com/futo/circles/di/UiModule.kt
+++ app/src/main/java/com/futo/circles/di/UiModule.kt
@@ -1,27 +1,27 @@
 package com.futo.circles.di
 
 import com.futo.circles.feature.circles.CirclesViewModel
-import com.futo.circles.feature.room.create_room.CreateRoomViewModel
-import com.futo.circles.feature.timeline.post.emoji.EmojiViewModel
 import com.futo.circles.feature.circles.following.FollowingViewModel
-import com.futo.circles.feature.room.invite.InviteMembersViewModel
-import com.futo.circles.feature.room.manage_members.ManageMembersViewModel
-import com.futo.circles.feature.room.manage_members.change_role.ChangeAccessLevelViewModel
 import com.futo.circles.feature.groups.GroupsViewModel
 import com.futo.circles.feature.home.HomeViewModel
 import com.futo.circles.feature.log_in.LogInViewModel
-import com.futo.circles.feature.timeline.post.CreatePostViewModel
-import com.futo.circles.feature.timeline.post.report.ReportViewModel
+import com.futo.circles.feature.room.create_room.CreateRoomViewModel
+import com.futo.circles.feature.room.invite.InviteMembersViewModel
+import com.futo.circles.feature.room.manage_members.ManageMembersViewModel
+import com.futo.circles.feature.room.manage_members.change_role.ChangeAccessLevelViewModel
 import com.futo.circles.feature.room.select_users.SelectUsersViewModel
+import com.futo.circles.feature.room.update_room.UpdateRoomViewModel
+import com.futo.circles.feature.sign_up.SignUpViewModel
 import com.futo.circles.feature.sign_up.setup_circles.SetupCirclesViewModel
 import com.futo.circles.feature.sign_up.setup_profile.SetupProfileViewModel
-import com.futo.circles.feature.sign_up.SignUpViewModel
 import com.futo.circles.feature.sign_up.sign_up_type.SelectSignUpTypeViewModel
 import com.futo.circles.feature.sign_up.terms.AcceptTermsViewModel
-import com.futo.circles.feature.timeline.TimelineViewModel
-import com.futo.circles.feature.room.update_room.UpdateRoomViewModel
 import com.futo.circles.feature.sign_up.validate_email.ValidateEmailViewModel
 import com.futo.circles.feature.sign_up.validate_token.ValidateTokenViewModel
+import com.futo.circles.feature.timeline.TimelineViewModel
+import com.futo.circles.feature.timeline.post.CreatePostViewModel
+import com.futo.circles.feature.timeline.post.emoji.EmojiViewModel
+import com.futo.circles.feature.timeline.post.report.ReportViewModel
 import com.futo.circles.model.CircleRoomTypeArg
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.parameter.parametersOf
@@ -29,7 +29,7 @@ import org.koin.dsl.module
 
 val uiModule = module {
     viewModel { LogInViewModel(get()) }
-    viewModel { GroupsViewModel() }
+    viewModel { GroupsViewModel(get()) }
     viewModel { (roomId: String, type: CircleRoomTypeArg) ->
         TimelineViewModel(get { parametersOf(roomId, type) })
     }
