55f493c039cd3cecdec508712bf0bd80568b8656
Taras <tarassmakula@gmail.com>
2022-05-12 17:19:57 +0300

    Setup view model

--- app/src/main/java/com/futo/circles/feature/circles/CirclesViewModel.kt
+++ app/src/main/java/com/futo/circles/feature/circles/CirclesViewModel.kt
@@ -2,24 +2,24 @@ package com.futo.circles.feature.circles
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import com.futo.circles.mapping.toCircleListItem
-import com.futo.circles.model.CIRCLE_TAG
-import com.futo.circles.model.CircleListItem
+import com.futo.circles.core.SingleEventLiveData
+import com.futo.circles.extensions.Response
+import com.futo.circles.extensions.launchBg
+import com.futo.circles.feature.circles.data_source.CirclesDataSource
 import com.futo.circles.provider.MatrixSessionProvider
-import org.matrix.android.sdk.api.session.room.model.RoomSummary
 import org.matrix.android.sdk.api.session.room.spaceSummaryQueryParams
 
-class CirclesViewModel : ViewModel() {
+class CirclesViewModel(
+    private val dataSource: CirclesDataSource
+) : ViewModel() {
 
     val circlesLiveData =
         MatrixSessionProvider.currentSession?.getRoomSummariesLive(spaceSummaryQueryParams())
-            ?.map { list -> filterCircles(list) }
+            ?.map { list -> dataSource.filterCircles(list) }
 
+    val inviteResultLiveData = SingleEventLiveData<Response<Unit?>>()
 
-    private fun filterCircles(list: List<RoomSummary>): List<CircleListItem> {
-        return list.mapNotNull { summary ->
-            if (summary.hasTag(CIRCLE_TAG) && summary.membership.isActive())
-                summary.toCircleListItem() else null
-        }
+    fun rejectInvite(roomId: String) {
+        launchBg { inviteResultLiveData.postValue(dataSource.rejectInvite(roomId)) }
     }
 }
