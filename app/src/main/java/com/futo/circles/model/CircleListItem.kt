a531216773d7a6734d755bb88739f918afca9a60
Taras <tarassmakula@gmail.com>
2022-05-12 17:18:27 +0300

    Create model

--- app/src/main/java/com/futo/circles/model/CircleListItem.kt
+++ app/src/main/java/com/futo/circles/model/CircleListItem.kt
@@ -1,11 +1,28 @@
 package com.futo.circles.model
 
 import com.futo.circles.core.list.IdEntity
+import org.matrix.android.sdk.api.session.room.model.Membership
 
-data class CircleListItem(
+sealed class CircleListItem(
     override val id: String,
-    val name: String,
-    val followingCount: Int,
-    val followedByCount: Int,
-    val avatarUrl: String
+    open val info: CircleInfo,
+    open val membership: Membership
 ) : IdEntity<String>
+
+data class JoinedCircleListItem(
+    override val id: String,
+    override val info: CircleInfo,
+    val followingCount: Int,
+    val followedByCount: Int
+) : CircleListItem(id, info, Membership.JOIN)
+
+data class InvitedCircleListItem(
+    override val id: String,
+    override val info: CircleInfo,
+    val inviterName: String,
+) : CircleListItem(id, info, Membership.INVITE)
+
+data class CircleInfo(
+    val title: String,
+    val avatarUrl: String
+)
