6a1ba4eb70e4eb9728e9470ef093fc3efc4a461d
Taras <tarassmakula@gmail.com>
2022-05-13 17:58:38 +0300

    Create abstract view model

--- app/src/main/java/com/futo/circles/feature/circles/CirclesViewModel.kt
+++ app/src/main/java/com/futo/circles/feature/circles/CirclesViewModel.kt
@@ -1,26 +1,18 @@
 package com.futo.circles.feature.circles
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import com.futo.circles.core.SingleEventLiveData
-import com.futo.circles.extensions.Response
-import com.futo.circles.extensions.launchBg
+import com.futo.circles.core.rooms.RoomsViewModel
 import com.futo.circles.feature.circles.data_source.CirclesDataSource
 import com.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
 class CirclesViewModel(
     private val dataSource: CirclesDataSource
-) : ViewModel() {
+) : RoomsViewModel(dataSource) {
 
-    val circlesLiveData =
+    override val roomsLiveData =
         MatrixSessionProvider.currentSession?.getRoomSummariesLive(roomSummaryQueryParams {
             excludeType = null
-        })?.map { list -> dataSource.filterCircles(list) }
+        })?.map { list -> dataSource.filterRooms(list) }
 
-    val inviteResultLiveData = SingleEventLiveData<Response<Unit?>>()
-
-    fun rejectInvite(roomId: String) {
-        launchBg { inviteResultLiveData.postValue(dataSource.rejectInvite(roomId)) }
-    }
 }
--- app/src/main/java/com/futo/circles/feature/groups/GroupsViewModel.kt
+++ app/src/main/java/com/futo/circles/feature/groups/GroupsViewModel.kt
@@ -1,30 +1,21 @@
 package com.futo.circles.feature.groups
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import com.futo.circles.core.SingleEventLiveData
-import com.futo.circles.extensions.Response
+import com.futo.circles.core.rooms.RoomsViewModel
 import com.futo.circles.extensions.launchBg
 import com.futo.circles.feature.groups.data_source.GroupsDataSource
 import com.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
-
 class GroupsViewModel(
     private val dataSource: GroupsDataSource
-) : ViewModel() {
+) : RoomsViewModel(dataSource) {
 
-    val groupsLiveData =
+    override val roomsLiveData =
         MatrixSessionProvider.currentSession?.getRoomSummariesLive(roomSummaryQueryParams())
-            ?.map { list -> dataSource.filterGroups(list) }
+            ?.map { list -> dataSource.filterRooms(list) }
 
-    val inviteResultLiveData = SingleEventLiveData<Response<Unit?>>()
-
-    fun acceptInvite(roomId: String) {
+    fun acceptGroupInvite(roomId: String) {
         launchBg { inviteResultLiveData.postValue(dataSource.acceptInvite(roomId)) }
-    }
-
-    fun rejectInvite(roomId: String) {
-        launchBg { inviteResultLiveData.postValue(dataSource.rejectInvite(roomId)) }
     }
 }
