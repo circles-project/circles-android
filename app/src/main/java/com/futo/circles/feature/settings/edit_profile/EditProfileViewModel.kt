e19b89ca29f68002ca9689686c23782bf45af38a
Taras <tarassmakula@gmail.com>
2022-05-20 14:55:33 +0300

    Create viewmodel

---
+++ app/src/main/java/com/futo/circles/feature/settings/edit_profile/EditProfileViewModel.kt
@@ -0,0 +1,39 @@
+package com.futo.circles.feature.settings.edit_profile
+
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.futo.circles.core.SingleEventLiveData
+import com.futo.circles.extensions.Response
+import com.futo.circles.extensions.launchBg
+import com.futo.circles.feature.sign_up.setup_profile.data_source.SetupProfileDataSource
+
+class EditProfileViewModel(
+    private val dataSource: SetupProfileDataSource
+) : ViewModel() {
+
+    val selectedImageLiveData = MutableLiveData<Uri>()
+    val editProfileResponseLiveData = SingleEventLiveData<Response<Unit?>>()
+    val profileLiveData = dataSource.profileLiveData
+    val threePidLiveData = dataSource.threePidLiveData
+    val isProfileDataChangedLiveData = MutableLiveData(false)
+
+    fun setImageUri(uri: Uri) {
+        selectedImageLiveData.value = uri
+    }
+
+    fun update(name: String) {
+        launchBg {
+            editProfileResponseLiveData.postValue(
+                dataSource.saveProfileData(selectedImageLiveData.value, name)
+            )
+        }
+    }
+
+    fun handleProfileDataUpdate(name: String) {
+        val isDataUpdated = dataSource.isNameChanged(name) ||
+                selectedImageLiveData.value != null
+        isProfileDataChangedLiveData.postValue(isDataUpdated)
+    }
+
+}
