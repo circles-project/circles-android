66f0bc9382c7fa3032ff8bec558393367676b43d
Taras <tarassmakula@gmail.com>
2022-05-23 14:19:38 +0300

    Setup view model

--- app/src/main/java/com/futo/circles/feature/settings/change_password/ChangePasswordViewModel.kt
+++ app/src/main/java/com/futo/circles/feature/settings/change_password/ChangePasswordViewModel.kt
@@ -1,9 +1,21 @@
 package com.futo.circles.feature.settings.change_password
 
 import androidx.lifecycle.ViewModel
+import com.futo.circles.core.SingleEventLiveData
+import com.futo.circles.extensions.Response
+import com.futo.circles.extensions.launchBg
 import com.futo.circles.feature.settings.change_password.data_source.ChangePasswordDataSource
 
 class ChangePasswordViewModel(
     private val dataSource: ChangePasswordDataSource
 ) : ViewModel() {
+
+    val changePasswordResponseLiveData = SingleEventLiveData<Response<Unit?>>()
+
+    fun changePassword(oldPassword: String, newPassword: String) {
+        launchBg {
+            val result = dataSource.changePassword(oldPassword, newPassword)
+            changePasswordResponseLiveData.postValue(result)
+        }
+    }
 }
