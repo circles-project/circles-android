f323cd6301aeb3f9bf89787f7d4d499c238bfc03
Taras <tarassmakula@gmail.com>
2022-05-23 14:19:49 +0300

    Add change password api

--- app/src/main/java/com/futo/circles/feature/settings/change_password/data_source/ChangePasswordDataSource.kt
+++ app/src/main/java/com/futo/circles/feature/settings/change_password/data_source/ChangePasswordDataSource.kt
@@ -1,4 +1,13 @@
 package com.futo.circles.feature.settings.change_password.data_source
 
+import com.futo.circles.extensions.createResult
+import com.futo.circles.provider.MatrixSessionProvider
+
 class ChangePasswordDataSource {
+
+
+    suspend fun changePassword(oldPassword: String, newPassword: String) = createResult {
+        MatrixSessionProvider.currentSession?.accountService()
+            ?.changePassword(oldPassword, newPassword)
+    }
 }
