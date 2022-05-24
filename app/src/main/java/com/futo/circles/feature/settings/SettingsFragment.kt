9e97ee2d6df31dfb69c6c639ea44ba15163995bf
Taras <tarassmakula@gmail.com>
2022-05-24 13:37:30 +0300

    Create fragment

--- app/src/main/java/com/futo/circles/feature/settings/SettingsFragment.kt
+++ app/src/main/java/com/futo/circles/feature/settings/SettingsFragment.kt
@@ -32,7 +32,7 @@ class SettingsFragment : Fragment(R.layout.settings_fragment) {
             tvLogout.setOnClickListener { showLogoutDialog() }
             tvEditProfile.setOnClickListener { navigateToProfile() }
             tvChangePassword.setOnClickListener { navigateToChangePassword() }
-            tvDeactivate.setOnClickListener { viewModel.deactivateAccount() }
+            tvDeactivate.setOnClickListener { navigateToDeactivateAccount() }
         }
 
     }
@@ -47,6 +47,10 @@ class SettingsFragment : Fragment(R.layout.settings_fragment) {
         viewModel.loadingLiveData.observeData(this) {
             loadingDialog.handleLoading(it)
         }
+    }
+
+    private fun navigateToDeactivateAccount() {
+        findNavController().navigate(SettingsFragmentDirections.toDeactivateAccountDialogFragment())
     }
 
     private fun navigateToChangePassword() {
