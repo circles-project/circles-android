018b12a3c24725b10a01e954c7144b32eaff600e
Taras <tarassmakula@gmail.com>
2022-05-31 16:52:48 +0300

    Change gallery rooms filter

--- app/src/main/java/com/futo/circles/feature/photos/PhotosDataSource.kt
+++ app/src/main/java/com/futo/circles/feature/photos/PhotosDataSource.kt
@@ -1,9 +1,8 @@
 package com.futo.circles.feature.photos
 
 import com.futo.circles.core.rooms.data_source.RoomsDataSource
-import com.futo.circles.mapping.toInviteGroupListItem
 import com.futo.circles.mapping.toJoinedGroupListItem
-import com.futo.circles.model.GROUP_TYPE
+import com.futo.circles.model.GALLERY_TYPE
 import com.futo.circles.model.RoomListItem
 import org.matrix.android.sdk.api.session.room.model.Membership
 import org.matrix.android.sdk.api.session.room.model.RoomSummary
@@ -12,13 +11,9 @@ class PhotosDataSource : RoomsDataSource() {
 
     override fun filterRooms(list: List<RoomSummary>): List<RoomListItem> {
         return list.mapNotNull { summary ->
-            if (summary.roomType == GROUP_TYPE) {
-                when (summary.membership) {
-                    Membership.INVITE -> summary.toInviteGroupListItem()
-                    Membership.JOIN -> summary.toJoinedGroupListItem()
-                    else -> null
-                }
+            if (summary.roomType == GALLERY_TYPE && summary.membership == Membership.JOIN) {
+                summary.toJoinedGroupListItem()
             } else null
-        }.sortedBy { it.membership }
+        }
     }
 }
