34ca99beaab8db54dd7c59f12c82520025814313
Taras <tarassmakula@gmail.com>
2022-06-03 18:46:58 +0300

    Create view model

---
+++ app/src/main/java/com/futo/circles/feature/photos/preview/GalleryImageViewModel.kt
@@ -0,0 +1,39 @@
+package com.futo.circles.feature.photos.preview
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.futo.circles.core.SingleEventLiveData
+import com.futo.circles.extensions.launchBg
+import com.futo.circles.feature.timeline.post.PostOptionsDataSource
+import com.futo.circles.feature.timeline.post.share.ShareableContent
+
+class GalleryImageViewModel(
+    private val roomId: String,
+    private val eventId: String,
+    galleryImageDataSource: GalleryImageDataSource,
+    private val postOptionsDataSource: PostOptionsDataSource
+) : ViewModel() {
+
+    val galleryImageLiveData = MutableLiveData(galleryImageDataSource.getImageItem())
+    val shareLiveData = SingleEventLiveData<ShareableContent>()
+    val downloadImageLiveData = SingleEventLiveData<Unit>()
+
+    fun shareImage() {
+        val content = galleryImageLiveData.value?.imageContent ?: return
+        launchBg {
+            shareLiveData.postValue(postOptionsDataSource.getShareableContent(content))
+        }
+    }
+
+    fun removeImage() {
+        postOptionsDataSource.removeMessage(roomId, eventId)
+    }
+
+    fun saveImage() {
+        val imageContent = galleryImageLiveData.value?.imageContent ?: return
+        launchBg {
+            postOptionsDataSource.saveImage(imageContent)
+            downloadImageLiveData.postValue(Unit)
+        }
+    }
+}
