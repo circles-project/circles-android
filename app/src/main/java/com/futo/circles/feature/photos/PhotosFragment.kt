3212bf578a990bb2b15520deb97b0b89e28b59c6
Taras <tarassmakula@gmail.com>
2022-06-09 16:31:19 +0300

    Add new mode to galleries fragment

--- app/src/main/java/com/futo/circles/feature/photos/PhotosFragment.kt
+++ app/src/main/java/com/futo/circles/feature/photos/PhotosFragment.kt
@@ -1,7 +1,12 @@
 package com.futo.circles.feature.photos
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.findNavController
+import com.futo.circles.core.image_picker.PickGalleryListener
 import com.futo.circles.core.rooms.RoomsFragment
+import com.futo.circles.extensions.setIsVisible
 import com.futo.circles.model.RoomListItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -9,8 +14,22 @@ class PhotosFragment : RoomsFragment() {
 
     override val viewModel by viewModel<PhotosViewModel>()
 
+    private var pickGalleryListener: PickGalleryListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        pickGalleryListener = parentFragment as? PickGalleryListener
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fbAddRoom.setIsVisible(pickGalleryListener == null)
+    }
+
     override fun onRoomListItemClicked(room: RoomListItem) {
-        findNavController().navigate(PhotosFragmentDirections.toGalleryFragment(room.id))
+        pickGalleryListener?.onGalleryChosen(room.id) ?: run {
+            findNavController().navigate(PhotosFragmentDirections.toGalleryFragment(room.id))
+        }
     }
 
     override fun navigateToCreateRoom() {
