7ce478205ae98c87aae232a1cfbfd11297e624db
Taras <tarassmakula@gmail.com>
2022-06-09 16:30:56 +0300

    Create screen for picker

---
+++ app/src/main/java/com/futo/circles/core/image_picker/PickGalleryImageDialogFragment.kt
@@ -0,0 +1,72 @@
+package com.futo.circles.core.image_picker
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import com.futo.circles.R
+import com.futo.circles.core.fragment.BaseFullscreenDialogFragment
+import com.futo.circles.databinding.PickGalleryImageDialogFragmentBinding
+import com.futo.circles.feature.photos.PhotosFragment
+import com.futo.circles.feature.photos.gallery.GalleryFragment
+
+interface PickGalleryListener {
+    fun onGalleryChosen(id: String)
+}
+
+interface PickGalleryImageListener {
+    fun onImageSelected(id: String)
+}
+
+class PickGalleryImageDialogFragment :
+    BaseFullscreenDialogFragment(PickGalleryImageDialogFragmentBinding::inflate),
+    PickGalleryListener, PickGalleryImageListener {
+
+    private val photosRoomsFragment by lazy { PhotosFragment() }
+
+    private val binding by lazy {
+        getBinding() as PickGalleryImageDialogFragmentBinding
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return object : Dialog(requireActivity(), theme) {
+            override fun onBackPressed() {
+                handleBackPress()
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener { handleBackPress() }
+        addGalleriesFragment()
+    }
+
+    private fun handleBackPress(){
+        if (photosRoomsFragment.isAdded) activity?.onBackPressed()
+        else addGalleriesFragment()
+    }
+
+    private fun addGalleriesFragment() {
+        binding.toolbar.title = getString(R.string.choose_gallery)
+        childFragmentManager.beginTransaction()
+            .replace(R.id.lContainer, photosRoomsFragment)
+            .commitAllowingStateLoss()
+    }
+
+    private fun addPhotosFragment(roomId: String) {
+        binding.toolbar.title = getString(R.string.pick_image)
+        val fragment = GalleryFragment.create(roomId)
+        childFragmentManager.beginTransaction()
+            .replace(R.id.lContainer, fragment)
+            .commitAllowingStateLoss()
+    }
+
+    override fun onGalleryChosen(id: String) {
+        addPhotosFragment(id)
+    }
+
+    override fun onImageSelected(id: String) {
+
+    }
+
+}
