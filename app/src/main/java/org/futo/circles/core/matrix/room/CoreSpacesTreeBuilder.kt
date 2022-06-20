95af318c2bff447542bc48fe0c1e719b62d4245c
Taras <tarassmakula@gmail.com>
2022-06-20 19:32:07 +0300

    Add isCirclesHierarchyCreated method

--- app/src/main/java/org/futo/circles/core/matrix/room/CoreSpacesTreeBuilder.kt
+++ app/src/main/java/org/futo/circles/core/matrix/room/CoreSpacesTreeBuilder.kt
@@ -1,16 +1,19 @@
 package org.futo.circles.core.matrix.room
 
 import android.content.Context
-import org.futo.circles.R
-import org.futo.circles.model.*
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import org.futo.circles.R
+import org.futo.circles.model.*
+import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
 class CoreSpacesTreeBuilder(
     private val context: Context,
     private val createRoomDataSource: CreateRoomDataSource
 ) {
+    private val session by lazy { MatrixSessionProvider.currentSession }
 
     suspend fun createCoreSpacesTree() {
         createRoomDataSource.createRoom(RootSpace())
@@ -23,4 +26,9 @@ class CoreSpacesTreeBuilder(
         }
         createRoomDataSource.createRoom(Gallery(), context.getString(R.string.photos))
     }
+
+    fun isCirclesHierarchyCreated(): Boolean = session?.roomService()
+        ?.getRoomSummaries(roomSummaryQueryParams { excludeType = null })
+        ?.firstOrNull { summary -> summary.hasTag(ROOT_SPACE_TAG) } != null
+
 }
