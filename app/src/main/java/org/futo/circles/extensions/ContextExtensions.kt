f40f2196b89b066d21a7d88ef4764882c2a62e93
Taras <tarassmakula@gmail.com>
2022-06-21 17:42:00 +0300

    Disable screen scale

--- app/src/main/java/org/futo/circles/extensions/ContextExtensions.kt
+++ app/src/main/java/org/futo/circles/extensions/ContextExtensions.kt
@@ -4,3 +4,11 @@ import android.content.Context
 import androidx.annotation.DimenRes
 
 fun Context.dimen(@DimenRes resource: Int): Int = resources.getDimensionPixelSize(resource)
+
+fun Context.disableScreenScale(): Context {
+    val overrideConfiguration = resources.configuration.apply {
+        fontScale = 1f
+        densityDpi = resources.displayMetrics.xdpi.toInt()
+    }
+    return createConfigurationContext(overrideConfiguration)
+}
