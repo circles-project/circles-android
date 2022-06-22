ddaeab2e69a8fd23f3e4f44ea2dc70989bf7fba8
Taras <tarassmakula@gmail.com>
2022-06-22 18:04:01 +0300

    Create home server utils

---
+++ app/src/main/java/org/futo/circles/core/HomeServerUtils.kt
@@ -0,0 +1,15 @@
+package org.futo.circles.core
+
+object HomeServerUtils {
+
+    private const val MATRIX_URL_PREFIX = "https://matrix."
+
+    fun getHomeServerUrlFromUserName(username: String): String {
+        val domain = username.substringAfter(":")
+        return getHomeServerUrlFromDomain(domain)
+    }
+
+    fun getHomeServerUrlFromDomain(domain: String): String {
+        return "$MATRIX_URL_PREFIX$domain/"
+    }
+}
