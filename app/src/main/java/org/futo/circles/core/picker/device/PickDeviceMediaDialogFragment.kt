b0a716140a25f6a166df0ec33bc8dfa56db43a27
Taras <tarassmakula@gmail.com>
2022-07-13 16:37:27 +0300

    Create fragment for media

---
+++ app/src/main/java/org/futo/circles/core/picker/device/PickDeviceMediaDialogFragment.kt
@@ -0,0 +1,43 @@
+package org.futo.circles.core.picker.device
+
+import android.os.Bundle
+import android.view.View
+import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
+import org.futo.circles.core.list.BaseRvDecoration
+import org.futo.circles.core.picker.device.list.DeviceMediaViewHolder
+import org.futo.circles.core.picker.device.list.DeviceMedialListAdapter
+import org.futo.circles.databinding.PickDeviceMediaDialogFragmentBinding
+import org.futo.circles.extensions.observeData
+import org.futo.circles.feature.photos.gallery.list.GalleryImageViewHolder
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class PickDeviceMediaDialogFragment :
+    BaseFullscreenDialogFragment(PickDeviceMediaDialogFragmentBinding::inflate) {
+
+    private val binding by lazy {
+        getBinding() as PickDeviceMediaDialogFragmentBinding
+    }
+
+    private val viewModel by viewModel<PickDeviceMediaViewModel>()
+
+    private val listAdapter by lazy {
+        DeviceMedialListAdapter(onClick = { postId -> })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObservers()
+    }
+
+    private fun setupViews() {
+        binding.rvMedia.apply {
+            adapter = listAdapter
+            addItemDecoration(BaseRvDecoration.OffsetDecoration<DeviceMediaViewHolder>(2))
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.mediaLiveData.observeData(this) { listAdapter.submitList(it) }
+    }
+}
