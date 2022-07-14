82da0acda3cc2b60f1677c15cb057870518cc85b
Taras <tarassmakula@gmail.com>
2022-07-14 18:10:46 +0300

    Create universal dispatch media intent

--- app/src/main/java/org/futo/circles/core/picker/MediaPickerHelper.kt
+++ app/src/main/java/org/futo/circles/core/picker/MediaPickerHelper.kt
@@ -9,54 +9,56 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import org.futo.circles.R
+import org.futo.circles.core.FileUtils.createImageFile
+import org.futo.circles.core.FileUtils.createVideoFile
 import org.futo.circles.core.PermissionHelper
 import org.futo.circles.core.picker.device.PickDeviceMediaDialogFragment
-import org.futo.circles.extensions.getContentUriForFileUri
+import org.futo.circles.extensions.getUri
 import org.futo.circles.extensions.showError
+import java.io.File
+import java.io.IOException
 
-class MediaPickerHelper(private val fragment: Fragment) : PickMediaDialogListener {
+enum class MediaType { Image, Video }
+
+class MediaPickerHelper(private val fragment: Fragment, allMediaTypeAvailable: Boolean = false) :
+    PickMediaDialogListener {
 
     private val pickMediaDialog by lazy {
-        PickMediaDialog(fragment.requireContext(), this)
+        PickMediaDialog(fragment.requireContext(), this, allMediaTypeAvailable)
     }
 
     private val permissionHelper = PermissionHelper(
         fragment,
         onGranted = { showDevicePicker() }
     )
+    private var mediaData: Pair<MediaType, Uri>? = null
 
-    private val intent =
+    private val launcher =
         fragment.registerForActivityResult(ActivityResultContracts.StartActivityForResult())
         { result: ActivityResult ->
-            val data = result.data
             when (result.resultCode) {
                 Activity.RESULT_OK -> {
-                    data?.data?.let { uri ->
-                        val contentUri =
-                            uri.getContentUriForFileUri(fragment.requireContext()) ?: run {
-                                fragment.showError(fragment.getString(R.string.unexpected_error))
-                                return@let
-                            }
-                        onSelected?.let { it(itemId, contentUri) }
+                    mediaData?.let {
+                        onSelected?.invoke(itemId, it.second, it.first)
                     } ?: fragment.showError(fragment.getString(R.string.unexpected_error))
                 }
             }
         }
 
-    private var onSelected: ((Int?, Uri) -> Unit)? = null
+    private var onSelected: ((Int?, Uri, MediaType) -> Unit)? = null
     private var itemId: Int? = null
 
 
-    fun showImagePickerDialog(onImageSelected: (Int?, Uri) -> Unit, id: Int? = null) {
+    fun showMediaPickerDialog(onMediaSelected: (Int?, Uri, MediaType) -> Unit, id: Int? = null) {
         itemId = id
-        onSelected = onImageSelected
+        onSelected = onMediaSelected
         pickMediaDialog.show()
     }
 
     override fun onPickMethodSelected(method: PickImageMethod, allMediaTypeAvailable: Boolean) {
         when (method) {
-            PickImageMethod.Photo -> intent.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
-            PickImageMethod.Video -> intent.launch(Intent(MediaStore.ACTION_VIDEO_CAPTURE))
+            PickImageMethod.Photo -> dispatchMediaIntent(MediaType.Image)
+            PickImageMethod.Video -> dispatchMediaIntent(MediaType.Video)
             PickImageMethod.Device -> permissionHelper.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             PickImageMethod.Gallery -> showGalleryPicker()
         }
@@ -69,7 +71,7 @@ class MediaPickerHelper(private val fragment: Fragment) : PickMediaDialogListene
         ) { key, bundle ->
             if (key == pickImageRequestKey) {
                 val uri = Uri.parse(bundle.getString(uriKey))
-                onSelected?.invoke(itemId, uri)
+                onSelected?.invoke(itemId, uri, MediaType.Image)
             }
         }
         PickGalleryImageDialogFragment()
@@ -83,12 +85,35 @@ class MediaPickerHelper(private val fragment: Fragment) : PickMediaDialogListene
         ) { key, bundle ->
             if (key == pickImageRequestKey) {
                 val uri = Uri.parse(bundle.getString(uriKey))
-                onSelected?.invoke(itemId, uri)
+                onSelected?.invoke(itemId, uri, MediaType.Image)
             }
         }
 
         PickDeviceMediaDialogFragment()
             .show(fragment.childFragmentManager, "PickDeviceMediaDialogFragment")
+    }
+
+    private fun dispatchMediaIntent(type: MediaType) {
+        val context = fragment.context ?: return
+        val intentAction = when (type) {
+            MediaType.Image -> MediaStore.ACTION_IMAGE_CAPTURE
+            MediaType.Video -> MediaStore.ACTION_VIDEO_CAPTURE
+        }
+        Intent(intentAction).also { captureIntent ->
+            val file: File? = try {
+                when (type) {
+                    MediaType.Image -> createImageFile(context)
+                    MediaType.Video -> createVideoFile(context)
+                }
+            } catch (ex: IOException) {
+                null
+            }
+            file?.let {
+                val uri: Uri = file.getUri(context).also { mediaData = type to it }
+                captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+                launcher.launch(captureIntent)
+            }
+        }
     }
 
     companion object {
