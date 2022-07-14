6d38d6afcd2785410884f832bdfca9a2eef2c349
Taras <tarassmakula@gmail.com>
2022-07-14 16:14:27 +0300

    Fix permission register

--- app/src/main/java/org/futo/circles/core/picker/MediaPickerHelper.kt
+++ app/src/main/java/org/futo/circles/core/picker/MediaPickerHelper.kt
@@ -1,10 +1,18 @@
 package org.futo.circles.core.picker
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import org.futo.circles.R
 import org.futo.circles.core.PermissionHelper
 import org.futo.circles.core.picker.device.PickDeviceMediaDialogFragment
+import org.futo.circles.extensions.getContentUriForFileUri
+import org.futo.circles.extensions.showError
 
 class MediaPickerHelper(private val fragment: Fragment) : PickMediaDialogListener {
 
@@ -12,12 +20,28 @@ class MediaPickerHelper(private val fragment: Fragment) : PickMediaDialogListene
         PickMediaDialog(fragment.requireContext(), this)
     }
 
-    private val permissionHelper by lazy {
-        PermissionHelper(
-            fragment,
-            onGranted = { showDevicePicker() }
-        )
-    }
+    private val permissionHelper = PermissionHelper(
+        fragment,
+        onGranted = { showDevicePicker() }
+    )
+
+    private val intent =
+        fragment.registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            val data = result.data
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+                    data?.data?.let { uri ->
+                        val contentUri =
+                            uri.getContentUriForFileUri(fragment.requireContext()) ?: run {
+                                fragment.showError(fragment.getString(R.string.unexpected_error))
+                                return@let
+                            }
+                        onSelected?.let { it(itemId, contentUri) }
+                    } ?: fragment.showError(fragment.getString(R.string.unexpected_error))
+                }
+            }
+        }
 
     private var onSelected: ((Int?, Uri) -> Unit)? = null
     private var itemId: Int? = null
@@ -31,13 +55,13 @@ class MediaPickerHelper(private val fragment: Fragment) : PickMediaDialogListene
 
     override fun onPickMethodSelected(method: PickImageMethod, allMediaTypeAvailable: Boolean) {
         when (method) {
-            PickImageMethod.Photo -> {}
-            PickImageMethod.Video -> {}
+            PickImageMethod.Photo -> intent.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+            PickImageMethod.Video -> intent.launch(Intent(MediaStore.ACTION_VIDEO_CAPTURE))
             PickImageMethod.Device -> permissionHelper.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             PickImageMethod.Gallery -> showGalleryPicker()
         }
     }
-    
+
     private fun showGalleryPicker() {
         fragment.childFragmentManager.setFragmentResultListener(
             pickImageRequestKey,
