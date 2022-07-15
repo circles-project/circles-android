9acad76b00cc10edd1ad79d6cd0ce94e929eca23
Taras <tarassmakula@gmail.com>
2022-07-15 18:56:53 +0300

    Add device picker fragment result

--- app/src/main/java/org/futo/circles/core/picker/device/PickDeviceMediaDialogFragment.kt
+++ app/src/main/java/org/futo/circles/core/picker/device/PickDeviceMediaDialogFragment.kt
@@ -3,13 +3,16 @@ package org.futo.circles.core.picker.device
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.core.list.BaseRvDecoration
+import org.futo.circles.core.picker.MediaPickerHelper
 import org.futo.circles.core.picker.MediaPickerHelper.Companion.IS_VIDEO_AVAILABLE
 import org.futo.circles.core.picker.device.list.DeviceMediaViewHolder
 import org.futo.circles.core.picker.device.list.DeviceMedialListAdapter
 import org.futo.circles.databinding.PickDeviceMediaDialogFragmentBinding
 import org.futo.circles.extensions.observeData
+import org.futo.circles.model.DeviceMediaListItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -25,7 +28,7 @@ class PickDeviceMediaDialogFragment :
     }
 
     private val listAdapter by lazy {
-        DeviceMedialListAdapter(onClick = { postId -> })
+        DeviceMedialListAdapter(onClick = { item -> onMediaSelected(item) })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,6 +47,17 @@ class PickDeviceMediaDialogFragment :
 
     private fun setupObservers() {
         viewModel.mediaLiveData.observeData(this) { listAdapter.submitList(it) }
+    }
+
+    private fun onMediaSelected(item: DeviceMediaListItem) {
+        setFragmentResult(
+            MediaPickerHelper.pickMediaRequestKey,
+            bundleOf(
+                MediaPickerHelper.uriKey to item.contentUri.toString(),
+                MediaPickerHelper.mediaTypeKey to item.type.ordinal
+            )
+        )
+        dismiss()
     }
 
     companion object {
