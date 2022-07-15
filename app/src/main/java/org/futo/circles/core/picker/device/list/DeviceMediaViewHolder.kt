4f46a8a2a3e6a8b5e89c9c1d6b8f3d5e461d5dfc
Taras <tarassmakula@gmail.com>
2022-07-15 17:49:14 +0300

    Add image/video view types

--- app/src/main/java/org/futo/circles/core/picker/device/list/DeviceMediaViewHolder.kt
+++ app/src/main/java/org/futo/circles/core/picker/device/list/DeviceMediaViewHolder.kt
@@ -1,16 +1,52 @@
 package org.futo.circles.core.picker.device.list
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.futo.circles.core.list.ViewBindingHolder
+import org.futo.circles.core.list.context
+import org.futo.circles.core.picker.device.PickDeviceMediaDataSource
 import org.futo.circles.databinding.DeviceVideoListItemBinding
+import org.futo.circles.databinding.GalleryImageListItemBinding
 import org.futo.circles.extensions.onClick
+import org.futo.circles.model.DeviceImageListItem
+import org.futo.circles.model.DeviceMediaListItem
 import org.futo.circles.model.DeviceVideoListItem
 
-class DeviceMediaViewHolder(
+abstract class DeviceMediaViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun bind(data: DeviceMediaListItem)
+}
+
+class DeviceImageViewHolder(
     parent: ViewGroup,
     onMediaClicked: (Int) -> Unit
-) : RecyclerView.ViewHolder(inflate(parent, DeviceVideoListItemBinding::inflate)) {
+) : DeviceMediaViewHolder(inflate(parent, GalleryImageListItemBinding::inflate)) {
+
+    private companion object : ViewBindingHolder
+
+    private val binding = baseBinding as GalleryImageListItemBinding
+
+    init {
+        onClick(itemView) { position -> onMediaClicked(position) }
+    }
+
+    override fun bind(data: DeviceMediaListItem) {
+        if (data !is DeviceImageListItem) return
+        Glide.with(context)
+            .load(data.contentUri)
+            .override(
+                PickDeviceMediaDataSource.THUMBNAIL_SIZE,
+                PickDeviceMediaDataSource.THUMBNAIL_SIZE
+            )
+            .into(binding.ivGalleryImage)
+    }
+}
+
+class DeviceVideoViewHolder(
+    parent: ViewGroup,
+    onMediaClicked: (Int) -> Unit
+) : DeviceMediaViewHolder(inflate(parent, DeviceVideoListItemBinding::inflate)) {
 
     private companion object : ViewBindingHolder
 
@@ -20,10 +56,12 @@ class DeviceMediaViewHolder(
         onClick(itemView) { position -> onMediaClicked(position) }
     }
 
-    fun bind(data: DeviceVideoListItem) {
+    override fun bind(data: DeviceMediaListItem) {
+        if (data !is DeviceVideoListItem) return
         with(binding) {
             ivVideoCover.setImageBitmap(data.thumbnail)
             tvDuration.text = data.durationString
         }
+
     }
 }
