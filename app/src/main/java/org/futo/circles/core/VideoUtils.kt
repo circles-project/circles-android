1b41059a71d0d0467bc59d09395ad56a860a4640
Taras <tarassmakula@gmail.com>
2022-07-18 16:50:12 +0300

    Add video resolution to videoUtils

--- app/src/main/java/org/futo/circles/core/VideoUtils.kt
+++ app/src/main/java/org/futo/circles/core/VideoUtils.kt
@@ -17,13 +17,25 @@ object VideoUtils {
         size: Int = PickDeviceMediaDataSource.THUMBNAIL_SIZE
     ): Bitmap = contentResolver.loadThumbnail(uri, Size(size, size), null)
 
-    fun getVideoDuration(context: Context, uri: Uri): Long {
+    fun getVideoDuration(context: Context, uri: Uri): Long = try {
         val retriever = MediaMetadataRetriever()
         retriever.setDataSource(context, uri)
         val duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
         retriever.release()
+        duration?.toLongOrNull() ?: 0L
+    } catch (e: Exception) {
+        0L
+    }
 
-        return duration?.toLongOrNull() ?: 0
+    fun getVideoResolution(context: Context, uri: Uri): Size = try {
+        val retriever = MediaMetadataRetriever()
+        retriever.setDataSource(context, uri)
+        val height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
+        val width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
+        retriever.release()
+        Size(width?.toInt() ?: 0, height?.toInt() ?: 0)
+    } catch (e: Exception) {
+        Size(0, 0)
     }
 
     fun getVideoDurationString(duration: Long, includeHoursZeros: Boolean = false): String {
