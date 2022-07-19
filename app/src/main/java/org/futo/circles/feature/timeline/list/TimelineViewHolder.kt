b44b931ee46a6fc00a681a8fcf155f23b4da7efd
Taras <tarassmakula@gmail.com>
2022-07-19 17:43:04 +0300

    Create view holder

--- app/src/main/java/org/futo/circles/feature/timeline/list/TimelineViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/timeline/list/TimelineViewHolder.kt
@@ -8,17 +8,13 @@ import org.futo.circles.R
 import org.futo.circles.core.list.ViewBindingHolder
 import org.futo.circles.databinding.ImagePostViewBinding
 import org.futo.circles.databinding.TextPostViewBinding
+import org.futo.circles.databinding.VideoPostViewBinding
 import org.futo.circles.extensions.UriContentScheme
 import org.futo.circles.extensions.gone
 import org.futo.circles.extensions.loadEncryptedImage
-import org.futo.circles.model.ImageContent
-import org.futo.circles.model.Post
-import org.futo.circles.model.PostItemPayload
-import org.futo.circles.model.TextContent
-import org.futo.circles.provider.MatrixSessionProvider
-import org.futo.circles.view.PostOptionsListener
+import org.futo.circles.model.*
 import org.futo.circles.view.PostLayout
-import org.matrix.android.sdk.api.session.content.ContentUploadStateTracker
+import org.futo.circles.view.PostOptionsListener
 
 sealed class PostViewHolder(view: View, private val userPowerLevel: Int) :
     RecyclerView.ViewHolder(view) {
@@ -62,15 +58,13 @@ class ImagePostViewHolder(
     parent: ViewGroup,
     postOptionsListener: PostOptionsListener,
     userPowerLevel: Int
-) : PostViewHolder(inflate(parent, ImagePostViewBinding::inflate), userPowerLevel) {
+) : PostViewHolder(inflate(parent, ImagePostViewBinding::inflate), userPowerLevel),
+    UploadMediaViewHolder {
 
     private companion object : ViewBindingHolder
 
     private val binding = baseBinding as ImagePostViewBinding
     override val postLayout: PostLayout = binding.lImagePost
-    private val tracker = MatrixSessionProvider.currentSession?.contentUploadProgressTracker()
-    private var postId: String? = null
-    private var listener: ContentUploadStateTracker.UpdateListener? = null
 
     init {
         binding.lImagePost.setListener(postOptionsListener)
@@ -79,25 +73,48 @@ class ImagePostViewHolder(
     override fun bind(post: Post) {
         super.bind(post)
         binding.vLoadingImage.gone()
-        tracker?.track(
-            post.id.also { postId = it },
-            UploadImageProgressHelper.getListener(binding.vLoadingImage).also { listener = it }
-        )
+        track(post.id, binding.vLoadingImage)
         (post.content as? ImageContent)?.let {
-            if (it.fileUrl.startsWith(UriContentScheme)) {
+            if (it.mediaContentData.fileUrl.startsWith(UriContentScheme)) {
                 binding.ivContent.setImageResource(R.drawable.blurred_placeholder)
             } else {
                 val imageWith = binding.ivContent.width
                 val size = Size(imageWith, (imageWith / it.aspectRatio).toInt())
-                binding.ivContent.loadEncryptedImage(it, size)
+                binding.ivContent.loadEncryptedImage(it.mediaContentData, size)
             }
         }
     }
+}
 
-    fun unTrack() {
-        val id = postId ?: return
-        val callback = listener ?: return
-        tracker?.untrack(id, callback)
+class VideoPostViewHolder(
+    parent: ViewGroup,
+    postOptionsListener: PostOptionsListener,
+    userPowerLevel: Int
+) : PostViewHolder(inflate(parent, VideoPostViewBinding::inflate), userPowerLevel),
+    UploadMediaViewHolder {
+
+    private companion object : ViewBindingHolder
+
+    private val binding = baseBinding as VideoPostViewBinding
+    override val postLayout: PostLayout = binding.lVideoPost
+
+    init {
+        binding.lVideoPost.setListener(postOptionsListener)
     }
 
+    override fun bind(post: Post) {
+        super.bind(post)
+        binding.vLoadingView.gone()
+        track(post.id, binding.vLoadingView)
+        (post.content as? VideoContent)?.let {
+            if (it.mediaContentData.fileUrl.startsWith(UriContentScheme)) {
+                binding.videoItem.ivVideoCover.setImageResource(R.drawable.blurred_placeholder)
+            } else {
+                val imageWith = binding.lVideoPost.width
+                val size = Size(imageWith, (imageWith / it.aspectRatio).toInt())
+                binding.videoItem.ivVideoCover.loadEncryptedImage(it.mediaContentData, size)
+            }
+            binding.videoItem.tvDuration.text = it.duration
+        }
+    }
 }
