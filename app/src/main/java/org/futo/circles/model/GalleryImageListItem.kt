0f74dfbc406215e06352b1f7ff37cb9258642d4c
Taras <tarassmakula@gmail.com>
2022-07-20 16:41:53 +0300

    Create model

--- app/src/main/java/org/futo/circles/model/GalleryImageListItem.kt
+++ app/src/main/java/org/futo/circles/model/GalleryImageListItem.kt
@@ -2,8 +2,20 @@ package org.futo.circles.model
 
 import org.futo.circles.core.list.IdEntity
 
+sealed class GalleryContentListItem(
+    override val id: String,
+    open val postInfo: PostInfo,
+    val type: PostContentType
+) : IdEntity<String>
+
 data class GalleryImageListItem(
     override val id: String,
     val imageContent: ImageContent,
-    val postInfo: PostInfo
-) : IdEntity<String>
+    override val postInfo: PostInfo
+) : GalleryContentListItem(id, postInfo, imageContent.type)
+
+data class GalleryVideoListItem(
+    override val id: String,
+    val videoContent: VideoContent,
+    override val postInfo: PostInfo
+) : GalleryContentListItem(id, postInfo, videoContent.type)
