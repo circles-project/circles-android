e4648d5db74ff52fc027f1abfc118d16dcb83ad6
Taras <tarassmakula@gmail.com>
2022-07-22 17:30:07 +0300

    Add download decrypted file logic

--- app/src/main/java/org/futo/circles/core/utils/FileUtils.kt
+++ app/src/main/java/org/futo/circles/core/utils/FileUtils.kt
@@ -1,7 +1,11 @@
 package org.futo.circles.core.utils
 
 import android.content.Context
+import android.net.Uri
 import android.os.Environment
+import org.futo.circles.extensions.getUri
+import org.futo.circles.model.MediaContentData
+import org.futo.circles.provider.MatrixSessionProvider
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -10,7 +14,7 @@ import java.util.*
 object FileUtils {
 
     @Throws(IOException::class)
-     fun createImageFile(context: Context): File {
+    fun createImageFile(context: Context): File {
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
@@ -22,4 +26,28 @@ object FileUtils {
         val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_MOVIES)
         return File.createTempFile("VIDEO_${timeStamp}_", ".mp4", storageDir)
     }
+
+    private suspend fun downloadEncryptedFile(
+        contentData: MediaContentData,
+        onError: (message: String) -> Unit = {}
+    ): File? {
+        val session = MatrixSessionProvider.currentSession ?: kotlin.run {
+            onError("Session is not active")
+            return null
+        }
+        return runCatching {
+            session.fileService().downloadFile(
+                fileName = contentData.fileName,
+                mimeType = contentData.mimeType,
+                url = contentData.fileUrl,
+                elementToDecrypt = contentData.elementToDecrypt
+            )
+        }.fold({ it }, { onError(it.message ?: ""); null }
+        )
+    }
+
+    suspend fun downloadEncryptedFileToContentUri(
+        context: Context,
+        contentData: MediaContentData
+    ): Uri? = downloadEncryptedFile(contentData)?.getUri(context)
 }
