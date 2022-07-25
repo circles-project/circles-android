402bdba843a7cf6807d8120e2a1317c6d8c51ef2
Taras <tarassmakula@gmail.com>
2022-07-25 14:35:54 +0300

    New full screen layout design

--- app/src/main/java/org/futo/circles/feature/photos/preview/MediaPreviewDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/photos/preview/MediaPreviewDialogFragment.kt
@@ -1,19 +1,16 @@
 package org.futo.circles.feature.photos.preview
 
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
-import android.view.WindowManager
 import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
 import org.futo.circles.R
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.databinding.MediaPreviewDialogFragmentBinding
-import org.futo.circles.extensions.loadEncryptedIntoWithAspect
-import org.futo.circles.extensions.observeData
-import org.futo.circles.extensions.showDialog
-import org.futo.circles.extensions.showSuccess
+import org.futo.circles.extensions.*
 import org.futo.circles.feature.timeline.post.share.ShareProvider
 import org.futo.circles.model.ImageContent
 import org.futo.circles.model.VideoContent
@@ -28,21 +25,26 @@ class MediaPreviewDialogFragment :
         parametersOf(args.roomId, args.eventId)
     }
 
-    private val binding by lazy {
-        getBinding() as MediaPreviewDialogFragmentBinding
-    }
+    private val binding by lazy { getBinding() as MediaPreviewDialogFragmentBinding }
+
+    private val hideHandler = Handler(Looper.myLooper()!!)
+    private val showRunnable = Runnable { binding.toolbar.visible() }
+    private val hideRunnable = Runnable { hide() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dialog?.window?.let {
-            it.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            it.statusBarColor = Color.BLACK
-        }
+        setupViews()
         setupToolbar()
         setupObservers()
     }
 
-    @SuppressLint("RestrictedApi")
+    private fun setupViews() {
+        binding.parent.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        binding.parent.setOnClickListener { toggle() }
+        delayedHide()
+    }
+
     private fun setupToolbar() {
         with(binding.toolbar) {
             setNavigationOnClickListener { activity?.onBackPressed() }
@@ -96,5 +98,29 @@ class MediaPreviewDialogFragment :
                 activity?.onBackPressed()
             }
         )
+    }
+
+    private fun toggle() {
+        if (binding.toolbar.isVisible) hide() else show()
+    }
+
+    private fun hide() {
+        binding.toolbar.gone()
+        hideHandler.removeCallbacks(showRunnable)
+    }
+
+    private fun show() {
+        hideHandler.postDelayed(showRunnable, UI_ANIMATION_DELAY)
+        delayedHide()
+    }
+
+    private fun delayedHide() {
+        hideHandler.removeCallbacks(hideRunnable)
+        hideHandler.postDelayed(hideRunnable, AUTO_HIDE_DELAY_MILLIS)
+    }
+
+    companion object {
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000L
+        private const val UI_ANIMATION_DELAY = 300L
     }
 }
