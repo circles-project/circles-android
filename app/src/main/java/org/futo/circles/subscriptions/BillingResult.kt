fbf4148b3dc1990824452b1784e2be81d9e71e56
Taras <tarassmakula@gmail.com>
2022-08-03 16:57:02 +0300

    Update billing result according to new api

--- app/src/main/java/org/futo/circles/subscriptions/BillingResult.kt
+++ app/src/main/java/org/futo/circles/subscriptions/BillingResult.kt
@@ -2,15 +2,12 @@ package org.futo.circles.subscriptions
 
 import com.android.billingclient.api.BillingClient
 
-
-@Suppress("unused")
 sealed class BillingResult<out T> {
     data class Success<out T>(val data: T) : BillingResult<T>()
 
-    data class Failure(@BillingClient.BillingResponse val code: Int) : BillingResult<Nothing>()
+    data class Failure(@BillingClient.BillingResponseCode val code: Int) : BillingResult<Nothing>()
     object FeatureNotSupported : BillingResult<Nothing>()
     object NotConnected : BillingResult<Nothing>()
 }
 
-@Suppress("unused")
 fun <T> BillingResult<T>.unwrap(): T? = (this as? BillingResult.Success)?.data
