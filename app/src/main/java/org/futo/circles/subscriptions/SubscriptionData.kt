60fb96e4010948c81072c88e48d9eb49f5053c98
Taras <tarassmakula@gmail.com>
2022-08-03 16:57:23 +0300

    Change subscription data model

--- app/src/main/java/org/futo/circles/subscriptions/SubscriptionData.kt
+++ app/src/main/java/org/futo/circles/subscriptions/SubscriptionData.kt
@@ -1,11 +1,10 @@
 package org.futo.circles.subscriptions
 
+import com.android.billingclient.api.ProductDetails
 import org.futo.circles.core.list.IdEntity
 
 data class SubscriptionData(
-    val sku: String,
-    val price: String,
-    val duration: String?
+    val details: ProductDetails
 ) : IdEntity<String> {
-    override val id: String get() = sku
+    override val id: String = details.productId
 }
