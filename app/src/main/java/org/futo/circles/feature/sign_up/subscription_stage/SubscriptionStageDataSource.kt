33a18c1d030b275476c549de531662121a374a5b
Taras <tarassmakula@gmail.com>
2022-08-03 17:24:05 +0300

    Create subscription stage view model draft

---
+++ app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageDataSource.kt
@@ -0,0 +1,4 @@
+package org.futo.circles.feature.sign_up.subscription_stage
+
+class SubscriptionStageDataSource {
+}
---
+++ app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageViewModel.kt
@@ -0,0 +1,6 @@
+package org.futo.circles.feature.sign_up.subscription_stage
+
+import androidx.lifecycle.ViewModel
+
+class SubscriptionStageViewModel:ViewModel() {
+}
