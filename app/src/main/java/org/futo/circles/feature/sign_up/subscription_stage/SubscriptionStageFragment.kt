dbf1bc0a8c49f465668eb1760dd58cfd415c0b4d
Taras <tarassmakula@gmail.com>
2022-08-04 17:36:06 +0300

    Setup adapter to list

--- app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageFragment.kt
+++ app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageFragment.kt
@@ -7,6 +7,8 @@ import org.futo.circles.R
 import org.futo.circles.core.fragment.ParentBackPressOwnerFragment
 import org.futo.circles.databinding.SubscriptionStageFragmentBinding
 import org.futo.circles.extensions.observeResponse
+import org.futo.circles.feature.sign_up.subscription_stage.list.SubscriptionsAdapter
+import org.futo.circles.subscriptions.SubscriptionData
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SubscriptionStageFragment :
@@ -15,6 +17,12 @@ class SubscriptionStageFragment :
     private val binding by viewBinding(SubscriptionStageFragmentBinding::bind)
     private val viewModel by viewModel<SubscriptionStageViewModel>()
 
+    private val listAdapter by lazy {
+        SubscriptionsAdapter(
+            onItemClicked = { data -> onSubscriptionSelected(data) },
+        )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViews()
@@ -22,12 +30,14 @@ class SubscriptionStageFragment :
     }
 
     private fun setupViews() {
-        with(binding) {
-
-        }
+        binding.rvSubscriptions.adapter = listAdapter
     }
 
     private fun setupObservers() {
         viewModel.subscribeLiveData.observeResponse(this)
+    }
+
+    private fun onSubscriptionSelected(subscriptionData: SubscriptionData) {
+
     }
 }
