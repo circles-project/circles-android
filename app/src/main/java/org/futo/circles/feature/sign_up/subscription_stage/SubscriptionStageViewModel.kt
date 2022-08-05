a34518ffbdf42e2ed3539df83e0b4f435ed3d727
Taras <tarassmakula@gmail.com>
2022-08-05 17:08:51 +0300

    Handle view model logic

--- app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageViewModel.kt
@@ -1,9 +1,13 @@
 package org.futo.circles.feature.sign_up.subscription_stage
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.extensions.Response
 import org.futo.circles.extensions.launchBg
+import org.futo.circles.extensions.launchUi
+import org.futo.circles.model.SubscriptionListItem
+import org.futo.circles.subscriptions.SubscriptionManager
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
 
 class SubscriptionStageViewModel(
@@ -11,10 +15,24 @@ class SubscriptionStageViewModel(
 ) : ViewModel() {
 
     val subscribeLiveData = SingleEventLiveData<Response<RegistrationResult>>()
+    val purchaseLiveData = SingleEventLiveData<Response<Unit>>()
+    val subscriptionsListLiveData = MutableLiveData<Response<List<SubscriptionListItem>>>()
 
     fun validateSubscriptionReceipt(receipt: String) {
         launchBg {
             subscribeLiveData.postValue(dataSource.validateSubscriptionReceipt(receipt))
+        }
+    }
+
+    fun loadSubscriptionsList(subscriptionManager: SubscriptionManager) {
+        launchBg {
+            subscriptionsListLiveData.postValue(subscriptionManager.getDetails(dataSource.getProductIdsList()))
+        }
+    }
+
+    fun purchaseProduct(subscriptionManager: SubscriptionManager, productId: String) {
+        launchUi {
+            purchaseLiveData.postValue(subscriptionManager.purchaseProduct(productId))
         }
     }
 
