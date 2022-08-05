07340c1074c3830c45d9298f5d858fedff984b3c
Taras <tarassmakula@gmail.com>
2022-08-05 17:08:27 +0300

    Add subscription manager to fragment

--- app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageFragment.kt
+++ app/src/main/java/org/futo/circles/feature/sign_up/subscription_stage/SubscriptionStageFragment.kt
@@ -7,8 +7,10 @@ import org.futo.circles.R
 import org.futo.circles.core.fragment.ParentBackPressOwnerFragment
 import org.futo.circles.databinding.SubscriptionStageFragmentBinding
 import org.futo.circles.extensions.observeResponse
+import org.futo.circles.extensions.showError
 import org.futo.circles.feature.sign_up.subscription_stage.list.SubscriptionsAdapter
-import org.futo.circles.subscriptions.SubscriptionData
+import org.futo.circles.subscriptions.ItemPurchasedListener
+import org.futo.circles.subscriptions.google.GoogleSubscriptionsManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SubscriptionStageFragment :
@@ -17,10 +19,28 @@ class SubscriptionStageFragment :
     private val binding by viewBinding(SubscriptionStageFragmentBinding::bind)
     private val viewModel by viewModel<SubscriptionStageViewModel>()
 
-    private val listAdapter by lazy {
-        SubscriptionsAdapter(
-            onItemClicked = { data -> onSubscriptionSelected(data) },
+    private val subscriptionManager by lazy {
+        GoogleSubscriptionsManager(
+            requireActivity(), object : ItemPurchasedListener {
+                override fun onItemPurchased(purchase: String) {
+                    viewModel.validateSubscriptionReceipt(purchase)
+                }
+
+                override fun onPurchaseFailed(errorCode: Int) {
+                    showError(getString(R.string.purchase_failed_format, errorCode))
+                }
+
+            }
         )
+    }
+
+    private val listAdapter by lazy {
+        SubscriptionsAdapter(onItemClicked = { id -> onSubscriptionSelected(id) })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.loadSubscriptionsList(subscriptionManager)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,9 +55,13 @@ class SubscriptionStageFragment :
 
     private fun setupObservers() {
         viewModel.subscribeLiveData.observeResponse(this)
+        viewModel.purchaseLiveData.observeResponse(this)
+        viewModel.subscriptionsListLiveData.observeResponse(this, success = {
+            listAdapter.submitList(it)
+        })
     }
 
-    private fun onSubscriptionSelected(subscriptionData: SubscriptionData) {
-
+    private fun onSubscriptionSelected(productId: String) {
+        viewModel.purchaseProduct(subscriptionManager, productId)
     }
 }
