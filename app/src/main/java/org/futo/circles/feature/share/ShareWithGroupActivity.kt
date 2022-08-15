b23558b7b04794c9240c6e240a3c85e886cc6803
Taras <tarassmakula@gmail.com>
2022-08-15 17:48:57 +0300

    Create share with group activity

---
+++ app/src/main/java/org/futo/circles/feature/share/ShareWithGroupActivity.kt
@@ -0,0 +1,12 @@
+package org.futo.circles.feature.share
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.futo.circles.R
+
+class ShareWithGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_with_group)
+    }
+}
