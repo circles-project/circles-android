c3291ca7de6a5d89aab510955a664151b4a4f348
Taras <tarassmakula@gmail.com>
2022-08-17 18:45:42 +0300

    Implement share with group

--- app/src/main/java/org/futo/circles/feature/share/group/ShareWithGroupActivity.kt
+++ app/src/main/java/org/futo/circles/feature/share/group/ShareWithGroupActivity.kt
@@ -1,15 +1,17 @@
 package org.futo.circles.feature.share.group
 
 import org.futo.circles.R
-import org.futo.circles.feature.photos.select.SelectRoomsFragment
+import org.futo.circles.feature.photos.select.RoomsPicker
+import org.futo.circles.feature.room.select.SelectRoomsFragment
 import org.futo.circles.feature.share.BaseShareActivity
+import org.futo.circles.model.CircleRoomTypeArg
 
 class ShareWithGroupActivity : BaseShareActivity() {
 
     override val titleResId: Int = R.string.share_with_group
 
-    override val selectRoomsFragment: SelectRoomsFragment = TODO()
+    override val roomsPicker: RoomsPicker = SelectRoomsFragment.create(CircleRoomTypeArg.Group)
 
-    override fun getShareRoomsIds(): List<String> = TODO()
+    override fun getShareRoomsIds(): List<String> = roomsPicker.getSelectedRooms().map { it.id }
 
 }
