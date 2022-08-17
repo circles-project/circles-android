0367a4aab3694007b516a3a9ead02d2d25de38d4
Taras <tarassmakula@gmail.com>
2022-08-17 18:45:06 +0300

    Add rooms fetching according to type

--- app/src/main/java/org/futo/circles/feature/room/select/SelectRoomsDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/room/select/SelectRoomsDataSource.kt
@@ -2,26 +2,25 @@ package org.futo.circles.feature.room.select
 
 import androidx.lifecycle.MutableLiveData
 import org.futo.circles.mapping.toSelectableRoomListItem
-import org.futo.circles.model.CIRCLE_TAG
-import org.futo.circles.model.SelectableRoomListItem
+import org.futo.circles.model.*
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.room.model.Membership
+import org.matrix.android.sdk.api.session.room.model.RoomSummary
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
-class SelectRoomsDataSource {
+class SelectRoomsDataSource(
+    private val roomType: CircleRoomTypeArg
+) {
 
     private val session by lazy { MatrixSessionProvider.currentSession }
 
     val roomsLiveData = MutableLiveData(getInitialRoomsList())
 
-    private fun getInitialRoomsList(): List<SelectableRoomListItem> =
-        session?.roomService()?.getRoomSummaries(roomSummaryQueryParams {
-            excludeType = null
-        })?.mapNotNull { summary ->
-            if (summary.hasTag(CIRCLE_TAG) && summary.membership == Membership.JOIN)
-                summary.toSelectableRoomListItem()
-            else null
-        } ?: emptyList()
+    private fun getInitialRoomsList(): List<SelectableRoomListItem> = when (roomType) {
+        CircleRoomTypeArg.Circle -> getRooms { it.hasTag(CIRCLE_TAG) && it.membership == Membership.JOIN }
+        CircleRoomTypeArg.Group -> getRooms { (it.roomType == GROUP_TYPE && it.membership == Membership.JOIN) }
+        CircleRoomTypeArg.Photo -> getRooms { (it.roomType == GALLERY_TYPE && it.membership == Membership.JOIN) }
+    }
 
     fun getSelectedRooms() = roomsLiveData.value?.filter { it.isSelected } ?: emptyList()
 
@@ -31,4 +30,13 @@ class SelectRoomsDataSource {
         }
         roomsLiveData.postValue(newList)
     }
+
+    private fun getRooms(filter: (summary: RoomSummary) -> Boolean) =
+        session?.roomService()?.getRoomSummaries(roomSummaryQueryParams {
+            excludeType = null
+        })?.mapNotNull { summary ->
+            if (filter(summary)) summary.toSelectableRoomListItem()
+            else null
+        } ?: emptyList()
+
 }
