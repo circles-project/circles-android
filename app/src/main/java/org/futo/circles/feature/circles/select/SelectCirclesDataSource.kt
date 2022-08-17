85c9520959c17b374f252027e69147094077f0a3
Taras <tarassmakula@gmail.com>
2022-08-17 14:55:13 +0300

    Create select circles fragment

---
+++ app/src/main/java/org/futo/circles/feature/circles/select/SelectCirclesDataSource.kt
@@ -0,0 +1,34 @@
+package org.futo.circles.feature.circles.select
+
+import androidx.lifecycle.MutableLiveData
+import org.futo.circles.mapping.toSelectableRoomListItem
+import org.futo.circles.model.CIRCLE_TAG
+import org.futo.circles.model.SelectableRoomListItem
+import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.session.room.model.Membership
+import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
+
+class SelectCirclesDataSource {
+
+    private val session by lazy { MatrixSessionProvider.currentSession }
+
+    val circlesLiveData = MutableLiveData(getInitialCirclesList())
+
+    private fun getInitialCirclesList(): List<SelectableRoomListItem> =
+        session?.roomService()?.getRoomSummaries(roomSummaryQueryParams {
+            excludeType = null
+        })?.mapNotNull { summary ->
+            if (summary.hasTag(CIRCLE_TAG) && summary.membership == Membership.JOIN)
+                summary.toSelectableRoomListItem()
+            else null
+        } ?: emptyList()
+
+    fun getSelectedCircles() = circlesLiveData.value?.filter { it.isSelected } ?: emptyList()
+
+    fun toggleCircleSelect(circle: SelectableRoomListItem) {
+        val newList = circlesLiveData.value?.toMutableList()?.map {
+            if (it.id == circle.id) it.copy(isSelected = !it.isSelected) else it
+        }
+        circlesLiveData.postValue(newList)
+    }
+}
---
+++ app/src/main/java/org/futo/circles/feature/circles/select/SelectCirclesFragment.kt
@@ -0,0 +1,63 @@
+package org.futo.circles.feature.circles.select
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import by.kirich1409.viewbindingdelegate.viewBinding
+import org.futo.circles.R
+import org.futo.circles.core.SelectRoomsListener
+import org.futo.circles.databinding.FragmentSelectCirclesBinding
+import org.futo.circles.extensions.observeData
+import org.futo.circles.extensions.setIsVisible
+import org.futo.circles.feature.circles.select.list.SelectCirclesAdapter
+import org.futo.circles.feature.circles.select.list.SelectedChipsCirclesAdapter
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class SelectCirclesFragment : Fragment(R.layout.fragment_select_circles) {
+
+    private val viewModel by viewModel<SelectCirclesViewModel>()
+    private val binding by viewBinding(FragmentSelectCirclesBinding::bind)
+
+    private val selectCirclesAdapter by lazy { SelectCirclesAdapter(viewModel::onCircleSelected) }
+    private val selectedCircleAdapter by lazy { SelectedChipsCirclesAdapter(viewModel::onCircleSelected) }
+
+    private var selectRoomsListener: SelectRoomsListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        selectRoomsListener = (parentFragment as? SelectRoomsListener)
+        if (selectRoomsListener == null)
+            selectRoomsListener = activity as? SelectRoomsListener
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObservers()
+    }
+
+    fun getSelectedCircles() = viewModel.getSelectedCircles()
+
+    private fun setupViews() {
+        with(binding) {
+            rvCircles.apply {
+                addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                adapter = selectCirclesAdapter
+            }
+            rvSelectedCircles.adapter = selectedCircleAdapter
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.circlesLiveData.observeData(this) { items ->
+            selectCirclesAdapter.submitList(items)
+            val selectedCircles = viewModel.getSelectedCircles()
+            selectedCircleAdapter.submitList(selectedCircles)
+            binding.selectedCircleDivider.setIsVisible(selectedCircles.isNotEmpty())
+            selectRoomsListener?.onRoomsSelected(selectedCircles)
+        }
+    }
+}
