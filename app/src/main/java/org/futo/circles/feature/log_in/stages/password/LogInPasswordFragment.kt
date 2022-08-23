eb85154ba4ed7824e94f2ed7b9629071961ad09f
Taras <tarassmakula@gmail.com>
2022-08-23 18:23:07 +0300

    Create login password fragment draft

---
+++ app/src/main/java/org/futo/circles/feature/log_in/stages/password/LogInPasswordFragment.kt
@@ -0,0 +1,7 @@
+package org.futo.circles.feature.log_in.stages.password
+
+import androidx.fragment.app.Fragment
+import org.futo.circles.R
+
+class LogInPasswordFragment: Fragment(R.layout.fragment_login_stages)  {
+}
