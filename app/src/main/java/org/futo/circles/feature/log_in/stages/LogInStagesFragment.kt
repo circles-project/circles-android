0a9f5bdc5b57060bd01827f6a5d7f3a982eb188b
Taras <tarassmakula@gmail.com>
2022-08-23 18:22:47 +0300

    Create login stages fragment

---
+++ app/src/main/java/org/futo/circles/feature/log_in/stages/LogInStagesFragment.kt
@@ -0,0 +1,7 @@
+package org.futo.circles.feature.log_in.stages
+
+import androidx.fragment.app.Fragment
+import org.futo.circles.R
+
+class LogInStagesFragment : Fragment(R.layout.fragment_login_stages) {
+}
