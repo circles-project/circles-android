ec530bc0d5cbd18f386228887168d2eeca8c9960
Taras <tarassmakula@gmail.com>
2022-09-15 16:34:08 +0300

    Add status message

--- app/src/main/java/org/futo/circles/view/PollContentView.kt
+++ app/src/main/java/org/futo/circles/view/PollContentView.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import org.futo.circles.R
 import org.futo.circles.databinding.ViewPollContentBinding
 import org.futo.circles.model.PollContent
+import org.futo.circles.model.PollState
 
 class PollContentView(
     context: Context,
@@ -25,11 +27,32 @@ class PollContentView(
             tvPollQuestions.text = poll.question
             lvOptionsContainer.removeAllViews()
             poll.options.forEach {
-                lvOptionsContainer.addView(PollOptionView(context).apply { setup(it) })
+                val layoutParams =
+                    LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+                layoutParams.setMargins(0, 8, 0, 0)
+                lvOptionsContainer.addView(
+                    PollOptionView(context).apply { setup(it) },
+                    layoutParams
+                )
             }
-            tvPollStatus.text = createPollStatusMessage()
+            tvPollStatus.text = createPollStatusMessage(poll.state, poll.totalVotes)
         }
     }
 
-    private fun createPollStatusMessage(): String = "Todo"
+    private fun createPollStatusMessage(
+        pollState: PollState,
+        votes: Int
+    ): String = when {
+        pollState == PollState.Ended -> context.resources.getQuantityString(
+            R.plurals.poll_total_vote_count_after_ended, votes, votes
+        )
+        pollState == PollState.Undisclosed -> ""
+        pollState == PollState.Voted -> context.resources.getQuantityString(
+            R.plurals.poll_total_vote_count_before_ended_and_voted, votes, votes
+        )
+        votes == 0 -> context.getString(R.string.poll_no_votes_cast)
+        else -> context.resources.getQuantityString(
+            R.plurals.poll_total_vote_count_before_ended_and_not_voted, votes, votes
+        )
+    }
 }
