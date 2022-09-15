8402d28f73e4943cc03573193d3440451ae34783
Taras <tarassmakula@gmail.com>
2022-09-15 17:28:57 +0300

    Bind poll option

--- app/src/main/java/org/futo/circles/view/PollOptionView.kt
+++ app/src/main/java/org/futo/circles/view/PollOptionView.kt
@@ -3,21 +3,22 @@ package org.futo.circles.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.FrameLayout
 import org.futo.circles.R
 import org.futo.circles.databinding.ViewPollOptionBinding
 import org.futo.circles.extensions.setIsVisible
 import org.futo.circles.model.PollOption
+import org.futo.circles.model.PollState
 
 class PollOptionView(
     context: Context,
     attrs: AttributeSet? = null,
-) : ConstraintLayout(context, attrs) {
+) : FrameLayout(context, attrs) {
 
     private val binding =
         ViewPollOptionBinding.inflate(LayoutInflater.from(context), this)
 
-    fun setup(option: PollOption) {
+    fun setup(option: PollOption, pollState: PollState) {
         with(binding) {
             tvOptionQuestion.text = option.optionAnswer
             ivWinner.setIsVisible(option.isWinner)
@@ -25,13 +26,26 @@ class PollOptionView(
                 R.plurals.votes, option.voteCount, option.voteCount
             )
             horizontalProgress.progress = option.votePercentage
-            if(option.isMyVote){
-
-            }else{
-
-            }
         }
+        setOptionBackground(pollState, option.isWinner, option.isMyVote)
+        setCheckIcon(pollState, option.isMyVote)
+    }
 
+    private fun setCheckIcon(pollState: PollState, isMyVote: Boolean) {
+        binding.ivCheck.setIsVisible(pollState != PollState.Ended)
+        binding.ivCheck.setImageResource(
+            if (isMyVote) R.drawable.ic_check_circle else R.drawable.ic_unselected
+        )
+    }
+
+    private fun setOptionBackground(pollState: PollState, isWinner: Boolean, isMyVote: Boolean) {
+        binding.root.setBackgroundResource(
+            if (pollState == PollState.Ended) {
+                if (isWinner) R.drawable.bg_border_selected else R.drawable.bg_border
+            } else {
+                if (isMyVote) R.drawable.bg_border_selected else R.drawable.bg_border
+            }
+        )
     }
 
 }
