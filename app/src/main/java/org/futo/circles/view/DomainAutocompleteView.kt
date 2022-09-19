4e982243c00c41715e3248c8ce170d850f11d84a
Taras <tarassmakula@gmail.com>
2022-09-19 14:32:30 +0300

    Create domain autocomplete

---
+++ app/src/main/java/org/futo/circles/view/DomainAutocompleteView.kt
@@ -0,0 +1,28 @@
+package org.futo.circles.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+
+@SuppressLint("ClickableViewAccessibility")
+class DomainAutocompleteView(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : AppCompatAutoCompleteTextView(context, attrs) {
+
+    init {
+        setOnTouchListener { v, _ ->
+            postDelayed( { if (hasFocus()) showDropDown() },100)
+            return@setOnTouchListener false
+        }
+    }
+
+    override fun enoughToFilter(): Boolean {
+        return true
+    }
+
+    override fun performFiltering(text: CharSequence?, keyCode: Int) {
+        super.performFiltering("", keyCode)
+    }
+}
