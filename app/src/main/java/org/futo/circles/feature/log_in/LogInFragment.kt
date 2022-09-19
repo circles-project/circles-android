f9563bae51fc089efc5ad17e82bb1c4c14881c1a
Taras <tarassmakula@gmail.com>
2022-09-19 14:32:43 +0300

    Build new user id input

--- app/src/main/java/org/futo/circles/feature/log_in/LogInFragment.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/LogInFragment.kt
@@ -2,9 +2,12 @@ package org.futo.circles.feature.log_in
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import org.futo.circles.BuildConfig
 import org.futo.circles.R
 import org.futo.circles.core.fragment.HasLoadingState
 import org.futo.circles.databinding.FragmentLogInBinding
@@ -21,8 +24,25 @@ class LogInFragment : Fragment(R.layout.fragment_log_in), HasLoadingState {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupViews()
         setOnClickActions()
         setupObservers()
+    }
+
+    private fun setupViews() {
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            listOf(BuildConfig.US_SERVER_DOMAIN, BuildConfig.EU_SERVER_DOMAIN)
+        )
+        binding.tvDomain.apply {
+            setAdapter(adapter)
+            onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+                binding.tilDomain.hint = if (hasFocus) getString(R.string.domain)
+                else BuildConfig.US_SERVER_DOMAIN
+            }
+        }
+        binding.tilDomain.hint = BuildConfig.US_SERVER_DOMAIN
     }
 
     private fun setupObservers() {
@@ -40,8 +60,15 @@ class LogInFragment : Fragment(R.layout.fragment_log_in), HasLoadingState {
             }
             btnLogin.setOnClickListener {
                 startLoading(btnLogin)
-                viewModel.startLogInFlow(tilUserName.getText())
+                viewModel.startLogInFlow(buildUserIdFromInputs())
             }
         }
+    }
+
+    private fun buildUserIdFromInputs(): String {
+        val userName = binding.tilUserName.getText()
+        val domain = binding.tvDomain.text.toString().takeIf { it.isNotEmpty() }
+            ?: BuildConfig.US_SERVER_DOMAIN
+        return "@$userName:$domain"
     }
 }
