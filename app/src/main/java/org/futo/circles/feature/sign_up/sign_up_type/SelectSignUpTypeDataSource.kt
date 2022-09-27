932646673c34097ef61212156e85c818053e7685
Taras <tarassmakula@gmail.com>
2022-09-27 14:11:39 +0300

    Add mapping for new stages

--- app/src/main/java/org/futo/circles/feature/sign_up/sign_up_type/SelectSignUpTypeDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/sign_up/sign_up_type/SelectSignUpTypeDataSource.kt
@@ -2,11 +2,15 @@ package org.futo.circles.feature.sign_up.sign_up_type
 
 import android.content.Context
 import org.futo.circles.R
+import org.futo.circles.core.REGISTRATION_EMAIL_REQUEST_TOKEN_TYPE
+import org.futo.circles.core.REGISTRATION_EMAIL_STAGE_KEY_PREFIX
+import org.futo.circles.core.REGISTRATION_EMAIL_SUBMIT_TOKEN_TYPE
 import org.futo.circles.core.utils.HomeServerUtils.buildHomeServerConfigFromDomain
 import org.futo.circles.extensions.createResult
 import org.futo.circles.feature.sign_up.SignUpDataSource
 import org.futo.circles.provider.MatrixInstanceProvider
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
+import org.matrix.android.sdk.api.auth.registration.Stage
 
 class SelectSignUpTypeDataSource(
     private val context: Context,
@@ -37,13 +41,28 @@ class SelectSignUpTypeDataSource(
                 as? RegistrationResult.FlowResponse)
             ?.let {
                 signUpDataSource.startSignUpStages(
-                    it.flowResult.missingStages,
+                    prepareStagesList(it.flowResult.missingStages),
                     name,
                     homeServerUrl,
                     isSubscription,
                     subscriptionReceipt
                 )
             }
+    }
+
+    private fun prepareStagesList(stages: List<Stage>): List<Stage> {
+        val requestTokenStage =
+            stages.firstOrNull { (it as? Stage.Other)?.type == REGISTRATION_EMAIL_REQUEST_TOKEN_TYPE }
+        val submitTokenStage =
+            stages.firstOrNull { (it as? Stage.Other)?.type == REGISTRATION_EMAIL_SUBMIT_TOKEN_TYPE }
+
+        return if (requestTokenStage != null && submitTokenStage != null)
+            stages.toMutableList().apply {
+                val position = stages.indexOf(requestTokenStage)
+                add(position, Stage.Other(true, REGISTRATION_EMAIL_STAGE_KEY_PREFIX, null))
+                remove(requestTokenStage)
+                remove(submitTokenStage)
+            } else stages
     }
 
 }
