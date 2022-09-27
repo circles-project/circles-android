17411436c8c99e6d3c1f7fc84dbf0e4823b87289
Taras <tarassmakula@gmail.com>
2022-09-27 14:11:17 +0300

    Add new stages constants

--- app/src/main/java/org/futo/circles/extensions/FileExtensions.kt
+++ app/src/main/java/org/futo/circles/extensions/FileExtensions.kt
@@ -3,9 +3,9 @@ package org.futo.circles.extensions
 import android.content.Context
 import android.net.Uri
 import androidx.core.content.FileProvider
-import org.futo.circles.core.FILE_PROVIDER_AUTHORITY_PREFIX
+import org.futo.circles.core.FILE_PROVIDER_AUTHORITY_EXTENSION
 import java.io.File
 
 fun File.getUri(context: Context): Uri = FileProvider.getUriForFile(
-    context, context.packageName + FILE_PROVIDER_AUTHORITY_PREFIX, this
+    context, context.packageName + FILE_PROVIDER_AUTHORITY_EXTENSION, this
 )
