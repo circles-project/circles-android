4bb0b147dd64d2482d714ddc1ca095159f35c82d
Taras <tarassmakula@gmail.com>
2022-09-30 14:20:50 +0300

    Remove enroll email mapping

--- app/src/main/java/org/futo/circles/feature/sign_up/sign_up_type/SelectSignUpTypeDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/sign_up/sign_up_type/SelectSignUpTypeDataSource.kt
@@ -1,14 +1,13 @@
 package org.futo.circles.feature.sign_up.sign_up_type
 
 import android.content.Context
-import kotlinx.coroutines.delay
 import org.futo.circles.R
-import org.futo.circles.core.*
+import org.futo.circles.core.REGISTRATION_SUBSCRIPTION_TYPE
+import org.futo.circles.core.REGISTRATION_TOKEN_TYPE
 import org.futo.circles.core.utils.HomeServerUtils.buildHomeServerConfigFromDomain
 import org.futo.circles.extensions.createResult
 import org.futo.circles.feature.sign_up.SignUpDataSource
 import org.futo.circles.provider.MatrixInstanceProvider
-import org.matrix.android.sdk.api.auth.registration.RegistrationResult
 import org.matrix.android.sdk.api.auth.registration.Stage
 
 class SelectSignUpTypeDataSource(
@@ -37,38 +36,15 @@ class SelectSignUpTypeDataSource(
             subscriptionReceipt
         )
     }
-    
-    private fun prepareStagesList(isSubscription: Boolean, flows: List<List<Stage>>): List<Stage> {
-        val flow = if (isSubscription) {
+
+    private fun prepareStagesList(isSubscription: Boolean, flows: List<List<Stage>>): List<Stage> =
+        if (isSubscription) {
             flows.firstOrNull {
-                (it.firstOrNull() as? Stage.Other)?.type?.endsWith(
-                    REGISTRATION_SUBSCRIPTION_KEY_EXTENSION
-                ) == true
+                (it.firstOrNull() as? Stage.Other)?.type == REGISTRATION_SUBSCRIPTION_TYPE
             }
         } else {
             flows.firstOrNull {
-                (it.firstOrNull() as? Stage.Other)?.type?.endsWith(
-                    REGISTRATION_TOKEN_KEY_EXTENSION
-                ) == true
+                (it.firstOrNull() as? Stage.Other)?.type == REGISTRATION_TOKEN_TYPE
             }
         } ?: throw IllegalArgumentException(context.getString(R.string.wrong_signup_config))
-
-        return mapEmailStages(flow)
-    }
-
-    private fun mapEmailStages(stages: List<Stage>): List<Stage> {
-        val requestTokenStage =
-            stages.firstOrNull { (it as? Stage.Other)?.type == REGISTRATION_EMAIL_REQUEST_TOKEN_TYPE }
-        val submitTokenStage =
-            stages.firstOrNull { (it as? Stage.Other)?.type == REGISTRATION_EMAIL_SUBMIT_TOKEN_TYPE }
-
-        return if (requestTokenStage != null && submitTokenStage != null)
-            stages.toMutableList().apply {
-                val position = stages.indexOf(requestTokenStage)
-                add(position, Stage.Other(true, REGISTRATION_EMAIL_STAGE_KEY_PREFIX, null))
-                remove(requestTokenStage)
-                remove(submitTokenStage)
-            } else stages
-    }
-
 }
