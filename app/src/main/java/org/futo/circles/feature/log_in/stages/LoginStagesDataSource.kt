62b62359cb04b56fd3d009b1dd383c68d7f0643f
Taras Smakula <tarassmakula@gmail.com>
2022-10-03 17:02:17 +0300

    Change login stages implementation to support flow

--- app/src/main/java/org/futo/circles/feature/log_in/stages/LoginStagesDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/stages/LoginStagesDataSource.kt
@@ -4,17 +4,19 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import org.futo.circles.R
-import org.futo.circles.core.SingleEventLiveData
+import org.futo.circles.core.*
 import org.futo.circles.core.matrix.pass_phrase.restore.RestoreBackupDataSource
 import org.futo.circles.core.matrix.room.CoreSpacesTreeBuilder
 import org.futo.circles.extensions.Response
 import org.futo.circles.extensions.createResult
+import org.futo.circles.feature.sign_up.SignUpNavigationEvents
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
+import org.matrix.android.sdk.api.auth.registration.Stage
 import org.matrix.android.sdk.api.crypto.MXCRYPTO_ALGORITHM_MEGOLM_BACKUP
 import org.matrix.android.sdk.api.session.Session
 
-enum class LoginNavigationEvent { Main, SetupCircles, PassPhrase, Password, Terms }
+enum class LoginNavigationEvent { Main, SetupCircles, PassPhrase, Password, Terms, DirectPassword }
 
 class LoginStagesDataSource(
     private val context: Context,
@@ -30,26 +32,29 @@ class LoginStagesDataSource(
     var userName: String = ""
         private set
 
-    private val stagesToComplete = mutableListOf<String>()
-    var currentStage: String? = null
+    private val stagesToComplete = mutableListOf<Stage>()
+    var currentStage: Stage? = null
         private set
 
     private var userPassword: String = ""
 
     fun startLoginStages(
-        supportedLoginTypes: List<String>,
+        loginStages: List<Stage>,
         userName: String
     ) {
         this.userName = userName
         userPassword = ""
         currentStage = null
         stagesToComplete.clear()
-        stagesToComplete.addAll(supportedLoginTypes)
+        stagesToComplete.addAll(loginStages)
         navigateToNextStage()
     }
 
-    suspend fun stageCompleted(result: RegistrationResult?, password: String? = null) {
-        password?.let { userPassword = it }
+    fun setPassword(password: String) {
+        userPassword = password
+    }
+
+    suspend fun stageCompleted(result: RegistrationResult) {
         (result as? RegistrationResult.Success)?.let {
             finishLogin(it.session)
         } ?: navigateToNextStage()
@@ -63,21 +68,18 @@ class LoginStagesDataSource(
     private fun getCurrentStageIndex() =
         stagesToComplete.indexOf(currentStage).takeIf { it != -1 } ?: 0
 
+    private fun setNextStage() {
+        currentStage = currentStage?.let {
+            stagesToComplete.getOrNull(getCurrentStageIndex() + 1)
+        } ?: stagesToComplete.firstOrNull()
+    }
+
     private fun navigateToNextStage() {
-        //TODO("Change for real stages implementation")
-//        val stage = currentStage?.let {
-//            stagesToComplete.getOrNull(getCurrentStageIndex() + 1)
-//        } ?: stagesToComplete.firstOrNull()
-        val stage = stagesToComplete.firstOrNull { it.endsWith("password") }
-
-        currentStage = stage
-
-        val event = if (stage?.endsWith("password") == true) {
-            LoginNavigationEvent.Password
-        } else if (stage?.endsWith("terms") == true) {
-            LoginNavigationEvent.Terms
-        } else {
-            throw IllegalArgumentException(
+        setNextStage()
+        val event = when (val stage = currentStage) {
+            is Stage.Terms -> LoginNavigationEvent.Terms
+            is Stage.Other -> handleStageOther(stage.type)
+            else -> throw IllegalArgumentException(
                 context.getString(R.string.not_supported_stage_format, stage.toString())
             )
         }
@@ -85,11 +87,17 @@ class LoginStagesDataSource(
         updatePageSubtitle()
     }
 
+    private fun handleStageOther(type: String): LoginNavigationEvent = when (type) {
+        LOGIN_PASSWORD_TYPE -> LoginNavigationEvent.Password
+        DIRECT_LOGIN_PASSWORD_TYPE -> LoginNavigationEvent.DirectPassword
+        else -> throw IllegalArgumentException(
+            context.getString(R.string.not_supported_stage_format, type)
+        )
+    }
+
     private fun updatePageSubtitle() {
-        //TODO("Change for real stages size")
-        val size = 1
-        //val number = getCurrentStageIndex() + 1
-        val number = 1
+        val size = stagesToComplete.size
+        val number = getCurrentStageIndex() + 1
         val subtitle = context.getString(R.string.sign_up_stage_subtitle_format, number, size)
         subtitleLiveData.postValue(subtitle)
     }
