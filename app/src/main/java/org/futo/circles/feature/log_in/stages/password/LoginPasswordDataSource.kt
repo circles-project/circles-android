15c1724f63b8360047d5340f029cdea3fe2f40f7
Taras Smakula <tarassmakula@gmail.com>
2022-10-03 17:04:35 +0300

    Implement password stage for login

--- app/src/main/java/org/futo/circles/feature/log_in/stages/password/LoginPasswordDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/stages/password/LoginPasswordDataSource.kt
@@ -1,40 +1,53 @@
 package org.futo.circles.feature.log_in.stages.password
 
-import android.content.Context
-import org.futo.circles.R
+import org.futo.circles.core.LOGIN_PASSWORD_TYPE
+import org.futo.circles.core.LOGIN_PASSWORD_USER_ID_TYPE
+import org.futo.circles.core.TYPE_PARAM_KEY
 import org.futo.circles.core.auth.PasswordDataSource
 import org.futo.circles.extensions.Response
 import org.futo.circles.extensions.createResult
 import org.futo.circles.feature.log_in.stages.LoginStagesDataSource
 import org.futo.circles.provider.MatrixInstanceProvider
-import org.matrix.android.sdk.api.auth.registration.RegistrationResult
+import org.matrix.android.sdk.api.auth.registration.Stage
 
 class LoginPasswordDataSource(
-    private val context: Context,
     private val loginStagesDataSource: LoginStagesDataSource
 ) : PasswordDataSource {
 
-    override fun getMinimumPasswordLength(): Int = 1
+    override fun getMinimumPasswordLength(): Int =
+        ((loginStagesDataSource.currentStage as? Stage.Other)?.params?.getOrDefault(
+            MINIMUM_LENGTH_KEY, 1.0
+        ) as? Double)?.toInt() ?: 1
+
 
     override suspend fun processPasswordStage(password: String): Response<Unit> {
+        val wizard = MatrixInstanceProvider.matrix.authenticationService().getLoginWizard()
         val result = createResult {
-            MatrixInstanceProvider.matrix.authenticationService().getLoginWizard().login(
-                login = loginStagesDataSource.userName,
-                password = password,
-                initialDeviceName = context.getString(
-                    R.string.initial_device_name,
-                    context.getString(R.string.app_name)
+            wizard.loginStageCustom(
+                mapOf(
+                    TYPE_PARAM_KEY to LOGIN_PASSWORD_TYPE,
+                    PASSWORD_PARAM_KEY to password,
+                    IDENTIFIER_PARAM_KEY to mapOf(
+                        USER_PARAM_KEY to loginStagesDataSource.userName,
+                        TYPE_PARAM_KEY to LOGIN_PASSWORD_USER_ID_TYPE
+                    )
                 )
             )
         }
         return when (result) {
             is Response.Success -> {
-                loginStagesDataSource.stageCompleted(
-                    RegistrationResult.Success(result.data), password
-                )
+                loginStagesDataSource.setPassword(password)
+                loginStagesDataSource.stageCompleted(result.data)
                 Response.Success(Unit)
             }
             is Response.Error -> result
         }
+    }
+
+    companion object {
+        private const val PASSWORD_PARAM_KEY = "password"
+        private const val MINIMUM_LENGTH_KEY = "minimum_length"
+        private const val IDENTIFIER_PARAM_KEY = "identifier"
+        private const val USER_PARAM_KEY = "user"
     }
 }
