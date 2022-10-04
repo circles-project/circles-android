a19c45d04b6e3ccf77cc4e4bf402f17b551513ed
Taras Smakula <tarassmakula@gmail.com>
2022-10-04 16:10:02 +0300

    Change to flow in viewmodel

--- app/src/main/java/org/futo/circles/feature/room/select/SelectRoomsViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/room/select/SelectRoomsViewModel.kt
@@ -1,13 +1,14 @@
 package org.futo.circles.feature.room.select
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import org.futo.circles.model.SelectableRoomListItem
 
 class SelectRoomsViewModel(
     private val dataSource: SelectRoomsDataSource
 ) : ViewModel() {
 
-    val roomsLiveData = dataSource.roomsLiveData
+    val roomsLiveData = dataSource.roomsFlow.asLiveData()
 
     fun getSelectedRooms() = dataSource.getSelectedRooms()
 
