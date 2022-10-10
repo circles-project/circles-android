9027c449c096f4e1d655eb7419dd1a250ca4d780
Taras Smakula <tarassmakula@gmail.com>
2022-10-10 12:49:35 +0300

    Update old login stage datasources to use perforLoginStage

--- app/src/main/java/org/futo/circles/feature/log_in/stages/terms/LoginAcceptTermsDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/stages/terms/LoginAcceptTermsDataSource.kt
@@ -3,10 +3,8 @@ package org.futo.circles.feature.log_in.stages.terms
 import org.futo.circles.core.TYPE_PARAM_KEY
 import org.futo.circles.core.auth.BaseAcceptTermsDataSource
 import org.futo.circles.extensions.Response
-import org.futo.circles.extensions.createResult
 import org.futo.circles.extensions.toTermsListItems
 import org.futo.circles.feature.log_in.stages.LoginStagesDataSource
-import org.futo.circles.provider.MatrixInstanceProvider
 import org.matrix.android.sdk.api.auth.data.LoginFlowTypes
 import org.matrix.android.sdk.api.auth.registration.Stage
 
@@ -15,15 +13,11 @@ class LoginAcceptTermsDataSource(
 ) : BaseAcceptTermsDataSource() {
 
     override suspend fun acceptTerms(): Response<Unit> {
-        val wizard = MatrixInstanceProvider.matrix.authenticationService().getLoginWizard()
-        val result = createResult {
-            wizard.loginStageCustom(mapOf(TYPE_PARAM_KEY to LoginFlowTypes.TERMS))
-        }
+        val result = loginStagesDataSource.performLoginStage(
+            mapOf(TYPE_PARAM_KEY to LoginFlowTypes.TERMS)
+        )
         return when (result) {
-            is Response.Success -> {
-                loginStagesDataSource.stageCompleted(result.data)
-                Response.Success(Unit)
-            }
+            is Response.Success -> Response.Success(Unit)
             is Response.Error -> result
         }
     }
