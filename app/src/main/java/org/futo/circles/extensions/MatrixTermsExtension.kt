ede013836cdd2d4b985a421746a5021938cdc7d0
Taras Smakula <tarassmakula@gmail.com>
2022-10-12 13:43:44 +0300

    Update terms list parser

--- app/src/main/java/org/futo/circles/extensions/MatrixTermsExtension.kt
+++ app/src/main/java/org/futo/circles/extensions/MatrixTermsExtension.kt
@@ -2,15 +2,30 @@ package org.futo.circles.extensions
 
 
 import org.futo.circles.model.TermsListItem
+import org.matrix.android.sdk.api.auth.data.LocalizedFlowDataLoginTerms
 import org.matrix.android.sdk.api.auth.registration.TermPolicies
-import org.matrix.android.sdk.api.auth.toLocalizedLoginTerms
 
 fun TermPolicies.toTermsListItems() =
-    toLocalizedLoginTerms("en").mapIndexed { i, item ->
+    toLoginTerms("en").mapIndexed { i, item ->
         TermsListItem(
             i,
             item.localizedName ?: item.policyName ?: "",
             item.localizedUrl ?: ""
         )
     }
+
+fun TermPolicies.toLoginTerms(
+    userLanguage: String = "en"
+): List<LocalizedFlowDataLoginTerms> =
+    (get("policies") as? ArrayList<*>)?.mapNotNull { it ->
+        val policy = (it as? Map<*, *>) ?: return@mapNotNull null
+        val policyData = (policy[userLanguage] as? Map<*, *>) ?: return@mapNotNull null
+        LocalizedFlowDataLoginTerms(
+            policyName = policy["name"]?.toString(),
+            version = policy["version"]?.toString(),
+            localizedUrl = policyData["url"]?.toString(),
+            localizedName = policyData["name"]?.toString()
+        )
+    } ?: emptyList()
+
 
