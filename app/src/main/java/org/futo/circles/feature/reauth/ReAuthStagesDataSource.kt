40797370fbb406e1807961d369e91a3861b7c0ce
Taras Smakula <tarassmakula@gmail.com>
2022-10-18 19:00:18 +0300

    Implement ReAuthStagesDataSource

--- app/src/main/java/org/futo/circles/feature/reauth/ReAuthStagesDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/reauth/ReAuthStagesDataSource.kt
@@ -1,40 +1,54 @@
 package org.futo.circles.feature.reauth
 
 import android.content.Context
+import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.core.auth.BaseLoginStagesDataSource
 import org.futo.circles.extensions.Response
+import org.futo.circles.model.CustomUIAuth
+import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.auth.UIABaseAuth
+import org.matrix.android.sdk.api.auth.registration.FlowResult
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
 import org.matrix.android.sdk.api.auth.registration.Stage
-import org.matrix.android.sdk.api.session.Session
 import org.matrix.android.sdk.api.util.JsonDict
 import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
 
 class ReAuthStagesDataSource(
     context: Context,
 ) : BaseLoginStagesDataSource(context) {
 
+    val finishReAuthEventLiveData = SingleEventLiveData<Unit>()
     private var authPromise: Continuation<UIABaseAuth>? = null
+    private var sessionId: String = ""
 
-    override fun startLoginStages(
+    fun startReAuthStages(
+        session: String,
         loginStages: List<Stage>,
-        name: String,
-        serverDomain: String,
         promise: Continuation<UIABaseAuth>?
     ) {
+        sessionId = session
         authPromise = promise
-        super.startLoginStages(loginStages, name, serverDomain, promise)
+        val userId = MatrixSessionProvider.currentSession?.myUserId ?: ""
+        val domain = userId.substringAfter(":")
+        val name = userId.substringAfter("@").substringAfter(":")
+        super.startLoginStages(loginStages, name, domain)
     }
 
     override suspend fun performLoginStage(
         authParams: JsonDict,
         password: String?
     ): Response<RegistrationResult> {
-        TODO("Not yet implemented")
+        authPromise?.resume(CustomUIAuth(sessionId, authParams))
+        return Response.Success(
+            RegistrationResult.FlowResponse(
+                FlowResult(emptyList(), emptyList())
+            )
+        )
     }
 
-    override suspend fun finishLogin(session: Session, password: String?) {
-        TODO("Not yet implemented")
+    fun handleNextStage() {
+        if (stagesToComplete.isEmpty()) finishReAuthEventLiveData.postValue(Unit)
+        else navigateToNextStage()
     }
-
 }
