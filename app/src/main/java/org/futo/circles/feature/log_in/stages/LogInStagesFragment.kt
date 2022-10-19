c5866ed8e10de26a034fef6fecb966e5888f8823
Taras Smakula <tarassmakula@gmail.com>
2022-10-19 13:22:10 +0300

    Extend login stages directly from Fragment

--- app/src/main/java/org/futo/circles/feature/log_in/stages/LogInStagesFragment.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/stages/LogInStagesFragment.kt
@@ -1,23 +1,29 @@
 package org.futo.circles.feature.log_in.stages
 
 import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import org.futo.circles.R
-import org.futo.circles.core.auth.BaseLoginStagesFragment
+import org.futo.circles.core.auth.LoginStageNavigationEvent
+import org.futo.circles.core.fragment.BackPressOwner
 import org.futo.circles.core.matrix.pass_phrase.LoadingDialog
-import org.futo.circles.extensions.observeData
-import org.futo.circles.extensions.observeResponse
-import org.futo.circles.extensions.showError
+import org.futo.circles.databinding.FragmentLoginStagesBinding
+import org.futo.circles.extensions.*
 import org.futo.circles.feature.log_in.EnterPassPhraseDialog
 import org.futo.circles.feature.log_in.EnterPassPhraseDialogListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LogInStagesFragment : BaseLoginStagesFragment() {
+class LogInStagesFragment : Fragment(R.layout.fragment_login_stages), BackPressOwner {
 
-    override val viewModel by viewModel<LoginStagesViewModel>()
-    override val isReAuth: Boolean = false
-    override val titleResId = R.string.log_in
+    private val viewModel by viewModel<LoginStagesViewModel>()
+    private val binding by viewBinding(FragmentLoginStagesBinding::bind)
 
     private val restorePassPhraseLoadingDialog by lazy { LoadingDialog(requireContext()) }
     private var enterPassPhraseDialog: EnterPassPhraseDialog? = null
@@ -29,8 +35,31 @@ class LogInStagesFragment : BaseLoginStagesFragment() {
         enterPassPhraseDialog?.selectFile(uri)
     }
 
-    override fun setupObservers() {
-        super.setupObservers()
+    private val childNavHostFragment by lazy {
+        childFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
+        binding.toolbar.title = getString(R.string.log_in)
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.loginStageNavigationLiveData.observeData(this) { event ->
+            val id = when (event) {
+                LoginStageNavigationEvent.DirectPassword -> R.id.to_direct_login
+                LoginStageNavigationEvent.Password -> R.id.to_password
+                LoginStageNavigationEvent.Terms -> R.id.to_acceptTerms
+                LoginStageNavigationEvent.BSspeke -> R.id.to_bsspeke
+                else -> throw IllegalArgumentException(getString(R.string.not_supported_navigation_event))
+            }
+            binding.navHostFragment.findNavController().navigate(id)
+        }
+        viewModel.subtitleLiveData.observeData(this) {
+            binding.toolbar.subtitle = it
+        }
         viewModel.restoreKeysLiveData.observeResponse(
             this,
             error = {
@@ -84,6 +113,23 @@ class LogInStagesFragment : BaseLoginStagesFragment() {
 
     private fun navigateToSetupCircles() {
         findNavController().navigate(LogInStagesFragmentDirections.toSetupCirclesFragment())
+    }
+
+    private fun showDiscardDialog() {
+        showDialog(
+            titleResIdRes = R.string.discard_current_login_progress,
+            negativeButtonVisible = true,
+            positiveAction = { findNavController().popBackStack() })
+    }
+
+    override fun onChildBackPress(callback: OnBackPressedCallback) {
+        val includedFragmentsManager = childNavHostFragment.childFragmentManager
+        if (includedFragmentsManager.backStackEntryCount == 0) {
+            callback.remove()
+            onBackPressed()
+        } else {
+            showDiscardDialog()
+        }
     }
 
     companion object {
