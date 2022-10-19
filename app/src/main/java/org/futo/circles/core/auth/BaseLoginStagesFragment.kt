4b515af7e34ad29f972095a5786052c6f44ec71c
Taras Smakula <tarassmakula@gmail.com>
2022-10-19 11:46:08 +0300

    Add reauth anvigation

--- app/src/main/java/org/futo/circles/core/auth/BaseLoginStagesFragment.kt
+++ app/src/main/java/org/futo/circles/core/auth/BaseLoginStagesFragment.kt
@@ -19,7 +19,7 @@ abstract class BaseLoginStagesFragment : Fragment(R.layout.fragment_login_stages
 
     abstract val viewModel: BaseLoginStagesViewModel
     abstract val isReAuth: Boolean
-    abstract val title: String
+    abstract val titleResId: Int
     protected val binding by viewBinding(FragmentLoginStagesBinding::bind)
 
     private val childNavHostFragment by lazy {
@@ -29,29 +29,25 @@ abstract class BaseLoginStagesFragment : Fragment(R.layout.fragment_login_stages
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbar.setNavigationOnClickListener { onBackPressed() }
-        binding.toolbar.title = title
+        binding.toolbar.title = getString(titleResId)
         setupObservers()
     }
 
     open fun setupObservers() {
         viewModel.loginStageNavigationLiveData.observeData(this) { event ->
-            when (event) {
-                LoginStageNavigationEvent.DirectPassword -> binding.navHostFragment.findNavController()
-                    .navigate(R.id.to_direct_login)
-                LoginStageNavigationEvent.Password -> binding.navHostFragment.findNavController()
-                    .navigate(R.id.to_password)
-                LoginStageNavigationEvent.Terms -> binding.navHostFragment.findNavController()
-                    .navigate(R.id.to_acceptTerms)
-                LoginStageNavigationEvent.BSspeke -> binding.navHostFragment.findNavController()
-                    .navigate(R.id.to_bsspeke)
+            val id = when (event) {
+                LoginStageNavigationEvent.DirectPassword -> R.id.to_direct_login
+                LoginStageNavigationEvent.Password -> if (isReAuth) R.id.to_reAuthPassword else R.id.to_password
+                LoginStageNavigationEvent.Terms -> if (isReAuth) R.id.to_ReAuthAcceptTerms else R.id.to_acceptTerms
+                LoginStageNavigationEvent.BSspeke -> if (isReAuth) R.id.to_reAuthBsSpeke else R.id.to_bsspeke
                 else -> throw IllegalArgumentException(getString(R.string.not_supported_navigation_event))
             }
+            binding.navHostFragment.findNavController().navigate(id)
         }
         viewModel.subtitleLiveData.observeData(this) {
             binding.toolbar.subtitle = it
         }
     }
-
 
     private fun showDiscardDialog() {
         showDialog(
