34432e9790c588fc4960d16115e45ea2c8f14858
Taras Smakula <tarassmakula@gmail.com>
2022-10-26 17:26:49 +0300

    Update init verification logic

--- app/src/main/java/org/futo/circles/feature/settings/active_sessions/verify/VerifySessionViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/settings/active_sessions/verify/VerifySessionViewModel.kt
@@ -1,9 +1,12 @@
 package org.futo.circles.feature.settings.active_sessions.verify
 
 import android.content.Context
-import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.futo.circles.R
+import org.futo.circles.model.QrLoading
+import org.futo.circles.model.QrReady
+import org.futo.circles.model.QrState
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.crypto.verification.*
 
@@ -16,48 +19,64 @@ class VerifySessionViewModel(
         context.getString(R.string.session_is_not_created)
     )
 
+    private val verificationMethods = listOf(
+        VerificationMethod.QR_CODE_SHOW,
+        VerificationMethod.QR_CODE_SCAN
+    )
+
+    val qrStateLiveData: MutableLiveData<QrState> = MutableLiveData(
+        QrLoading(
+            deviceId,
+            session.cryptoService().crossSigningService().isCrossSigningInitialized()
+        )
+    )
+
+    private var qrTransaction: QrCodeVerificationTransaction? = null
+
     init {
-        Log.d("MyLog", "init $deviceId")
         session.cryptoService().verificationService().addListener(this)
+        requestVerification()
     }
 
     override fun verificationRequestCreated(pr: PendingVerificationRequest) {
-        Log.d("MyLog", "v create")
+        verificationRequestUpdated(pr)
     }
 
     override fun verificationRequestUpdated(pr: PendingVerificationRequest) {
-        Log.d("MyLog", "v update")
+        if (pr.isIncoming && !pr.isReady) {
+            session.cryptoService().verificationService()
+                .readyPendingVerification(
+                    verificationMethods,
+                    pr.otherUserId,
+                    pr.transactionId ?: ""
+                )
+        }
     }
 
 
     override fun transactionCreated(tx: VerificationTransaction) {
-        Log.d("MyLog", "tx created $tx")
+        transactionUpdated(tx)
     }
-
 
     override fun transactionUpdated(tx: VerificationTransaction) {
-        Log.d("MyLog", "tx update $tx")
+        qrTransaction = tx as? QrCodeVerificationTransaction
+        qrTransaction?.qrCodeText?.let { qrStateLiveData.postValue(QrReady(deviceId, it)) }
     }
 
-    override fun markedAsManuallyVerified(userId: String, deviceId: String) {
-        Log.d("MyLog", "manually $userId")
-    }
-
-    fun requestVerification() {
-        val a = session.cryptoService().verificationService().requestKeyVerification(
-            listOf(VerificationMethod.QR_CODE_SCAN),
+    private fun requestVerification() {
+        session.cryptoService().verificationService().requestKeyVerification(
+            verificationMethods,
             session.myUserId,
             listOf(deviceId)
-        )
-        session.cryptoService().verificationService().readyPendingVerification(
-            listOf(VerificationMethod.QR_CODE_SCAN),
-            session.myUserId,
-            a.transactionId ?: ""
         )
     }
 
     override fun onCleared() {
         session.cryptoService().verificationService().removeListener(this)
         super.onCleared()
+    }
+
+    fun onQrScanned(data: String) {
+        qrTransaction?.userHasScannedOtherQrCode(data)
     }
 }
