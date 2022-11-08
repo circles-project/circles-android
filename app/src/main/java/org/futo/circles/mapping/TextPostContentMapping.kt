27ee474fb449b037b29f013c0b7cf369b7278fbb
Taras Smakula <tarassmakula@gmail.com>
2022-11-08 14:08:12 +0200

    Fix display latest text post info

--- app/src/main/java/org/futo/circles/mapping/TextPostContentMapping.kt
+++ app/src/main/java/org/futo/circles/mapping/TextPostContentMapping.kt
@@ -1,10 +1,10 @@
 package org.futo.circles.mapping
 
 import org.futo.circles.model.TextContent
-import org.matrix.android.sdk.api.session.events.model.toModel
-import org.matrix.android.sdk.api.session.room.model.message.MessageTextContent
 import org.matrix.android.sdk.api.session.room.timeline.TimelineEvent
+import org.matrix.android.sdk.api.session.room.timeline.getLastMessageContent
+import org.matrix.android.sdk.api.session.room.timeline.getTextDisplayableContent
 
 fun TimelineEvent.toTextContent(): TextContent = TextContent(
-    message = root.getClearContent().toModel<MessageTextContent>()?.body ?: ""
+    message = getLastMessageContent()?.getTextDisplayableContent() ?: ""
 )
