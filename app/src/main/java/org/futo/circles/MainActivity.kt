5103af0937ba5003e7e235490c072bfbc155ebb9
Taras Smakula <tarassmakula@gmail.com>
2022-11-09 15:05:18 +0200

    Resync session after clear cache

--- app/src/main/java/org/futo/circles/MainActivity.kt
+++ app/src/main/java/org/futo/circles/MainActivity.kt
@@ -10,19 +10,42 @@ import org.futo.circles.provider.MatrixSessionProvider
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setInvalidTokenListener()
+        syncSessionIfCashWasCleared()
+    }
+
     // Special action to clear cache and/or clear credentials (Element workaround to clear database)
     fun restartForLogout() {
         MatrixSessionProvider.clearSession()
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        this.startActivity(intent)
+        this.startActivity(createRestartIntent())
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    fun restartForClearCache() {
+        this.startActivity(createRestartIntent().apply { putExtra(IS_CLEAR_CACHE, true) })
+    }
+
+    private fun syncSessionIfCashWasCleared() {
+        val isClearCashReload = intent?.getBooleanExtra(IS_CLEAR_CACHE, false) ?: false
+        if (isClearCashReload) MatrixSessionProvider.currentSession?.syncService()?.startSync(true)
+        intent?.removeExtra(IS_CLEAR_CACHE)
+    }
+
+    private fun createRestartIntent(): Intent {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        return intent
+    }
+
+    private fun setInvalidTokenListener() {
         MatrixSessionListenerProvider.setOnInvalidTokenListener {
             Toast.makeText(this, getString(R.string.you_are_signed_out), Toast.LENGTH_LONG).show()
             restartForLogout()
         }
+    }
+
+    companion object {
+        private const val IS_CLEAR_CACHE = "is_clear_cache"
     }
 }
