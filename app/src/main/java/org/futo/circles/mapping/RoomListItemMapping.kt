7b546b0289d1f2ee0fe1ec492974a1ab3f0e1ed1
Taras Smakula <tarassmakula@gmail.com>
2022-11-11 14:34:13 +0200

    Add user fallback in user datasource

--- app/src/main/java/org/futo/circles/mapping/RoomListItemMapping.kt
+++ app/src/main/java/org/futo/circles/mapping/RoomListItemMapping.kt
@@ -3,7 +3,6 @@ package org.futo.circles.mapping
 import org.futo.circles.extensions.getTimelineRoomFor
 import org.futo.circles.model.*
 import org.futo.circles.provider.MatrixSessionProvider
-import org.matrix.android.sdk.api.session.getUser
 import org.matrix.android.sdk.api.session.getUserOrDefault
 import org.matrix.android.sdk.api.session.room.model.RoomSummary
 
@@ -58,4 +57,5 @@ fun RoomSummary.getFollowersCount(): Int =
     getTimelineRoomFor(roomId)?.roomSummary()?.otherMemberIds?.size ?: 0
 
 fun RoomSummary.getInviterName() =
-    MatrixSessionProvider.currentSession?.getUserOrDefault(inviterId ?: "")?.notEmptyDisplayName() ?: ""
+    MatrixSessionProvider.currentSession?.getUserOrDefault(inviterId ?: "")?.notEmptyDisplayName()
+        ?: ""
