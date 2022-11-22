92f11d723b932849fcda056ad5a03e8e41430679
Taras Smakula <tarassmakula@gmail.com>
2022-11-22 21:32:01 +0200

    Add shouldIndicateAsNew in readInfo mapping

--- app/src/main/java/org/futo/circles/mapping/TimelineEventMapping.kt
+++ app/src/main/java/org/futo/circles/mapping/TimelineEventMapping.kt
@@ -1,5 +1,6 @@
 package org.futo.circles.mapping
 
+import org.futo.circles.extensions.getReadByCountForEvent
 import org.futo.circles.model.*
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.getRoom
@@ -8,12 +9,13 @@ import org.matrix.android.sdk.api.session.room.timeline.getRelationContent
 import org.matrix.android.sdk.api.session.room.timeline.hasBeenEdited
 import org.matrix.android.sdk.api.session.room.timeline.isReply
 
-
 fun TimelineEvent.toPost(
     postContentType: PostContentType,
+    lastReadEventTime: Long = 0L,
     isRepliesVisible: Boolean = false
 ): Post =
-    if (isReply()) toReplyPost(postContentType) else toRootPost(postContentType, isRepliesVisible)
+    if (isReply()) toReplyPost(postContentType, lastReadEventTime)
+    else toRootPost(postContentType, isRepliesVisible, lastReadEventTime)
 
 private fun TimelineEvent.toPostInfo(): PostInfo = PostInfo(
     id = eventId,
@@ -27,28 +29,33 @@ private fun TimelineEvent.toPostInfo(): PostInfo = PostInfo(
     isEdited = hasBeenEdited()
 )
 
-private fun TimelineEvent.toReadInfo(): PostReadInfo = PostReadInfo(
-    isReadByMe = MatrixSessionProvider.currentSession?.getRoom(roomId)?.readService()
-        ?.isEventRead(eventId) ?: false,
-    readByCount = readReceipts.size - 1
+private fun TimelineEvent.toReadInfo(lastReadEventTime: Long): PostReadInfo = PostReadInfo(
+    shouldIndicateAsNew = (root.originServerTs ?: 0L) > lastReadEventTime,
+    readByCount = MatrixSessionProvider.currentSession?.getRoom(roomId)
+        ?.getReadByCountForEvent(eventId) ?: 0
 )
 
-private fun TimelineEvent.toRootPost(postContentType: PostContentType, isRepliesVisible: Boolean) =
+private fun TimelineEvent.toRootPost(
+    postContentType: PostContentType,
+    isRepliesVisible: Boolean,
+    lastReadEventTime: Long
+) =
     RootPost(
         postInfo = toPostInfo(),
         content = toPostContent(postContentType),
         isRepliesVisible = isRepliesVisible,
         sendState = root.sendState,
-        readInfo = toReadInfo()
+        readInfo = toReadInfo(lastReadEventTime)
     )
 
-private fun TimelineEvent.toReplyPost(postContentType: PostContentType) = ReplyPost(
-    postInfo = toPostInfo(),
-    content = toPostContent(postContentType),
-    replyToId = getRelationContent()?.inReplyTo?.eventId ?: "",
-    readInfo = toReadInfo(),
-    sendState = root.sendState
-)
+private fun TimelineEvent.toReplyPost(postContentType: PostContentType, lastReadEventTime: Long) =
+    ReplyPost(
+        postInfo = toPostInfo(),
+        content = toPostContent(postContentType),
+        replyToId = getRelationContent()?.inReplyTo?.eventId ?: "",
+        readInfo = toReadInfo(lastReadEventTime),
+        sendState = root.sendState
+    )
 
 private fun TimelineEvent.toPostContent(postContentType: PostContentType): PostContent =
     when (postContentType) {
