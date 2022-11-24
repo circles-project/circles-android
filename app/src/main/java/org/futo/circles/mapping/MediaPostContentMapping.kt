8bdd33785f8e16ae44a34700b47412fe6af6cd75
Taras Smakula <tarassmakula@gmail.com>
2022-11-24 22:01:15 +0200

    Update mappings

--- app/src/main/java/org/futo/circles/mapping/MediaPostContentMapping.kt
+++ app/src/main/java/org/futo/circles/mapping/MediaPostContentMapping.kt
@@ -3,9 +3,10 @@ package org.futo.circles.mapping
 import com.bumptech.glide.request.target.Target
 import org.futo.circles.core.picker.MediaType
 import org.futo.circles.core.utils.VideoUtils
-import org.futo.circles.model.ImageContent
-import org.futo.circles.model.MediaContentData
-import org.futo.circles.model.VideoContent
+import org.futo.circles.model.MediaContent
+import org.futo.circles.model.MediaContentInfo
+import org.futo.circles.model.MediaFileData
+import org.futo.circles.model.PostContentType
 import org.matrix.android.sdk.api.session.crypto.attachments.toElementToDecrypt
 import org.matrix.android.sdk.api.session.events.model.toModel
 import org.matrix.android.sdk.api.session.room.model.message.MessageImageContent
@@ -16,44 +17,52 @@ import org.matrix.android.sdk.api.session.room.timeline.TimelineEvent
 
 const val MediaCaptionFieldKey = "caption"
 
-private fun TimelineEvent.getCaption(): String? =
+
+fun TimelineEvent.toMediaContent(mediaType: MediaType): MediaContent {
+    val messageContentInfo = root.getClearContent().let {
+        when (mediaType) {
+            MediaType.Image -> it.toModel<MessageImageContent>().toMediaContentInfo(getCaption())
+            MediaType.Video -> it.toModel<MessageVideoContent>().toMediaContentInfo(getCaption())
+        }
+    }
+    return MediaContent(
+        type = if (mediaType == MediaType.Image) PostContentType.IMAGE_CONTENT else PostContentType.VIDEO_CONTENT,
+        mediaFileData = toMediaContentData(mediaType),
+        mediaContentInfo = messageContentInfo
+    )
+}
+
+private fun TimelineEvent.getCaption() =
     root.getClearContent()?.get(MediaCaptionFieldKey)?.toString()
 
-fun TimelineEvent.toImageContent(): ImageContent {
-    val messageContent = root.getClearContent().toModel<MessageImageContent>()
-
-    return ImageContent(
-        caption = getCaption(),
-        mediaContentData = toMediaContentData(MediaType.Image),
-        thumbnailUrl = messageContent?.info?.thumbnailFile?.url ?: "",
-        width = messageContent?.info?.width ?: Target.SIZE_ORIGINAL,
-        height = messageContent?.info?.height ?: Target.SIZE_ORIGINAL
+private fun MessageImageContent?.toMediaContentInfo(caption: String?): MediaContentInfo {
+    return MediaContentInfo(
+        caption = caption,
+        thumbnailUrl = this?.info?.thumbnailFile?.url ?: "",
+        width = this?.info?.width ?: Target.SIZE_ORIGINAL,
+        height = this?.info?.height ?: Target.SIZE_ORIGINAL,
+        duration = ""
     )
 }
 
-fun TimelineEvent.toVideoContent(): VideoContent {
-    val messageContent = root.getClearContent().toModel<MessageVideoContent>()
-
-    return VideoContent(
-        caption = getCaption(),
-        mediaContentData = toMediaContentData(MediaType.Video),
-        thumbnailUrl = messageContent?.videoInfo?.thumbnailFile?.url ?: "",
-        width = messageContent?.videoInfo?.width ?: Target.SIZE_ORIGINAL,
-        height = messageContent?.videoInfo?.height ?: Target.SIZE_ORIGINAL,
-        duration = VideoUtils.getVideoDurationString(
-            messageContent?.videoInfo?.duration?.toLong() ?: 0L
-        )
+private fun MessageVideoContent?.toMediaContentInfo(caption: String?): MediaContentInfo {
+    return MediaContentInfo(
+        caption = caption,
+        thumbnailUrl = this?.videoInfo?.thumbnailFile?.url ?: "",
+        width = this?.videoInfo?.width ?: Target.SIZE_ORIGINAL,
+        height = this?.videoInfo?.height ?: Target.SIZE_ORIGINAL,
+        duration = VideoUtils.getVideoDurationString(this?.videoInfo?.duration?.toLong() ?: 0L)
     )
 }
 
-private fun TimelineEvent.toMediaContentData(mediaType: MediaType): MediaContentData {
+private fun TimelineEvent.toMediaContentData(mediaType: MediaType): MediaFileData {
     val messageContent = root.getClearContent().let {
         when (mediaType) {
             MediaType.Image -> it.toModel<MessageImageContent>()
             MediaType.Video -> it.toModel<MessageVideoContent>()
         }
     }
-    return MediaContentData(
+    return MediaFileData(
         fileName = messageContent?.getFileName() ?: "",
         mimeType = messageContent?.mimeType ?: "",
         fileUrl = messageContent?.getFileUrl() ?: "",
