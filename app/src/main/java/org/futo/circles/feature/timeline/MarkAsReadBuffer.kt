86b9f5e915a9a06a9a7e8b72c5239a269ff3590b
Taras Smakula <tarassmakula@gmail.com>
2022-11-25 19:57:37 +0200

    Create mark as read buffer

---
+++ app/src/main/java/org/futo/circles/feature/timeline/MarkAsReadBuffer.kt
@@ -0,0 +1,56 @@
+package org.futo.circles.feature.timeline
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+
+class MarkAsReadBuffer(
+    private val recyclerView: RecyclerView,
+    private val onMarkAsRead: (List<Int>) -> Unit
+) {
+
+    private val positionsBuffer = mutableSetOf<Int>()
+
+    init {
+        handleInitiallyVisibleItems()
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                collectPositions()
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) callMarkAsRead()
+            }
+        })
+    }
+
+    private fun callMarkAsRead() {
+        positionsBuffer.takeIf { it.isNotEmpty() }?.let {
+            onMarkAsRead(it.toList())
+            positionsBuffer.clear()
+        }
+    }
+
+    private fun collectPositions() {
+        val manager = (recyclerView.layoutManager as? LinearLayoutManager) ?: return
+        val firstVisiblePosition = manager.findFirstVisibleItemPosition()
+        val lastVisiblePosition = manager.findLastVisibleItemPosition()
+        (firstVisiblePosition..lastVisiblePosition).forEach { positionsBuffer.add(it) }
+    }
+
+    private fun handleInitiallyVisibleItems(){
+        recyclerView.adapter?.registerAdapterDataObserver(object : AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                super.onItemRangeInserted(positionStart, itemCount)
+                recyclerView.post {
+                    collectPositions()
+                    if (positionsBuffer.isNotEmpty()) {
+                        callMarkAsRead()
+                        recyclerView.adapter?.unregisterAdapterDataObserver(this)
+                    }
+                }
+            }
+        })
+    }
+}
