fd1b6b2d6ac9de24936bd9c722dc64fae561cb04
Taras Smakula <tarassmakula@gmail.com>
2022-11-28 14:11:23 +0200

    Update adapter and view holder

--- app/src/main/java/org/futo/circles/feature/room/manage_members/list/MemberViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/room/manage_members/list/MemberViewHolder.kt
@@ -9,12 +9,14 @@ import org.futo.circles.core.list.context
 import org.futo.circles.databinding.ListItemInviteHeaderBinding
 import org.futo.circles.databinding.ListItemInvitedUserBinding
 import org.futo.circles.databinding.ListItemMemberBinding
+import org.futo.circles.databinding.ListItemNotJoinedUserBinding
 import org.futo.circles.extensions.*
+import org.futo.circles.feature.room.ManageMembersOptionsListener
 import org.futo.circles.model.GroupMemberListItem
-import org.futo.circles.model.InvitedUserListItem
 import org.futo.circles.model.ManageMembersHeaderListItem
 import org.futo.circles.model.ManageMembersListItem
-import org.futo.circles.view.ManageMembersOptionsListener
+import org.futo.circles.model.NotJoinedUserListItem
+import org.matrix.android.sdk.api.session.room.model.Membership
 
 abstract class ManageMembersViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     abstract fun bind(data: ManageMembersListItem)
@@ -50,7 +52,7 @@ class MemberViewHolder(
                 onClick(binding.contentLayout) { position -> onUserClicked(position) }
                 with(optionsView) {
                     setListener(manageMembersListener)
-                    setIsVisible(data.isOptionsOpened && data.isOptionsAvailable)
+                    setIsVisible(data.isOptionsOpened)
                     setData(data.id, data.powerLevelsContent)
                 }
             } else {
@@ -75,32 +77,42 @@ class ManageMembersHeaderViewHolder(
     }
 }
 
-class InvitedUserViewHolder(
+class NotJoinedUserViewHolder(
     parent: ViewGroup,
-    private val onCancelInvitation: (Int) -> Unit,
-) : ManageMembersViewHolder(inflate(parent, ListItemInvitedUserBinding::inflate)) {
+    private val manageMembersListener: ManageMembersOptionsListener,
+) : ManageMembersViewHolder(inflate(parent, ListItemNotJoinedUserBinding::inflate)) {
 
     private companion object : ViewBindingHolder
 
-    private val binding = baseBinding as ListItemInvitedUserBinding
+    private val binding = baseBinding as ListItemNotJoinedUserBinding
 
     override fun bind(data: ManageMembersListItem) {
-        if (data !is InvitedUserListItem) return
-
-        with(binding.ivCancelInvite) {
-            if (data.powerLevelsContent.isCurrentUserAbleToKick()) {
-                onClick(this) { position -> onCancelInvitation(position) }
-                visible()
-            } else {
-                setOnClickListener(null)
-                gone()
-            }
-        }
+        if (data !is NotJoinedUserListItem) return
 
         with(binding.lUser) {
             ivUserImage.loadProfileIcon(data.user.avatarUrl, data.user.name)
             tvUserName.text = data.user.name
             tvUserId.text = data.id
+        }
+
+        when (data.membership) {
+            Membership.INVITE -> {
+                binding.tvStatus.text = context.getString(R.string.invited)
+                val isAbleToKick = data.powerLevelsContent.isCurrentUserAbleToKick()
+                binding.ivRemove.setIsVisible(isAbleToKick)
+                if (isAbleToKick) binding.ivRemove.setOnClickListener {
+                    manageMembersListener.cancelPendingInvitation(data.user.id)
+                }
+            }
+            Membership.BAN -> {
+                binding.tvStatus.text = context.getString(R.string.banned)
+                val isAbleToBan = data.powerLevelsContent.isCurrentUserAbleToBan()
+                binding.ivRemove.setIsVisible(isAbleToBan)
+                if (isAbleToBan) binding.ivRemove.setOnClickListener {
+                    manageMembersListener.unBanUser(data.user.id)
+                }
+            }
+            else -> return
         }
     }
 }
