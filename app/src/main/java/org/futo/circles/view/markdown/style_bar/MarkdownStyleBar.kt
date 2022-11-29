02ee96f3999254e604ce24ccf2247c650729152c
Taras Smakula <tarassmakula@gmail.com>
2022-11-29 15:40:53 +0200

    Add stylebar

---
+++ app/src/main/java/org/futo/circles/view/markdown/style_bar/MarkdownStyleBar.kt
@@ -0,0 +1,75 @@
+package org.futo.circles.view.markdown.style_bar
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.yahiaangelo.markdownedittext.adapter.StylesBarAdapter
+import com.yahiaangelo.markdownedittext.model.StyleButton
+import org.futo.circles.R
+import org.futo.circles.view.markdown.MarkdownEditText
+
+class MarkdownStylesBar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private var stylesBarAdapter: StylesBarAdapter
+    private var styleButtons: ArrayList<StyleButton>
+    private var recyclerView: RecyclerView? = RecyclerView(context)
+    var stylesList: Array<MarkdownEditText.TextStyle> = MarkdownEditText.TextStyle.values()
+        set(value) {
+            field = value
+            updateStyles()
+        }
+    var markdownEditText: MarkdownEditText? = null
+        set(value) {
+            stylesBarAdapter.markdownEditText = value
+            field = value
+        }
+
+    init {
+        val linearLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        recyclerView?.layoutManager = linearLayoutManager
+        styleButtons = ArrayList()
+        setStylesButtons()
+        stylesBarAdapter = StylesBarAdapter(styleButtons)
+        recyclerView?.adapter = stylesBarAdapter
+
+        val a = context.obtainStyledAttributes(attrs, R.styleable.MarkdownStylesBar)
+        val buttonColorStateList = a.getColorStateList(R.styleable.MarkdownStylesBar_buttonColor)
+        if (buttonColorStateList != null){
+            stylesBarAdapter.buttonsColor = buttonColorStateList
+        }
+        a.recycle()
+        addView(recyclerView)
+
+    }
+
+    private fun updateStyles(){
+        styleButtons.clear()
+        setStylesButtons()
+        stylesBarAdapter.setStyles(styleButtons)
+    }
+
+    private fun setStylesButtons(){
+        for (style in stylesList){
+            when(style){
+                MarkdownEditText.TextStyle.BOLD -> styleButtons.add(StyleButton(R.drawable.ic_bold, R.id.style_button_bold))
+                MarkdownEditText.TextStyle.ITALIC -> styleButtons.add(StyleButton(R.drawable.ic_italic, R.id.style_button_italic))
+                MarkdownEditText.TextStyle.STRIKE -> styleButtons.add(StyleButton(R.drawable.ic_strikethrough, R.id.style_button_strike))
+                MarkdownEditText.TextStyle.UNORDERED_LIST -> styleButtons.add(StyleButton(R.drawable.ic_bullet_list, R.id.style_button_unordered_list))
+                MarkdownEditText.TextStyle.ORDERED_LIST -> styleButtons.add(StyleButton(R.drawable.ic_number_list, R.id.style_button_ordered_list))
+                MarkdownEditText.TextStyle.TASKS_LIST -> styleButtons.add(StyleButton(R.drawable.ic_check_box, R.id.style_button_task_list))
+                MarkdownEditText.TextStyle.LINK -> styleButtons.add(StyleButton(R.drawable.ic_link, R.id.style_button_link))
+                else ->{}
+            }
+        }
+    }
+
+    fun getViewWithId(id: Int): View?{
+        return recyclerView?.findViewById(id)
+    }
+
+}
---
+++ app/src/main/java/org/futo/circles/view/markdown/style_bar/StyleBarAdapter.kt
@@ -0,0 +1,95 @@
+package org.futo.circles.view.markdown.style_bar
+
+
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.yahiaangelo.markdownedittext.MarkdownEditText
+import com.yahiaangelo.markdownedittext.R
+import com.yahiaangelo.markdownedittext.model.StyleButton
+
+class StylesBarAdapter(private var buttonsList: ArrayList<StyleButton>) : RecyclerView.Adapter<StylesBarAdapter.MyViewHolder>() {
+
+    private var selectedButton: MaterialButton? = null
+    var buttonsColor: ColorStateList? = null
+    var markdownEditText: MarkdownEditText? = null
+
+    class MyViewHolder(val styleButton: MaterialButton) : RecyclerView.ViewHolder(styleButton)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val styleButtonView = LayoutInflater.from(parent.context).inflate(R.layout.styles_bar_item, parent, false) as MaterialButton
+        return MyViewHolder(styleButtonView)
+    }
+
+
+    override fun getItemCount(): Int {
+        return buttonsList.size
+    }
+
+    fun setStyles(newButtonsList: ArrayList<StyleButton>){
+        buttonsList = newButtonsList
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val styleButtonModel = buttonsList[position]
+        if (buttonsColor != null){
+            holder.styleButton.backgroundTintList = buttonsColor
+        }
+        holder.styleButton.icon = holder.itemView.context.getDrawable(styleButtonModel.icon)
+        holder.styleButton.id = styleButtonModel.id
+        if (styleButtonModel.id == R.id.style_button_link){
+            holder.styleButton.isCheckable = false
+            holder.styleButton.setOnClickListener {
+                styleButtonClick(holder.styleButton)
+            }
+        }
+        holder.styleButton.addOnCheckedChangeListener { button, _ ->
+            if (selectedButton != null) {
+                if (selectedButton != button) {
+                    selectedButton!!.isChecked = false
+                    styleButtonClick(selectedButton!!)
+                }
+                styleButtonClick(button)
+                selectedButton = button
+            } else {
+                styleButtonClick(button)
+                selectedButton = button
+            }
+
+        }
+    }
+
+    private fun styleButtonClick(button: MaterialButton) {
+        if(markdownEditText != null) {
+            when (button.id) {
+                R.id.style_button_bold -> markdownEditText!!.triggerStyle(
+                    MarkdownEditText.TextStyle.BOLD,
+                    !button.isChecked
+                )
+                R.id.style_button_italic -> markdownEditText!!.triggerStyle(
+                    MarkdownEditText.TextStyle.ITALIC,
+                    !button.isChecked
+                )
+                R.id.style_button_strike -> markdownEditText!!.triggerStyle(
+                    MarkdownEditText.TextStyle.STRIKE,
+                    !button.isChecked
+                )
+                R.id.style_button_unordered_list -> markdownEditText!!.triggerStyle(
+                    MarkdownEditText.TextStyle.UNORDERED_LIST,
+                    !button.isChecked
+                )
+                R.id.style_button_ordered_list -> markdownEditText!!.triggerStyle(
+                    MarkdownEditText.TextStyle.ORDERED_LIST,
+                    !button.isChecked)
+                R.id.style_button_task_list -> markdownEditText!!.triggerStyle(
+                    MarkdownEditText.TextStyle.TASKS_LIST,
+                    !button.isChecked)
+                R.id.style_button_link -> markdownEditText!!.showInsertLinkDialog()
+            }
+
+        }
+    }
+}
