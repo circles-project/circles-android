663fcd322182f49ebf122550e6167189facc56dd
Taras Smakula <tarassmakula@gmail.com>
2022-11-29 18:03:06 +0200

    Update MarkdownStylesBar

--- app/src/main/java/org/futo/circles/view/markdown/style_bar/MarkdownStyleBar.kt
+++ app/src/main/java/org/futo/circles/view/markdown/style_bar/MarkdownStyleBar.kt
@@ -2,74 +2,89 @@ package org.futo.circles.view.markdown.style_bar
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import android.widget.LinearLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.yahiaangelo.markdownedittext.adapter.StylesBarAdapter
-import com.yahiaangelo.markdownedittext.model.StyleButton
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.futo.circles.R
-import org.futo.circles.view.markdown.MarkdownEditText
+import org.futo.circles.databinding.ViewMarkdownStylebarBinding
+import org.futo.circles.extensions.setIsVisible
+import org.futo.circles.feature.timeline.post.create.PostConfigurationOptionListener
+import org.futo.circles.model.MainStyleBarOption
+import org.futo.circles.model.StyleBarListItem
+import org.futo.circles.view.markdown.TextStyle
 
-class MarkdownStylesBar @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+class MarkdownStylesBar(
+    context: Context,
+    attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
 
-    private var stylesBarAdapter: StylesBarAdapter
-    private var styleButtons: ArrayList<StyleButton>
-    private var recyclerView: RecyclerView? = RecyclerView(context)
-    var stylesList: Array<MarkdownEditText.TextStyle> = MarkdownEditText.TextStyle.values()
-        set(value) {
-            field = value
-            updateStyles()
-        }
-    var markdownEditText: MarkdownEditText? = null
-        set(value) {
-            stylesBarAdapter.markdownEditText = value
-            field = value
-        }
+    private val binding =
+        ViewMarkdownStylebarBinding.inflate(LayoutInflater.from(context), this)
+
+    private val mainOptions = listOf(
+        StyleBarListItem(MainStyleBarOption.Media.ordinal, R.drawable.ic_image),
+        StyleBarListItem(MainStyleBarOption.Emoji.ordinal, R.drawable.ic_emoji),
+        StyleBarListItem(MainStyleBarOption.Mention.ordinal, R.drawable.ic_mention),
+        StyleBarListItem(MainStyleBarOption.Link.ordinal, R.drawable.ic_link),
+        StyleBarListItem(MainStyleBarOption.TextStyle.ordinal, R.drawable.ic_text)
+    )
+
+    private val textStyleOptions = listOf(
+        StyleBarListItem(TextStyle.BOLD.ordinal, R.drawable.ic_bold),
+        StyleBarListItem(TextStyle.ITALIC.ordinal, R.drawable.ic_italic),
+        StyleBarListItem(TextStyle.STRIKE.ordinal, R.drawable.ic_strikethrough),
+        StyleBarListItem(TextStyle.UNORDERED_LIST.ordinal, R.drawable.ic_bullet_list),
+        StyleBarListItem(TextStyle.ORDERED_LIST.ordinal, R.drawable.ic_number_list),
+        StyleBarListItem(TextStyle.TASKS_LIST.ordinal, R.drawable.ic_check_box)
+    )
+
+    private val mainOptionsAdapter = OptionsStyleBarAdapter(::onMainOptionSelected)
+    private val textStyleOptionsAdapter =
+        OptionsStyleBarAdapter(::onTextStyleSelected).apply { submitList(textStyleOptions) }
+
+    private var postConfigurationListener: PostConfigurationOptionListener? = null
+
+    private var isTextOptionsOpened = false
 
     init {
-        val linearLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-        recyclerView?.layoutManager = linearLayoutManager
-        styleButtons = ArrayList()
-        setStylesButtons()
-        stylesBarAdapter = StylesBarAdapter(styleButtons)
-        recyclerView?.adapter = stylesBarAdapter
+        setupViews()
+    }
 
-        val a = context.obtainStyledAttributes(attrs, R.styleable.MarkdownStylesBar)
-        val buttonColorStateList = a.getColorStateList(R.styleable.MarkdownStylesBar_buttonColor)
-        if (buttonColorStateList != null){
-            stylesBarAdapter.buttonsColor = buttonColorStateList
+    fun setOptionsListener(listener: PostConfigurationOptionListener) {
+        postConfigurationListener = listener
+    }
+
+    fun showMainOptionsList(isMediaAvailable: Boolean) {
+        mainOptionsAdapter.submitList(
+            if (isMediaAvailable) mainOptions
+            else mainOptions.filter { it.id != MainStyleBarOption.Media.ordinal }
+        )
+    }
+
+    private fun setupViews() {
+        setTextOptionsOpened(false)
+        binding.ivCancel.setOnClickListener { setTextOptionsOpened(false) }
+        binding.rvMainOptions.adapter = mainOptionsAdapter
+        binding.rvTextOptions.adapter = textStyleOptionsAdapter
+    }
+
+    private fun setTextOptionsOpened(isOpened: Boolean) {
+        isTextOptionsOpened = isOpened
+        binding.rvMainOptions.setIsVisible(!isTextOptionsOpened)
+        binding.textOptionsGroup.setIsVisible(isTextOptionsOpened)
+    }
+
+    private fun onMainOptionSelected(id: Int) {
+        when (MainStyleBarOption.values()[id]) {
+            MainStyleBarOption.Media -> postConfigurationListener?.onUploadMediaClicked()
+            MainStyleBarOption.Emoji -> postConfigurationListener?.onEmojiClicked()
+            MainStyleBarOption.Mention -> postConfigurationListener?.onMentionClicked()
+            MainStyleBarOption.Link -> postConfigurationListener?.onAddLinkClicked()
+            MainStyleBarOption.TextStyle -> setTextOptionsOpened(true)
         }
-        a.recycle()
-        addView(recyclerView)
-
     }
 
-    private fun updateStyles(){
-        styleButtons.clear()
-        setStylesButtons()
-        stylesBarAdapter.setStyles(styleButtons)
-    }
-
-    private fun setStylesButtons(){
-        for (style in stylesList){
-            when(style){
-                MarkdownEditText.TextStyle.BOLD -> styleButtons.add(StyleButton(R.drawable.ic_bold, R.id.style_button_bold))
-                MarkdownEditText.TextStyle.ITALIC -> styleButtons.add(StyleButton(R.drawable.ic_italic, R.id.style_button_italic))
-                MarkdownEditText.TextStyle.STRIKE -> styleButtons.add(StyleButton(R.drawable.ic_strikethrough, R.id.style_button_strike))
-                MarkdownEditText.TextStyle.UNORDERED_LIST -> styleButtons.add(StyleButton(R.drawable.ic_bullet_list, R.id.style_button_unordered_list))
-                MarkdownEditText.TextStyle.ORDERED_LIST -> styleButtons.add(StyleButton(R.drawable.ic_number_list, R.id.style_button_ordered_list))
-                MarkdownEditText.TextStyle.TASKS_LIST -> styleButtons.add(StyleButton(R.drawable.ic_check_box, R.id.style_button_task_list))
-                MarkdownEditText.TextStyle.LINK -> styleButtons.add(StyleButton(R.drawable.ic_link, R.id.style_button_link))
-                else ->{}
-            }
-        }
-    }
-
-    fun getViewWithId(id: Int): View?{
-        return recyclerView?.findViewById(id)
+    private fun onTextStyleSelected(id: Int) {
+        postConfigurationListener?.onTextStyleSelected(TextStyle.values()[id])
     }
 
 }
