c2bc43b36f250479b7aef2ecd5b1780efd4bb919
Taras Smakula <tarassmakula@gmail.com>
2022-11-29 15:40:06 +0200

    Add TextStyle enum

---
+++ app/src/main/java/org/futo/circles/view/markdown/TextStyle.kt
@@ -0,0 +1,12 @@
+package org.futo.circles.view.markdown
+
+enum class TextStyle {
+    BOLD,
+    ITALIC,
+    STRIKE,
+    QUOTE,
+    LINK,
+    UNORDERED_LIST,
+    ORDERED_LIST,
+    TASKS_LIST
+}
