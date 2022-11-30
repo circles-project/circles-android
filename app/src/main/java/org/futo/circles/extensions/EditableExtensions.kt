5987a9db0ea3652a1ed39c4af425d3122a1fb880
Taras Smakula <tarassmakula@gmail.com>
2022-11-30 20:16:20 +0200

    Fix getGivenSpansAt extension

--- app/src/main/java/org/futo/circles/extensions/EditableExtensions.kt
+++ app/src/main/java/org/futo/circles/extensions/EditableExtensions.kt
@@ -2,23 +2,16 @@ package org.futo.circles.extensions
 
 import android.text.Editable
 import org.futo.circles.view.markdown.TextStyle
-
-fun Editable.getGivenSpans(vararg span: TextStyle): MutableList<Any> {
-    val spanList = mutableListOf<Any>()
-    for (selectedSpan in span) {
-        getSpans(0, length, selectedSpan::class.java).forEach { spanList.add(it) }
-    }
-    return spanList
-}
+import org.futo.circles.view.markdown.toSpanClass
 
 fun Editable.getGivenSpansAt(
-    vararg span: Any,
-    start: Int,
-    end: Int
+    vararg span: TextStyle,
+    start: Int = 0,
+    end: Int = length
 ): MutableList<Any> {
     val spanList = mutableListOf<Any>()
     for (selectedSpan in span) {
-        getSpans(start, end, selectedSpan::class.java).forEach { spanList.add(it) }
+        getSpans(start, end, selectedSpan.toSpanClass()).forEach { spanList.add(it) }
     }
     return spanList
 }
