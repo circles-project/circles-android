8efc9d656813774127bbdab0c804f02d611f4f65
Taras Smakula <tarassmakula@gmail.com>
2022-12-02 19:01:42 +0200

    Update parser

--- app/src/main/java/org/futo/circles/view/markdown/MarkdownParser.kt
+++ app/src/main/java/org/futo/circles/view/markdown/MarkdownParser.kt
@@ -1,13 +1,24 @@
 package org.futo.circles.view.markdown
 
+import android.content.Context
+import android.graphics.Color
 import android.text.Editable
-import android.text.Spannable
 import android.text.style.StrikethroughSpan
+import androidx.core.content.ContextCompat
+import io.noties.markwon.AbstractMarkwonPlugin
+import io.noties.markwon.Markwon
+import io.noties.markwon.MarkwonSpansFactory
+import io.noties.markwon.MarkwonVisitor
 import io.noties.markwon.core.spans.BulletListItemSpan
 import io.noties.markwon.core.spans.EmphasisSpan
 import io.noties.markwon.core.spans.LinkSpan
 import io.noties.markwon.core.spans.StrongEmphasisSpan
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
+import io.noties.markwon.ext.tasklist.TaskListItem
+import io.noties.markwon.ext.tasklist.TaskListPlugin
 import io.noties.markwon.ext.tasklist.TaskListSpan
+import org.commonmark.node.SoftLineBreak
+import org.futo.circles.R
 import org.futo.circles.extensions.getGivenSpansAt
 
 
@@ -16,9 +27,8 @@ object MarkdownParser {
     private const val boldMark = "**"
     private const val italicMark = "_"
     private const val strikeMark = "~~"
-    private const val notDoneMark = "* []"
+    private const val notDoneMark = "* [ ]"
     private const val doneMark = "* [x]"
-
 
     fun editableToMarkdown(text: Editable): String {
         val textCopy = Editable.Factory.getInstance().newEditable(text)
@@ -54,28 +64,35 @@ object MarkdownParser {
         return textCopy.toString()
     }
 
-    fun markdownToEditable(markdown: String): Editable {
-        val editable = Editable.Factory.getInstance().newEditable(markdown)
-        setTextStyleSpan(editable, boldMark, StrongEmphasisSpan())
-        setTextStyleSpan(editable, italicMark, EmphasisSpan())
-        setTextStyleSpan(editable, strikeMark, StrikethroughSpan())
-        return editable
+    fun markdownToEditable(markdown: String, context: Context): Editable {
+        return Editable.Factory.getInstance()
+            .newEditable(markwonBuilder(context).toMarkdown(markdown))
     }
 
-    private fun setTextStyleSpan(editable: Editable, mark: String, span: Any) {
-        var pointer = 0
-        while (pointer <= editable.lastIndex) {
-            val start = editable.indexOf(mark, pointer)
-            val end = editable.indexOf(mark, start + mark.length)
-            if (start == -1 || end == -1) break
-            editable.setSpan(span, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-            pointer = end + mark.length
-        }
-        while (true) {
-            val index = editable.indexOf(mark, 0)
-            if (index == -1) break
-            editable.replace(index, index + mark.length, "")
-        }
+    fun markwonBuilder(context: Context): Markwon = Markwon.builder(context)
+        .usePlugin(StrikethroughPlugin.create())
+        .usePlugin(
+            TaskListPlugin.create(
+                ContextCompat.getColor(context, R.color.blue),
+                ContextCompat.getColor(context, R.color.blue),
+                Color.WHITE
+            )
+        )
+        .usePlugin(object : AbstractMarkwonPlugin() {
+            override fun configureVisitor(builder: MarkwonVisitor.Builder) {
+                super.configureVisitor(builder)
+                builder.on(SoftLineBreak::class.java) { visitor, _ -> visitor.forceNewLine() }
+            }
 
-    }
+            override fun configureSpansFactory(builder: MarkwonSpansFactory.Builder) {
+                val origin = builder.getFactory(TaskListItem::class.java)
+                builder.setFactory(
+                    TaskListItem::class.java
+                ) { configuration, props ->
+                    val span = origin?.getSpans(configuration, props)
+                    (span as? TaskListSpan)?.let { arrayOf(span) }
+                }
+            }
+        }).build()
+
 }
