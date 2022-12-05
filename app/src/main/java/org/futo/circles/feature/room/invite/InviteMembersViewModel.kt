9b62e01cc64d1d33aa37a854d936ac28b9627079
Taras Smakula <tarassmakula@gmail.com>
2022-12-05 17:01:31 +0200

    Add real message for invite error

--- app/src/main/java/org/futo/circles/feature/room/invite/InviteMembersViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/room/invite/InviteMembersViewModel.kt
@@ -2,10 +2,11 @@ package org.futo.circles.feature.room.invite
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import org.futo.circles.core.ErrorParser
 import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.extensions.Response
 import org.futo.circles.extensions.launchBg
-import org.futo.circles.model.UserListItem
 
 class InviteMembersViewModel(
     private val dataSource: InviteMembersDataSource
@@ -16,7 +17,11 @@ class InviteMembersViewModel(
     val inviteResultLiveData = SingleEventLiveData<Response<List<Unit?>>>()
 
     fun invite(usersIds: List<String>) {
-        launchBg { inviteResultLiveData.postValue(dataSource.inviteUsers(this, usersIds)) }
+        launchBg(
+            CoroutineExceptionHandler { _, t ->
+                inviteResultLiveData.postValue(Response.Error(ErrorParser.getErrorMessage(t)))
+            }
+        ) { inviteResultLiveData.postValue(dataSource.inviteUsers(this, usersIds)) }
     }
 
 }
