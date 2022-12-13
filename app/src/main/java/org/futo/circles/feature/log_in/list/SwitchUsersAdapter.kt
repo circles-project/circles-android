e8f920695ce8a2651adb5ec60ce194837339bbb3
Taras Smakula <tarassmakula@gmail.com>
2022-12-13 17:16:22 +0200

    Create switch user adapter

---
+++ app/src/main/java/org/futo/circles/feature/log_in/list/SwitchUsersAdapter.kt
@@ -0,0 +1,25 @@
+package org.futo.circles.feature.log_in.list
+
+import android.view.ViewGroup
+import org.futo.circles.core.list.BaseRvAdapter
+import org.futo.circles.model.SwitchUserListItem
+
+class SwitchUsersAdapter(
+    private val onResumeClicked: (String) -> Unit,
+    private val onRemoveClicked: (String) -> Unit
+) : BaseRvAdapter<SwitchUserListItem, SwitchUsersViewHolder>(DefaultIdEntityCallback()) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SwitchUsersViewHolder = SwitchUsersViewHolder(
+        parent = parent,
+        onUserClicked = { position -> onResumeClicked(getItem(position).id) },
+        onRemoveClicked = { position -> onRemoveClicked(getItem(position).id) },
+    )
+
+    override fun onBindViewHolder(holder: SwitchUsersViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+}
