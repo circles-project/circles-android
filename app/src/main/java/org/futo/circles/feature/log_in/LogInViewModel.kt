464851896f4516522f30d50903716ede2423122d
Taras Smakula <tarassmakula@gmail.com>
2022-12-14 19:43:34 +0200

    Add resumeSwitchUserSession

--- app/src/main/java/org/futo/circles/feature/log_in/LogInViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/LogInViewModel.kt
@@ -6,6 +6,7 @@ import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.extensions.Response
 import org.futo.circles.extensions.launchBg
 import org.futo.circles.feature.log_in.switch_user.SwitchUserDataSource
+import org.futo.circles.provider.MatrixSessionProvider
 
 class LogInViewModel(
     private val loginDataSource: LoginDataSource,
@@ -14,12 +15,12 @@ class LogInViewModel(
 
     val loginResultLiveData = SingleEventLiveData<Response<Unit>>()
     val switchUsersLiveData = MutableLiveData(switchUserDataSource.getSwitchUsersList())
+    val navigateToBottomMenuScreenLiveData = SingleEventLiveData<Unit>()
 
     fun startLogInFlow(userName: String, domain: String) {
-        launchBg {
-            val loginResult = loginDataSource.startLogin(userName, domain)
-            loginResultLiveData.postValue(loginResult)
-        }
+        switchUserDataSource.getSessionCredentialsIdByUserInfo(userName, domain)
+            ?.let { resumeSwitchUserSession(it) }
+            ?: login(userName, domain)
     }
 
     fun removeSwitchUser(id: String) {
@@ -27,11 +28,21 @@ class LogInViewModel(
             switchUserDataSource.removeSwitchUser(id)
             switchUsersLiveData.postValue(switchUserDataSource.getSwitchUsersList())
         }
-
     }
 
     fun resumeSwitchUserSession(id: String) {
+        val session = switchUserDataSource.getSessionWithId(id) ?: return
+        launchBg {
+            MatrixSessionProvider.awaitForSessionSync(session)
+            navigateToBottomMenuScreenLiveData.postValue(Unit)
+        }
+    }
 
+    private fun login(userName: String, domain: String) {
+        launchBg {
+            val loginResult = loginDataSource.startLogin(userName, domain)
+            loginResultLiveData.postValue(loginResult)
+        }
     }
 
 }
