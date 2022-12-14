292ae41f3ef2c73e0e29ab955d347493beb17e07
Taras Smakula <tarassmakula@gmail.com>
2022-12-14 15:54:22 +0200

    Create view holder for switch user

--- app/src/main/java/org/futo/circles/feature/log_in/switch_user/list/SwitchUsersViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/log_in/switch_user/list/SwitchUsersViewHolder.kt
@@ -1,16 +1,18 @@
-package org.futo.circles.feature.log_in.list
+package org.futo.circles.feature.log_in.switch_user.list
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.futo.circles.core.list.ViewBindingHolder
 import org.futo.circles.databinding.ListItemSwitchUserBinding
+import org.futo.circles.extensions.loadProfileIcon
 import org.futo.circles.extensions.onClick
+import org.futo.circles.mapping.notEmptyDisplayName
 import org.futo.circles.model.SwitchUserListItem
 
 class SwitchUsersViewHolder(
     parent: ViewGroup,
     onUserClicked: (Int) -> Unit,
-    onRemoveClicked: (Int) -> Unit
+    private val onRemoveClicked: (Int) -> Unit
 ) : RecyclerView.ViewHolder(inflate(parent, ListItemSwitchUserBinding::inflate)) {
 
     private companion object : ViewBindingHolder
@@ -19,9 +21,18 @@ class SwitchUsersViewHolder(
 
     init {
         onClick(itemView) { position -> onUserClicked(position) }
+        onClick(binding.ivRemove) { position -> onRemoveClicked(position) }
     }
 
     fun bind(data: SwitchUserListItem) {
-
+        with(binding) {
+            ivUserImage.loadProfileIcon(
+                data.user.avatarUrl,
+                data.user.notEmptyDisplayName(),
+                session = data.session
+            )
+            tvUserName.text = data.user.notEmptyDisplayName()
+            tvUserId.text = data.user.userId
+        }
     }
 }
