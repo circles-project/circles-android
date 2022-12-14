b7abcf1f9228e3daeaf25a9c36985380332bdfa9
Taras Smakula <tarassmakula@gmail.com>
2022-12-14 13:29:19 +0200

    Add session id to user

--- app/src/main/java/org/futo/circles/model/SwitchUserListItem.kt
+++ app/src/main/java/org/futo/circles/model/SwitchUserListItem.kt
@@ -2,9 +2,10 @@ package org.futo.circles.model
 
 import org.futo.circles.core.list.IdEntity
 import org.matrix.android.sdk.api.auth.data.SessionParams
+import org.matrix.android.sdk.api.auth.data.sessionId
 
 data class SwitchUserListItem(
     val params: SessionParams
 ) : IdEntity<String> {
-    override val id: String = params.credentials.
+    override val id: String = params.credentials.sessionId()
 }
