e19560ea90f09ae2b196c359e3ff3e62d748cc1d
Taras Smakula <tarassmakula@gmail.com>
2022-12-15 16:49:23 +0200

    Add MXCryptoError to error parser

--- app/src/main/java/org/futo/circles/core/ErrorParser.kt
+++ app/src/main/java/org/futo/circles/core/ErrorParser.kt
@@ -2,6 +2,7 @@ package org.futo.circles.core
 
 import org.json.JSONObject
 import org.matrix.android.sdk.api.failure.Failure
+import org.matrix.android.sdk.api.session.crypto.MXCryptoError
 import retrofit2.HttpException
 
 object ErrorParser {
@@ -9,7 +10,7 @@ object ErrorParser {
     private const val AUTH_EXCEPTION_REASON_KEY = "reason"
 
     fun getErrorMessage(t: Throwable): String =
-        handleErrorBodyException(t) ?: handleServerException(t) ?: "Unexpected error"
+        handleErrorBodyException(t) ?: handleOtherException(t) ?: "Unexpected error"
 
 
     private fun handleErrorBodyException(t: Throwable): String? =
@@ -21,6 +22,10 @@ object ErrorParser {
             }
         }
 
-    private fun handleServerException(t: Throwable): String? =
-        (t as? Failure.ServerError)?.error?.message ?: t.message
+    private fun handleOtherException(t: Throwable): String? = when (t) {
+        is Failure.ServerError -> t.error.message
+        is MXCryptoError.Base -> t.technicalMessage
+        else -> t.message
+    }
+
 }
