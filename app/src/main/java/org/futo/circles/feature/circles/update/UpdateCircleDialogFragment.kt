f9a5a90fc852838f564f0b6de4187b0c1276ebde
Taras Smakula <tarassmakula@gmail.com>
2022-12-27 13:33:17 +0200

    Create updateCircleDialogfragment

---
+++ app/src/main/java/org/futo/circles/feature/circles/update/UpdateCircleDialogFragment.kt
@@ -0,0 +1,63 @@
+package org.futo.circles.feature.circles.update
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import org.futo.circles.R
+import org.futo.circles.core.picker.MediaPickerHelper
+import org.futo.circles.databinding.DialogFragmentUpdateCircleBinding
+import org.futo.circles.extensions.getText
+import org.futo.circles.extensions.loadProfileIcon
+import org.futo.circles.feature.room.update.UpdateRoomDialogFragment
+import org.matrix.android.sdk.api.session.room.model.RoomSummary
+
+class UpdateCircleDialogFragment :
+    UpdateRoomDialogFragment(DialogFragmentUpdateCircleBinding::inflate) {
+
+    override val fragment: Fragment = this
+    override val mediaPickerHelper: MediaPickerHelper = MediaPickerHelper(this)
+    override val successMessageResId: Int = R.string.circle_updated
+
+    private val binding by lazy {
+        getBinding() as DialogFragmentUpdateCircleBinding
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    override fun onCoverImageSelected(uri: Uri) {
+        binding.ivCover.setImageURI(uri)
+        onInputDataChanged()
+    }
+
+    override fun setInitialGroupData(room: RoomSummary) {
+        binding.ivCover.loadProfileIcon(room.avatarUrl, room.displayName)
+        binding.tilName.editText?.setText(room.displayName)
+    }
+
+    override fun setUpdateButtonEnabled(isEnabled: Boolean) {
+        binding.btnSave.isEnabled = isEnabled
+    }
+
+    private fun setupViews() {
+        with(binding) {
+            ivCover.setOnClickListener { changeCoverImage() }
+            btnChangeIcon.setOnClickListener { changeCoverImage() }
+            tilName.editText?.doAfterTextChanged {
+                it?.let { onInputDataChanged() }
+            }
+            btnSave.setOnClickListener {
+                updateRoom(tilName.getText())
+                startLoading(btnSave)
+            }
+        }
+    }
+
+    private fun onInputDataChanged() {
+        onInputRoomDataChanged(binding.tilName.getText())
+    }
+}
