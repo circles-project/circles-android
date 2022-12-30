52f35ea19bd4e9a107434e9950973b7d7ac5d3c1
Taras Smakula <tarassmakula@gmail.com>
2022-12-30 13:04:37 +0200

    Auto accept notices room invite

--- app/src/main/java/org/futo/circles/feature/bottom_navigation/SystemNoticesCountSharedViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/bottom_navigation/SystemNoticesCountSharedViewModel.kt
@@ -2,8 +2,13 @@ package org.futo.circles.feature.bottom_navigation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
+import org.futo.circles.core.DEFAULT_USER_PREFIX
 import org.futo.circles.core.SYSTEM_NOTICES_TAG
+import org.futo.circles.core.utils.UserUtils
+import org.futo.circles.extensions.launchBg
 import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.session.Session
+import org.matrix.android.sdk.api.session.room.model.Membership
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
 class SystemNoticesCountSharedViewModel : ViewModel() {
@@ -11,5 +16,23 @@ class SystemNoticesCountSharedViewModel : ViewModel() {
     val systemNoticesCountLiveData = MatrixSessionProvider.currentSession?.roomService()
         ?.getRoomSummariesLive(roomSummaryQueryParams())
         ?.map { list -> list.firstOrNull { it.hasTag(SYSTEM_NOTICES_TAG) }?.notificationCount }
+
+    init {
+        autoAcceptNoticeRoomInvite()
+    }
+
+    private fun autoAcceptNoticeRoomInvite() {
+        val session = MatrixSessionProvider.currentSession ?: return
+        session.roomService().getRoomSummaries(roomSummaryQueryParams {
+            memberships = listOf(Membership.INVITE)
+        }).firstOrNull { it.inviterId == getNoticesUserId(session) }?.let {
+            launchBg { session.roomService().joinRoom(it.roomId) }
+        }
+    }
+
+    private fun getNoticesUserId(session: Session): String {
+        val domain = UserUtils.getServerDomain(session.myUserId)
+        return DEFAULT_USER_PREFIX + domain
+    }
 
 }
