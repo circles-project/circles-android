78091e6431f6b06a25eebea9aadd5671950004a8
Taras Smakula <tarassmakula@gmail.com>
2023-01-04 17:05:36 +0200

    Implement follow request logic

--- app/src/main/java/org/futo/circles/feature/people/PeopleDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/people/PeopleDataSource.kt
@@ -1,22 +1,23 @@
 package org.futo.circles.feature.people
 
 import androidx.lifecycle.asFlow
+import androidx.lifecycle.map
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 import org.futo.circles.core.utils.UserUtils
 import org.futo.circles.extensions.Response
 import org.futo.circles.extensions.createResult
 import org.futo.circles.feature.room.select_users.SearchUserDataSource
-import org.futo.circles.mapping.toPeopleUserListItem
-import org.futo.circles.model.PeopleHeaderItem
-import org.futo.circles.model.PeopleListItem
-import org.futo.circles.model.PeopleUserListItem
+import org.futo.circles.mapping.toPeopleFollowingUserListItem
+import org.futo.circles.mapping.toPeopleIgnoredUserListItem
+import org.futo.circles.mapping.toPeopleRequestUserListItem
+import org.futo.circles.mapping.toPeopleSuggestionUserListItem
+import org.futo.circles.model.*
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.getRoom
 import org.matrix.android.sdk.api.session.room.Room
+import org.matrix.android.sdk.api.session.room.members.roomMemberQueryParams
+import org.matrix.android.sdk.api.session.room.model.Membership
 import org.matrix.android.sdk.api.session.room.model.roomdirectory.PublicRoomsFilter
 import org.matrix.android.sdk.api.session.room.model.roomdirectory.PublicRoomsParams
 import org.matrix.android.sdk.api.session.user.model.User
@@ -27,20 +28,29 @@ class PeopleDataSource(
 
     private val session = MatrixSessionProvider.currentSession
 
+    //TODO get profile room id from user's profile
     //61 - !dYyuPbUOkbUYUOMbot:nl.circles-dev.net
     //64 - !fznKPZDngFIyKzprgp:nl.circles-dev.net
-    suspend fun followUser(userListItem: PeopleUserListItem) =
-        createResult {
-            session?.roomService()?.knock("!dYyuPbUOkbUYUOMbot:nl.circles-dev.net")
-        }
+    private val profileRoomId = "!dYyuPbUOkbUYUOMbot:nl.circles-dev.net"
+
+    suspend fun followUser(userListItem: PeopleSuggestionUserListItem) =
+        createResult { session?.roomService()?.knock(profileRoomId) }
+
+    private fun getProfileRoomMembersKnockFlow(): Flow<List<User>> =
+        session?.getRoom(profileRoomId)?.membershipService()
+            ?.getRoomMembersLive(roomMemberQueryParams { memberships = listOf(Membership.KNOCK) })
+            ?.map { it.map { User(it.userId, it.displayName, it.avatarUrl) } }
+            ?.asFlow()
+            ?: flowOf()
 
     suspend fun getPeopleList(query: String) = combine(
         searchUserDataSource.searchKnownUsers(query),
         searchUserDataSource.searchSuggestions(query),
-        getIgnoredUserFlow()
+        getIgnoredUserFlow(),
+        getProfileRoomMembersKnockFlow()
     )
-    { knowUsers, suggestions, ignoredUsers ->
-        buildList(knowUsers, suggestions, ignoredUsers)
+    { knowUsers, suggestions, ignoredUsers, requests ->
+        buildList(knowUsers, suggestions, ignoredUsers, requests)
     }.flowOn(Dispatchers.IO).distinctUntilChanged()
 
     suspend fun refreshRoomMembers() {
@@ -53,49 +63,61 @@ class PeopleDataSource(
     private suspend fun buildList(
         knowUsers: List<User>,
         suggestions: List<User>,
-        ignoredUsers: List<User>
+        ignoredUsers: List<User>,
+        requests: List<User>
     ): List<PeopleListItem> {
-        val list = mutableListOf<PeopleListItem>()
-
-        val filteredKnownUsers = getKnownPeopleWithoutIgnored(knowUsers, ignoredUsers)
-
-        val followingUsers = filteredKnownUsers.filter { it.isFollowedByMe }
-        if (followingUsers.isNotEmpty()) {
-            list.add(PeopleHeaderItem.followingUsersHeader)
-            list.addAll(followingUsers)
-        }
-
-        val known = filteredKnownUsers.filterNot { it.isFollowedByMe }
-        if (known.isNotEmpty()) {
-            list.add(PeopleHeaderItem.knownUsersHeader)
-            list.addAll(known)
-        }
-
-        if (ignoredUsers.isNotEmpty()) {
-            list.add(PeopleHeaderItem.ignoredUsers)
-            list.addAll(ignoredUsers.map { it.toPeopleUserListItem(isIgnored = true) })
-        }
-
-        val existingIds = list.map { it.id }
-        val filteredSuggestion = suggestions.filterNot { existingIds.contains(it.userId) }
-        if (filteredSuggestion.isNotEmpty()) {
-            list.add(PeopleHeaderItem.suggestions)
-            list.addAll(filteredSuggestion.map {
-                it.toPeopleUserListItem(profileRoomId = getProfileRoomForUser(it.userId)?.roomId)
+        val uniqueItemsList = mutableListOf<PeopleListItem>().apply {
+            addAll(ignoredUsers.map { it.toPeopleIgnoredUserListItem() })
+            addAll(requests.map { it.toPeopleRequestUserListItem() })
+            addAll(knowUsers.map {
+                val profileRoomId = getProfileRoomForUser(it.userId)?.roomId
+                if (amIFollowThisUserProfile(profileRoomId)) it.toPeopleFollowingUserListItem()
+                else it.toPeopleSuggestionUserListItem(true, profileRoomId)
             })
-        }
-        return list
-    }
+            addAll(
+                suggestions.map {
+                    it.toPeopleSuggestionUserListItem(
+                        false,
+                        getProfileRoomForUser(it.userId)?.roomId
+                    )
+                }
+            )
+        }.distinctBy { it.id }
 
-    private suspend fun getKnownPeopleWithoutIgnored(
-        knowUsers: List<User>,
-        ignoredUsers: List<User>
-    ) = knowUsers.filterNot { knowUser ->
-        ignoredUsers.firstOrNull { ignoredUser -> ignoredUser.userId == knowUser.userId } != null
-    }.map {
-        val profileRoom = getProfileRoomForUser(it.userId)
-        val isFollowedByMe = profileRoom?.let { amIFollowThisUserProfile(it.roomId) } ?: false
-        it.toPeopleUserListItem(profileRoom?.roomId, false, isFollowedByMe)
+        val displayList = mutableListOf<PeopleListItem>()
+
+        val requestsItems = uniqueItemsList.filterIsInstance<PeopleRequestUserListItem>()
+        if (requestsItems.isNotEmpty()) {
+            displayList.add(PeopleHeaderItem.requests)
+            displayList.addAll(requestsItems)
+        }
+
+        val followingItems = uniqueItemsList.filterIsInstance<PeopleFollowingUserListItem>()
+        if (followingItems.isNotEmpty()) {
+            displayList.add(PeopleHeaderItem.followingUsersHeader)
+            displayList.addAll(followingItems)
+        }
+
+        val knownItems = uniqueItemsList.filter { it is PeopleSuggestionUserListItem && it.isKnown }
+        if (knownItems.isNotEmpty()) {
+            displayList.add(PeopleHeaderItem.knownUsersHeader)
+            displayList.addAll(knownItems)
+        }
+
+        val suggestionsItems =
+            uniqueItemsList.filter { it is PeopleSuggestionUserListItem && !it.isKnown }
+        if (suggestionsItems.isNotEmpty()) {
+            displayList.add(PeopleHeaderItem.suggestions)
+            displayList.addAll(suggestionsItems)
+        }
+
+        val ignoredItems = uniqueItemsList.filterIsInstance<PeopleIgnoredUserListItem>()
+        if (ignoredItems.isNotEmpty()) {
+            displayList.add(PeopleHeaderItem.ignoredUsers)
+            displayList.addAll(ignoredItems)
+        }
+
+        return displayList
     }
 
     private suspend fun getProfileRoomForUser(userId: String): Room? {
@@ -115,7 +137,10 @@ class PeopleDataSource(
         }
     }
 
-    private fun amIFollowThisUserProfile(profileRoomId: String) =
-        session?.roomService()?.getRoom(profileRoomId)?.membershipService()
+    private fun amIFollowThisUserProfile(profileRoomId: String?): Boolean {
+        profileRoomId ?: return false
+        return session?.roomService()?.getRoom(profileRoomId)?.membershipService()
             ?.getRoomMember(session.myUserId) != null
+    }
+
 }
