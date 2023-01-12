0d77fe54abba1d3ab59497fabe23333485241f3e
Taras Smakula <tarassmakula@gmail.com>
2023-01-12 18:04:38 +0200

    Create internal distributor

---
+++ app/src/main/java/org/futo/circles/feature/notifications/KeepInternalDistributor.kt
@@ -0,0 +1,9 @@
+package org.futo.circles.feature.notifications
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class KeepInternalDistributor : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
