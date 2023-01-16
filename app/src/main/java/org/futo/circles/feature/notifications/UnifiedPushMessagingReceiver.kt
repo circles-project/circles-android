55a85ed1f4299be922789de1ee8a61e5c797ac30
Taras Smakula <tarassmakula@gmail.com>
2023-01-16 17:53:19 +0200

    Start working on UnifiedPushMessagingReceiver

---
+++ app/src/main/java/org/futo/circles/feature/notifications/UnifiedPushMessagingReceiver.kt
@@ -0,0 +1,60 @@
+package org.futo.circles.feature.notifications
+
+import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.futo.circles.provider.MatrixSessionProvider
+import org.futo.circles.provider.PreferencesProvider
+import org.unifiedpush.android.connector.MessagingReceiver
+
+
+class UnifiedPushMessagingReceiver(
+    private val pushersManager: PushersManager,
+    private val pushParser: PushParser,
+    private val preferencesProvider: PreferencesProvider,
+    private val vectorPushHandler: VectorPushHandler,
+    private val guardServiceStarter: GuardServiceStarter,
+    private val unifiedPushHelper: UnifiedPushHelper
+) : MessagingReceiver() {
+
+    private val coroutineScope = CoroutineScope(SupervisorJob())
+
+    override fun onMessage(context: Context, message: ByteArray, instance: String) {
+        pushParser.parsePushDataUnifiedPush(message)?.let { vectorPushHandler.handle(it) }
+    }
+
+    override fun onNewEndpoint(context: Context, endpoint: String, instance: String) {
+        if (preferencesProvider.areNotificationEnabledForDevice() && MatrixSessionProvider.currentSession != null) {
+            if (unifiedPushHelper.getEndpointOrToken() != endpoint) {
+                preferencesProvider.storeUnifiedPushEndpoint(endpoint)
+                coroutineScope.launch {
+                    unifiedPushHelper.storeCustomOrDefaultGateway(endpoint) {
+                        unifiedPushHelper.getPushGateway()?.let {
+                            pushersManager.enqueueRegisterPusher(endpoint, it)
+                        }
+                    }
+                }
+            }
+        }
+        preferencesProvider.setFdroidSyncBackgroundMode(BackgroundSyncMode.FDROID_BACKGROUND_SYNC_MODE_DISABLED)
+        guardServiceStarter.stop()
+    }
+
+    override fun onRegistrationFailed(context: Context, instance: String) {
+        preferencesProvider.setFdroidSyncBackgroundMode(BackgroundSyncMode.FDROID_BACKGROUND_SYNC_MODE_FOR_REALTIME)
+        guardServiceStarter.start()
+    }
+
+    override fun onUnregistered(context: Context, instance: String) {
+        preferencesProvider.setFdroidSyncBackgroundMode(BackgroundSyncMode.FDROID_BACKGROUND_SYNC_MODE_FOR_REALTIME)
+        guardServiceStarter.start()
+        runBlocking {
+            try {
+                pushersManager.unregisterPusher(unifiedPushHelper.getEndpointOrToken().orEmpty())
+            } catch (ignore: Exception) {
+            }
+        }
+    }
+}
