51a271114639d7444848b8f910a1b02665cf979e
Taras Smakula <tarassmakula@gmail.com>
2023-01-17 15:45:35 +0200

    Add NotificationActionIds

---
+++ app/src/main/java/org/futo/circles/feature/notifications/NotificationActionIds.kt
@@ -0,0 +1,20 @@
+package org.futo.circles.feature.notifications
+
+import org.futo.circles.BuildConfig
+
+data class NotificationActionIds(
+    private val applicationId: String = BuildConfig.APPLICATION_ID
+) {
+
+    val join = "$applicationId.NotificationActions.JOIN_ACTION"
+    val reject = "$applicationId.NotificationActions.REJECT_ACTION"
+    val quickLaunch = "$applicationId.NotificationActions.QUICK_LAUNCH_ACTION"
+    val markRoomRead = "$applicationId.NotificationActions.MARK_ROOM_READ_ACTION"
+    val smartReply = "$applicationId.NotificationActions.SMART_REPLY_ACTION"
+    val dismissSummary = "$applicationId.NotificationActions.DISMISS_SUMMARY_ACTION"
+    val dismissRoom = "$applicationId.NotificationActions.DISMISS_ROOM_NOTIF_ACTION"
+    val tapToView = "$applicationId.NotificationActions.TAP_TO_VIEW_ACTION"
+    val diagnostic = "$applicationId.NotificationActions.DIAGNOSTIC"
+    val push = "$applicationId.PUSH"
+
+}
