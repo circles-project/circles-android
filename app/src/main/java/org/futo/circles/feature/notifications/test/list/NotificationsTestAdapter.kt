3925071b9b64303c1ee9532f970dc4c38aaba7be
Taras Smakula <tarassmakula@gmail.com>
2023-01-24 18:07:50 +0200

    Create adapter

--- app/src/main/java/org/futo/circles/feature/notifications/test/list/NotificationsTestAdapter.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/test/list/NotificationsTestAdapter.kt
@@ -1,4 +1,20 @@
 package org.futo.circles.feature.notifications.test.list
 
-class NotificationsTestAdapter {
+import android.view.ViewGroup
+import org.futo.circles.core.list.BaseRvAdapter
+import org.futo.circles.model.NotificationTestListItem
+
+class NotificationsTestAdapter :
+    BaseRvAdapter<NotificationTestListItem, NotificationTestViewHolder>(DefaultIdEntityCallback()) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NotificationTestViewHolder = NotificationTestViewHolder(parent)
+
+
+    override fun onBindViewHolder(holder: NotificationTestViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
 }
