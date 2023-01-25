05254f7adb4b10fedaac88de6d7e74794479cc89
Taras Smakula <tarassmakula@gmail.com>
2023-01-25 19:34:58 +0200

    Create NotificationAccountSettingsTest

---
+++ app/src/main/java/org/futo/circles/feature/notifications/test/task/NotificationAccountSettingsTest.kt
@@ -0,0 +1,53 @@
+package org.futo.circles.feature.notifications.test.task
+
+import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.futo.circles.R
+import org.futo.circles.extensions.coroutineScope
+import org.futo.circles.model.NotificationTestStatus
+import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.extensions.tryOrNull
+import org.matrix.android.sdk.api.session.pushrules.RuleIds
+import org.matrix.android.sdk.api.session.pushrules.RuleKind
+
+class NotificationAccountSettingsTest(private val context: Context) : BaseNotificationTest(
+    R.string.settings_troubleshoot_test_account_settings_title
+) {
+
+    override fun perform() {
+        val session = MatrixSessionProvider.currentSession ?: return
+        val defaultRule = session.pushRuleService().getPushRules().getAllRules()
+            .find { it.ruleId == RuleIds.RULE_ID_DISABLE_ALL }
+
+        if (defaultRule != null) {
+            if (!defaultRule.enabled) {
+                description =
+                    context.getString(R.string.settings_troubleshoot_test_account_settings_success)
+                quickFix = null
+                status = NotificationTestStatus.SUCCESS
+            } else {
+                description =
+                    context.getString(R.string.settings_troubleshoot_test_account_settings_failed)
+                quickFix = object : NotificationQuickFix() {
+                    override fun runFix() {
+                        session.coroutineScope.launch {
+                            tryOrNull {
+                                session.pushRuleService().updatePushRuleEnableStatus(
+                                    RuleKind.OVERRIDE,
+                                    defaultRule,
+                                    !defaultRule.enabled
+                                )
+                            }
+                            withContext(Dispatchers.Main) { perform() }
+                        }
+                    }
+                }
+                status = NotificationTestStatus.SUCCESS
+            }
+        } else {
+            status = NotificationTestStatus.FAILED
+        }
+    }
+}
