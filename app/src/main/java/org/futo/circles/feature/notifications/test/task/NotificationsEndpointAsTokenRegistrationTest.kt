97fabc892e835b57e9e1c2be8072af3ea6b921f9
Taras Smakula <tarassmakula@gmail.com>
2023-01-26 14:05:59 +0200

    Create NotificationsEndpointAsTokenRegistrationTest

---
+++ app/src/main/java/org/futo/circles/feature/notifications/test/task/NotificationsEndpointAsTokenRegistrationTest.kt
@@ -0,0 +1,46 @@
+package org.futo.circles.feature.notifications.test.task
+
+import android.content.Context
+import androidx.fragment.app.FragmentActivity
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import kotlinx.coroutines.launch
+import org.futo.circles.R
+import org.futo.circles.extensions.coroutineScope
+import org.futo.circles.feature.notifications.PushersManager
+import org.futo.circles.feature.notifications.UnifiedPushHelper
+import org.futo.circles.model.NotificationTestStatus
+import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.session.pushers.PusherState
+
+
+class NotificationsEndpointAsTokenRegistrationTest(
+    private val context: Context,
+    private val unifiedPushHelper: UnifiedPushHelper
+) : BaseNotificationTest(R.string.settings_troubleshoot_test_endpoint_registration_title) {
+
+    override fun perform() {
+        // Check if we have a registered pusher for this token
+        val endpoint = unifiedPushHelper.getEndpointOrToken() ?: run {
+            status = NotificationTestStatus.FAILED
+            return
+        }
+        val session = MatrixSessionProvider.currentSession ?: run {
+            status = NotificationTestStatus.FAILED
+            return
+        }
+        val pushers = session.pushersService().getPushers().filter {
+            it.pushKey == endpoint && it.state == PusherState.REGISTERED
+        }
+        if (pushers.isEmpty()) {
+            description = context.getString(
+                R.string.settings_troubleshoot_test_endpoint_registration_failed,
+                context.getString(R.string.unexpected_error)
+            )
+            status = NotificationTestStatus.FAILED
+        } else {
+            description = context.getString(R.string.settings_troubleshoot_test_endpoint_registration_success)
+            status = NotificationTestStatus.SUCCESS
+        }
+    }
+}
