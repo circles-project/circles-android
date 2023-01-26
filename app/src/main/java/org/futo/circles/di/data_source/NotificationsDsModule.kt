9fd480af21fcd91970abe8d245b68f456b957103
Taras Smakula <tarassmakula@gmail.com>
2023-01-26 16:38:06 +0200

    Setup di for notifications

--- app/src/main/java/org/futo/circles/di/data_source/NotificationsDsModule.kt
+++ app/src/main/java/org/futo/circles/di/data_source/NotificationsDsModule.kt
@@ -1,8 +1,11 @@
 package org.futo.circles.di.data_source
 
-import org.futo.circles.feature.log_in.LoginDataSource
+import org.futo.circles.feature.notifications.PushersManager
+import org.futo.circles.feature.notifications.UnifiedPushHelper
 import org.koin.dsl.module
 
 val notificationsDsModule = module {
+    factory { PushersManager(get(), get()) }
+    factory { UnifiedPushHelper(get(), get(), get()) }
 
 }
