4691ab79f5e163d7a9e9d7c13499454ace1fd3d2
Taras Smakula <tarassmakula@gmail.com>
2023-01-26 14:04:13 +0200

    Create NotificationsBatteryOptimizationTest

---
+++ app/src/main/java/org/futo/circles/feature/notifications/test/task/NotificationsBatteryOptimizationTest.kt
@@ -0,0 +1,28 @@
+package org.futo.circles.feature.notifications.test.task
+
+import android.content.Context
+import android.os.PowerManager
+import androidx.core.content.getSystemService
+import org.futo.circles.R
+import org.futo.circles.model.NotificationTestStatus
+
+class NotificationsBatteryOptimizationTest(
+    private val context: Context
+) : BaseNotificationTest(R.string.settings_troubleshoot_test_battery_title) {
+
+    override fun perform() {
+        if (isIgnoringBatteryOptimizations()) {
+            description = context.getString(R.string.settings_troubleshoot_test_battery_success)
+            status = NotificationTestStatus.SUCCESS
+            quickFix = null
+        } else {
+            description = context.getString(R.string.settings_troubleshoot_test_battery_failed)
+            status = NotificationTestStatus.FAILED
+        }
+    }
+
+    private fun isIgnoringBatteryOptimizations(): Boolean {
+        return context.getSystemService<PowerManager>()
+            ?.isIgnoringBatteryOptimizations(context.packageName) == true
+    }
+}
