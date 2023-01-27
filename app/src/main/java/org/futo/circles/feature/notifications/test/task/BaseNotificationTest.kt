20e4c18a86bcfee20e9f60216f550b138b59f84b
Taras Smakula <tarassmakula@gmail.com>
2023-01-27 16:40:56 +0200

    Add logic to base notification test

--- app/src/main/java/org/futo/circles/feature/notifications/test/task/BaseNotificationTest.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/test/task/BaseNotificationTest.kt
@@ -1,6 +1,7 @@
 package org.futo.circles.feature.notifications.test.task
 
 import androidx.annotation.StringRes
+import org.futo.circles.model.NotificationTestListItem
 import org.futo.circles.model.NotificationTestStatus
 
 abstract class BaseNotificationTest(@StringRes val titleResId: Int) {
@@ -8,5 +9,34 @@ abstract class BaseNotificationTest(@StringRes val titleResId: Int) {
     protected var description: String = ""
     protected var quickFix: NotificationQuickFix? = null
     protected var status: NotificationTestStatus = NotificationTestStatus.IDLE
-    abstract fun perform()
+    protected abstract fun perform()
+
+    private var onTestUpdateListener: ((NotificationTestListItem) -> Unit)? = null
+    fun runTest(updateListener: (NotificationTestListItem) -> Unit) {
+        onTestUpdateListener = updateListener
+        status = NotificationTestStatus.RUNNING
+        description = ""
+        quickFix = null
+        updateTestInfo()
+        perform()
+        updateTestInfo()
+    }
+
+    fun runFix() {
+        status = NotificationTestStatus.RUNNING
+        description = ""
+        quickFix = null
+        updateTestInfo()
+        quickFix?.runFix()
+        updateTestInfo()
+    }
+
+    fun toListItem() = NotificationTestListItem(
+        titleResId, description, status, quickFix != null
+    )
+
+    protected fun updateTestInfo() {
+        onTestUpdateListener?.invoke(toListItem())
+    }
+
 }
