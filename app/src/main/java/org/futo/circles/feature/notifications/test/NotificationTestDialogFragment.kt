36e2843719a7a0c1912ef9fddba263576568a97b
Taras Smakula <tarassmakula@gmail.com>
2023-01-27 16:41:31 +0200

    Add test receivers to fragment

--- app/src/main/java/org/futo/circles/feature/notifications/test/NotificationTestDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/test/NotificationTestDialogFragment.kt
@@ -1,11 +1,20 @@
 package org.futo.circles.feature.notifications.test
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import org.futo.circles.R
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.databinding.DialogFragmentNotificationsTestBinding
+import org.futo.circles.extensions.observeData
+import org.futo.circles.feature.notifications.NotificationActionIds
 import org.futo.circles.feature.notifications.test.list.NotificationsTestAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.matrix.android.sdk.api.extensions.tryOrNull
 
 class NotificationTestDialogFragment :
     BaseFullscreenDialogFragment(DialogFragmentNotificationsTestBinding::inflate) {
@@ -16,7 +25,23 @@ class NotificationTestDialogFragment :
         getBinding() as DialogFragmentNotificationsTestBinding
     }
 
-    private val testAdapter by lazy { NotificationsTestAdapter() }
+    private val testAdapter by lazy {
+        NotificationsTestAdapter {
+            viewModel.onFixNotificationTest(it)
+        }
+    }
+
+    private val broadcastReceiverPush = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            viewModel.onTestPushReceived()
+        }
+    }
+
+    private val broadcastReceiverNotification = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            viewModel.onTestPushClicked()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,11 +49,40 @@ class NotificationTestDialogFragment :
         setupObservers()
     }
 
+    override fun onResume() {
+        super.onResume()
+        tryOrNull(getString(R.string.unable_to_register_receiver)) {
+            LocalBroadcastManager.getInstance(requireContext())
+                .registerReceiver(broadcastReceiverPush, IntentFilter(NotificationActionIds.push))
+        }
+        tryOrNull(getString(R.string.unable_to_register_receiver)) {
+            LocalBroadcastManager.getInstance(requireContext())
+                .registerReceiver(
+                    broadcastReceiverNotification,
+                    IntentFilter(NotificationActionIds.diagnostic)
+                )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        tryOrNull {
+            LocalBroadcastManager.getInstance(requireContext())
+                .unregisterReceiver(broadcastReceiverPush)
+        }
+        tryOrNull {
+            LocalBroadcastManager.getInstance(requireContext())
+                .unregisterReceiver(broadcastReceiverNotification)
+        }
+    }
+
     private fun setupViews() {
         binding.rvNotificationsTests.adapter = testAdapter
     }
 
     private fun setupObservers() {
-
+        viewModel.testsLiveData.observeData(this) {
+            testAdapter.submitList(it)
+        }
     }
 }
