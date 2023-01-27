d0e32595d85706980692067289f2f51505e58b46
Taras Smakula <tarassmakula@gmail.com>
2023-01-27 16:41:13 +0200

    Implement test viewmodel

--- app/src/main/java/org/futo/circles/feature/notifications/test/NotificationTestViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/test/NotificationTestViewModel.kt
@@ -3,19 +3,45 @@ package org.futo.circles.feature.notifications.test
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.futo.circles.feature.notifications.NotificationTestsProvider
-import org.futo.circles.feature.notifications.test.task.BaseNotificationTest
+import org.futo.circles.feature.notifications.test.task.TestPushClicker
+import org.futo.circles.feature.notifications.test.task.TestPushDisplayEvenReceiver
+import org.futo.circles.model.NotificationTestListItem
 
 class NotificationTestViewModel(
-    private val testProvider: NotificationTestsProvider
+    testProvider: NotificationTestsProvider
 ) : ViewModel() {
 
-    val testsLiveData = MutableLiveData<BaseNotificationTest>()
+    val testsLiveData = MutableLiveData<List<NotificationTestListItem>>()
+    private val testList = testProvider.getTestsList()
 
     init {
         setupTests()
     }
 
     private fun setupTests() {
+        testsLiveData.postValue(testList.map { it.toListItem() })
+        testList.forEach {
+            it.runTest { updateTestInfo(it) }
+        }
+    }
 
+    private fun updateTestInfo(item: NotificationTestListItem) {
+        val list = testsLiveData.value?.toMutableList() ?: mutableListOf()
+        val index = list.indexOfFirst { it.id == item.id }.takeIf { it != -1 } ?: return
+        list[index] = item
+        testsLiveData.postValue(list)
+    }
+
+    fun onTestPushReceived() {
+        testList.filterIsInstance<TestPushDisplayEvenReceiver>()
+            .forEach { it.onTestPushDisplayed() }
+    }
+
+    fun onTestPushClicked() {
+        testList.filterIsInstance<TestPushClicker>().forEach { it.onTestPushClicked() }
+    }
+
+    fun onFixNotificationTest(id: Int) {
+        testList.firstOrNull { it.titleResId == id }?.runFix()
     }
 }
