5bddf35c946bf35f1de8c3114133193a79ad4dbe
Taras Smakula <tarassmakula@gmail.com>
2023-01-27 16:41:42 +0200

    Fix list item binding

--- app/src/main/java/org/futo/circles/feature/notifications/test/list/NotificationTestViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/test/list/NotificationTestViewHolder.kt
@@ -4,24 +4,33 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.futo.circles.R
 import org.futo.circles.core.list.ViewBindingHolder
+import org.futo.circles.core.list.context
 import org.futo.circles.databinding.ListItemNotificationTestBinding
 import org.futo.circles.extensions.gone
+import org.futo.circles.extensions.onClick
+import org.futo.circles.extensions.setIsVisible
 import org.futo.circles.extensions.visible
 import org.futo.circles.model.NotificationTestListItem
 import org.futo.circles.model.NotificationTestStatus
 
 class NotificationTestViewHolder(
-    parent: ViewGroup
+    parent: ViewGroup,
+    onFixClicked: (Int) -> Unit
 ) : RecyclerView.ViewHolder(inflate(parent, ListItemNotificationTestBinding::inflate)) {
 
     private companion object : ViewBindingHolder
 
     private val binding = baseBinding as ListItemNotificationTestBinding
 
+    init {
+        onClick(binding.btnFix) { position -> onFixClicked(position) }
+    }
+
     fun bind(data: NotificationTestListItem) {
         with(binding) {
-            tvName.text = data.name
+            tvName.text = context.getString(data.titleId)
             tvMessage.text = data.message
+            btnFix.setIsVisible(data.hasFix)
             when (data.status) {
                 NotificationTestStatus.RUNNING -> {
                     ivTestStatus.gone()
