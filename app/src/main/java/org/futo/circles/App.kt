96fd0792b74b4962a637ebfb74a6e82d1a2dcb82
Taras Smakula <tarassmakula@gmail.com>
2023-01-30 13:52:17 +0200

    Setup notification chanels

--- app/src/main/java/org/futo/circles/App.kt
+++ app/src/main/java/org/futo/circles/App.kt
@@ -1,16 +1,24 @@
 package org.futo.circles
 
 import android.app.Application
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
 import com.vanniktech.emoji.EmojiManager
 import com.vanniktech.emoji.google.GoogleEmojiProvider
 import org.futo.circles.di.applicationModules
+import org.futo.circles.feature.notifications.FcmHelper
+import org.futo.circles.feature.notifications.NotificationUtils
 import org.futo.circles.provider.MatrixSessionProvider
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 
 class App : Application() {
 
+    private val notificationUtils: NotificationUtils by inject()
+    private val fcmHelper: FcmHelper by inject()
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -19,5 +27,20 @@ class App : Application() {
         }
         MatrixSessionProvider.initSession(applicationContext)
         EmojiManager.install(GoogleEmojiProvider())
+        notificationUtils.createNotificationChannels()
+        setupLifecycleObserver()
+    }
+
+    private fun setupLifecycleObserver() {
+        ProcessLifecycleOwner.get().lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onResume(owner: LifecycleOwner) {
+                fcmHelper.onEnterForeground()
+                MatrixSessionProvider.currentSession?.syncService()?.stopAnyBackgroundSync()
+            }
+
+            override fun onPause(owner: LifecycleOwner) {
+                fcmHelper.onEnterBackground()
+            }
+        })
     }
 }
