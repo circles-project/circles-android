5c47eae22d78b92a1ec37f93738f5b1d4f412d67
Taras Smakula <tarassmakula@gmail.com>
2023-02-03 16:30:18 +0200

    Create guard service starter for flavours

--- app/src/fdroid/java/org/futo/circles/di/FlavourModule.kt
+++ app/src/fdroid/java/org/futo/circles/di/FlavourModule.kt
@@ -1,23 +1,22 @@
 package org.futo.circles.di
 
 import org.futo.circles.feature.notifications.FcmHelper
+import org.futo.circles.feature.notifications.GuardServiceStarter
 import org.futo.circles.feature.notifications.test.task.NotificationTestsProvider
 import org.futo.circles.notifications.BackgroundSyncStarter
 import org.futo.circles.notifications.FdroidFcmHelper
+import org.futo.circles.notifications.FdroidGuardServiceStarter
 import org.futo.circles.notifications.test.FdroidNotificationTestProvider
 import org.futo.circles.notifications.test.NotificationsBackgroundRestrictionsTest
-import org.futo.circles.notifications.test.NotificationsBatteryOptimizationTest
 import org.koin.dsl.module
 
 val flavourModule = module {
     factory<FcmHelper> { FdroidFcmHelper(get(), get()) }
-    factory { BackgroundSyncStarter(get(), get()) }
+    factory { BackgroundSyncStarter(get()) }
 
     factory { NotificationsBackgroundRestrictionsTest(get()) }
-    factory { NotificationsBatteryOptimizationTest(get()) }
     factory<NotificationTestsProvider> {
         FdroidNotificationTestProvider(
-            get(),
             get(),
             get(),
             get(),
@@ -27,4 +26,5 @@ val flavourModule = module {
             get()
         )
     }
+    single<GuardServiceStarter> { FdroidGuardServiceStarter(get()) }
 }
