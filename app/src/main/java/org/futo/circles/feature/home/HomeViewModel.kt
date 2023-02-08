bd218cf48bac7638118a938657ae67d1f07e90d4
Taras Smakula <tarassmakula@gmail.com>
2023-02-08 18:13:34 +0200

    Add observeRoomsAndBuildShortcuts

--- app/src/main/java/org/futo/circles/feature/home/HomeViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/home/HomeViewModel.kt
@@ -2,8 +2,10 @@ package org.futo.circles.feature.home
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.feature.notifications.PushersManager
+import org.futo.circles.feature.notifications.ShortcutsHandler
 import org.futo.circles.model.CIRCLE_TAG
 import org.futo.circles.model.GROUP_TYPE
 import org.futo.circles.provider.MatrixSessionProvider
@@ -12,10 +14,15 @@ import org.matrix.android.sdk.api.session.room.model.RoomSummary
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
 class HomeViewModel(
-    private val pushersManager: PushersManager
+    private val pushersManager: PushersManager,
+    private val shortcutsHandler: ShortcutsHandler
 ) : ViewModel() {
 
     val notificationLiveData = SingleEventLiveData<String>()
+
+    init {
+        shortcutsHandler.observeRoomsAndBuildShortcuts(viewModelScope)
+    }
 
     fun registerPushNotifications(context: Context) {
         pushersManager.registerPushNotifications(context)
