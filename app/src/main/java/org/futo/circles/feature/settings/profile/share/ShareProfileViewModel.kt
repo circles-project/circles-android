96f792f3e04df65e2eb15dd89f691092cdf6badb
Taras Smakula <tarassmakula@gmail.com>
2023-02-22 15:34:41 +0200

    Create share profile view model

---
+++ app/src/main/java/org/futo/circles/feature/settings/profile/share/ShareProfileViewModel.kt
@@ -0,0 +1,9 @@
+package org.futo.circles.feature.settings.profile.share
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ShareProfileViewModel : ViewModel() {
+
+    val qrProfileLiveData = MutableLiveData<String>()
+}
