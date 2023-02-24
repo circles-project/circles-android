08702a5959d34e676ed4320ffde6cd1cce1fdabf
Taras Smakula <tarassmakula@gmail.com>
2023-02-24 18:13:15 +0200

    Auto accept knock invites

--- app/src/main/java/org/futo/circles/feature/home/HomeViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import org.futo.circles.core.SingleEventLiveData
+import org.futo.circles.extensions.launchBg
 import org.futo.circles.feature.notifications.PushersManager
 import org.futo.circles.feature.notifications.ShortcutsHandler
 import org.futo.circles.model.CIRCLE_TAG
@@ -11,17 +12,19 @@ import org.futo.circles.model.GROUP_TYPE
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.room.model.Membership
 import org.matrix.android.sdk.api.session.room.model.RoomSummary
+import org.matrix.android.sdk.api.session.room.model.RoomType
 import org.matrix.android.sdk.api.session.room.roomSummaryQueryParams
 
 class HomeViewModel(
     private val pushersManager: PushersManager,
-    private val shortcutsHandler: ShortcutsHandler
+    shortcutsHandler: ShortcutsHandler
 ) : ViewModel() {
 
     val notificationLiveData = SingleEventLiveData<String>()
 
     init {
         shortcutsHandler.observeRoomsAndBuildShortcuts(viewModelScope)
+        autoAcceptInviteOnKnock()
     }
 
     fun registerPushNotifications(context: Context) {
@@ -47,5 +50,14 @@ class HomeViewModel(
             circles.firstOrNull { it.spaceChildren?.firstOrNull { it.childRoomId == summary.roomId } != null }
 
         return parentCircle?.roomId
+    }
+
+    private fun autoAcceptInviteOnKnock() {
+        val session = MatrixSessionProvider.currentSession ?: return
+        session.roomService().getRoomSummaries(roomSummaryQueryParams {
+            memberships = listOf(Membership.INVITE)
+        }).firstOrNull { it.roomType == RoomType.SPACE }?.let {
+            launchBg { session.roomService().joinRoom(it.roomId) }
+        }
     }
 }
