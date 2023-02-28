3116b9c407a279796c6d358fd1a5c5ecd72a0332
Taras Smakula <tarassmakula@gmail.com>
2023-02-28 15:13:27 +0200

    Add handelPrivateSharedVisibilityUpdate for update room

--- app/src/main/java/org/futo/circles/feature/room/update/UpdateRoomDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/room/update/UpdateRoomDataSource.kt
@@ -2,39 +2,48 @@ package org.futo.circles.feature.room.update
 
 import android.content.Context
 import android.net.Uri
+import org.futo.circles.core.matrix.room.CreateRoomDataSource
+import org.futo.circles.core.utils.getTimelineRoomFor
+import org.futo.circles.core.utils.isCircleShared
 import org.futo.circles.extensions.createResult
 import org.futo.circles.extensions.getFilename
 import org.futo.circles.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.getRoom
-import org.matrix.android.sdk.api.session.room.model.GuestAccess
-import org.matrix.android.sdk.api.session.room.model.RoomJoinRules
 import java.util.*
 
 class UpdateRoomDataSource(
     roomId: String,
-    private val context: Context
+    private val context: Context,
+    private val createRoomDataSource: CreateRoomDataSource
 ) {
 
     private val room = MatrixSessionProvider.currentSession?.getRoom(roomId)
 
     fun getRoomSummary() = room?.roomSummary()
 
-    suspend fun updateRoom(name: String, topic: String, uri: Uri?, joinRules: RoomJoinRules) =
+    suspend fun updateRoom(name: String, topic: String, uri: Uri?, isPublic: Boolean) =
         createResult {
             if (isNameChanged(name)) room?.stateService()?.updateName(name)
             if (isTopicChanged(topic)) room?.stateService()?.updateTopic(topic)
-            if (isJoinRulesChanged(joinRules)) room?.stateService()
-                ?.updateJoinRule(joinRules, GuestAccess.CanJoin)
+            if (isPrivateSharedChanged(isPublic)) handelPrivateSharedVisibilityUpdate(isPublic)
             uri?.let {
                 room?.stateService()
                     ?.updateAvatar(it, it.getFilename(context) ?: UUID.randomUUID().toString())
             }
         }
 
+    private suspend fun handelPrivateSharedVisibilityUpdate(isPublic: Boolean) {
+        val timelineId = room?.roomId?.let { getTimelineRoomFor(it)?.roomId } ?: return
+        if (isPublic) createRoomDataSource.addToSharedCircles(timelineId)
+        else createRoomDataSource.removeFromSharedCircles(timelineId)
+    }
+
     fun isNameChanged(newName: String) = room?.roomSummary()?.displayName != newName
 
     fun isTopicChanged(newTopic: String) = room?.roomSummary()?.topic != newTopic
 
-    fun isJoinRulesChanged(joinRules: RoomJoinRules) = room?.roomSummary()?.joinRules != joinRules
+    fun isPrivateSharedChanged(isPublic: Boolean) = room?.roomId?.let {
+        isCircleShared(it) != isPublic
+    } ?: false
 
 }
