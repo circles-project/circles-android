0b8241775dd344ed6fb6fe8763109f3adc9328f9
Taras Smakula <tarassmakula@gmail.com>
2023-03-06 17:45:52 +0200

    Implement view model logic

--- app/src/main/java/org/futo/circles/feature/people/user/UserViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/people/user/UserViewModel.kt
@@ -1,12 +1,28 @@
 package org.futo.circles.feature.people.user
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.collectLatest
+import org.futo.circles.extensions.launchUi
+import org.futo.circles.model.TimelineListItem
 
 class UserViewModel(
-    userDataSource: UserDataSource
+    private val userDataSource: UserDataSource
 ) : ViewModel() {
 
     val userLiveData = userDataSource.userLiveData
-    val timelineLiveDataLiveData = userDataSource.getTimelinesFlow().asLiveData()
+    val timelineLiveDataLiveData = MutableLiveData<List<TimelineListItem>>()
+
+    init {
+        getUsersTimelines()
+    }
+
+    private fun getUsersTimelines() {
+        launchUi {
+            userDataSource.getTimelinesFlow().collectLatest {
+                timelineLiveDataLiveData.postValue(it)
+            }
+        }
+    }
+
 }
