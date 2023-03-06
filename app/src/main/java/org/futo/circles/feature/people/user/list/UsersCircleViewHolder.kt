152063bf555ecd7c17c3c2aba2c0751b664c7275
Taras Smakula <tarassmakula@gmail.com>
2023-03-06 17:07:05 +0200

    Add view types to adapter

--- app/src/main/java/org/futo/circles/feature/people/user/list/UsersCircleViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/people/user/list/UsersCircleViewHolder.kt
@@ -1,25 +1,49 @@
 package org.futo.circles.feature.people.user.list
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.futo.circles.core.list.ViewBindingHolder
+import org.futo.circles.core.list.context
+import org.futo.circles.databinding.ListItemInviteHeaderBinding
 import org.futo.circles.databinding.ListItemSelectRoomBinding
 import org.futo.circles.extensions.loadProfileIcon
-import org.futo.circles.model.JoinedCircleListItem
+import org.futo.circles.model.TimelineHeaderItem
+import org.futo.circles.model.TimelineListItem
+import org.futo.circles.model.TimelineRoomListItem
 
-class UsersCircleViewHolder(
+
+abstract class UserTimelineViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun bind(data: TimelineListItem)
+}
+
+class UsersTimelineRoomViewHolder(
     parent: ViewGroup
-) : RecyclerView.ViewHolder(inflate(parent, ListItemSelectRoomBinding::inflate)) {
+) : UserTimelineViewHolder(inflate(parent, ListItemSelectRoomBinding::inflate)) {
 
     private companion object : ViewBindingHolder
 
     private val binding = baseBinding as ListItemSelectRoomBinding
 
-
-    fun bind(data: JoinedCircleListItem) {
+    override fun bind(data: TimelineListItem) {
+        if (data !is TimelineRoomListItem) return
         with(binding) {
             tvCircleName.text = data.info.title
             binding.ivCircleImage.loadProfileIcon(data.info.avatarUrl, data.info.title)
         }
+    }
+}
+
+class UserTimelineHeaderViewHolder(
+    parent: ViewGroup,
+) : UserTimelineViewHolder(inflate(parent, ListItemInviteHeaderBinding::inflate)) {
+
+    private companion object : ViewBindingHolder
+
+    private val binding = baseBinding as ListItemInviteHeaderBinding
+
+    override fun bind(data: TimelineListItem) {
+        if (data !is TimelineHeaderItem) return
+        binding.tvHeader.text = context.getString(data.titleRes)
     }
 }
--- app/src/main/java/org/futo/circles/feature/people/user/list/UsersCirclesAdapter.kt
+++ app/src/main/java/org/futo/circles/feature/people/user/list/UsersCirclesAdapter.kt
@@ -2,18 +2,30 @@ package org.futo.circles.feature.people.user.list
 
 import android.view.ViewGroup
 import org.futo.circles.core.list.BaseRvAdapter
-import org.futo.circles.model.JoinedCircleListItem
+import org.futo.circles.feature.people.user.list.TimelineViewType.*
+import org.futo.circles.model.TimelineHeaderItem
+import org.futo.circles.model.TimelineListItem
+import org.futo.circles.model.TimelineRoomListItem
 
-class UsersCirclesAdapter() :
-    BaseRvAdapter<JoinedCircleListItem, UsersCircleViewHolder>(DefaultIdEntityCallback()) {
+private enum class TimelineViewType { Header, Room }
+class UsersCirclesAdapter :
+    BaseRvAdapter<TimelineListItem, UserTimelineViewHolder>(DefaultIdEntityCallback()) {
+
+    override fun getItemViewType(position: Int): Int = when (getItem(position)) {
+        is TimelineHeaderItem -> Header
+        is TimelineRoomListItem -> Room
+    }.ordinal
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): UsersCircleViewHolder = UsersCircleViewHolder(parent)
+    ): UserTimelineViewHolder = when (values()[viewType]) {
+        Header -> UserTimelineHeaderViewHolder(parent)
+        Room -> UsersTimelineRoomViewHolder(parent)
+    }
 
 
-    override fun onBindViewHolder(holder: UsersCircleViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: UserTimelineViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
