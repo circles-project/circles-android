cbea23cb37b27478c18ef9fe3b60b516e8f9512f
Taras Smakula <tarassmakula@gmail.com>
2023-03-07 17:28:55 +0200

    Add list section

--- app/src/main/java/org/futo/circles/feature/circles/CirclesDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/circles/CirclesDataSource.kt
@@ -5,6 +5,7 @@ import org.futo.circles.core.utils.isCircleShared
 import org.futo.circles.extensions.createResult
 import org.futo.circles.mapping.toInviteCircleListItem
 import org.futo.circles.mapping.toJoinedCircleListItem
+import org.futo.circles.mapping.toRequestCircleListItem
 import org.futo.circles.model.CIRCLE_TAG
 import org.futo.circles.model.CircleListItem
 import org.futo.circles.model.CirclesHeaderItem
@@ -23,23 +24,22 @@ class CirclesDataSource {
     private fun buildCirclesList(list: List<RoomSummary>): List<CircleListItem> {
         val invites =
             list.filter { isInviteToCircleTimeline(it) }.map { it.toInviteCircleListItem() }
+        val requests =
+            list.filter { isRequestToCircleTimeline(it) }.map { it.toRequestCircleListItem() }
         val joinedCircles = list.filter { isJoinedCircle(it) }
         val sharedCircles =
             joinedCircles.filter { joinedCircle -> isCircleShared(joinedCircle.roomId) }
         val privateCircles = joinedCircles - sharedCircles.toSet()
 
-        val displayList = mutableListOf<CircleListItem>()
-        if (invites.isNotEmpty()) {
-            displayList.add(CirclesHeaderItem.invitesCirclesHeader)
-            displayList.addAll(invites)
-        }
-        if (sharedCircles.isNotEmpty()) {
-            displayList.add(CirclesHeaderItem.sharedCirclesHeader)
-            displayList.addAll(sharedCircles.map { it.toJoinedCircleListItem(true) })
-        }
-        if (privateCircles.isNotEmpty()) {
-            displayList.add(CirclesHeaderItem.privateCirclesHeader)
-            displayList.addAll(privateCircles.map { it.toJoinedCircleListItem(false) })
+        val displayList = mutableListOf<CircleListItem>().apply {
+            addSection(CirclesHeaderItem.requestsCirclesHeader, requests)
+            addSection(CirclesHeaderItem.invitesCirclesHeader, invites)
+            addSection(
+                CirclesHeaderItem.sharedCirclesHeader,
+                sharedCircles.map { it.toJoinedCircleListItem(true) })
+            addSection(
+                CirclesHeaderItem.privateCirclesHeader,
+                privateCircles.map { it.toJoinedCircleListItem(false) })
         }
         return displayList
     }
@@ -50,7 +50,20 @@ class CirclesDataSource {
     private fun isInviteToCircleTimeline(summary: RoomSummary) =
         summary.roomType == TIMELINE_TYPE && summary.membership == Membership.INVITE
 
+    private fun isRequestToCircleTimeline(summary: RoomSummary) =
+         summary.membership == Membership.KNOCK
+
     suspend fun rejectInvite(roomId: String) = createResult {
         MatrixSessionProvider.currentSession?.roomService()?.leaveRoom(roomId)
+    }
+
+    private fun MutableList<CircleListItem>.addSection(
+        title: CirclesHeaderItem,
+        items: List<CircleListItem>
+    ) {
+        if (items.isNotEmpty()) {
+            add(title)
+            addAll(items)
+        }
     }
 }
