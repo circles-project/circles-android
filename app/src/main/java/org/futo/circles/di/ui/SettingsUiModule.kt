4484a52f5f0a6744c92a7ecf034c857b779f893d
Taras Smakula <tarassmakula@gmail.com>
2023-03-09 15:59:17 +0200

    Update di

--- app/src/main/java/org/futo/circles/di/ui/SettingsUiModule.kt
+++ app/src/main/java/org/futo/circles/di/ui/SettingsUiModule.kt
@@ -20,7 +20,7 @@ val settingsUiModule = module {
     viewModel { EditProfileViewModel(get()) }
     viewModel { ChangePasswordViewModel(get()) }
     viewModel { ActiveSessionsViewModel(get()) }
-    viewModel { (userId: String) -> UserViewModel(get { parametersOf(userId) }) }
+    viewModel { (userId: String) -> UserViewModel(userId, get { parametersOf(userId) }, get()) }
     viewModel { SystemNoticesCountSharedViewModel() }
     viewModel { (roomId: String, type: CircleRoomTypeArg) ->
         SystemNoticesTimelineViewModel(get { parametersOf(roomId, type) })
