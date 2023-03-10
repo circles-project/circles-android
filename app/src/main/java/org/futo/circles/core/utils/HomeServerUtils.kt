dc064726a102bc7a1b64599143ad58711355eec0
Taras Smakula <tarassmakula@gmail.com>
2023-03-10 13:53:36 +0200

    Remove matrix sufix from home server config

--- app/src/main/java/org/futo/circles/core/utils/HomeServerUtils.kt
+++ app/src/main/java/org/futo/circles/core/utils/HomeServerUtils.kt
@@ -5,23 +5,9 @@ import org.matrix.android.sdk.api.auth.data.HomeServerConnectionConfig
 
 object HomeServerUtils {
 
-    private const val MATRIX_DOMAIN_PREFIX = "matrix."
+    fun buildHomeServerConfigFromDomain(domain: String) = HomeServerConnectionConfig
+        .Builder()
+        .withHomeServerUri(Uri.parse("https://$domain"))
+        .build()
 
-    fun buildHomeServerConfigFromDomain(domain: String) = buildHomeServerConfig(
-        getHomeServerUrlFromDomain(domain)
-    )
-
-    private fun getHomeServerUrlFromDomain(domain: String): String {
-        var formattedDomain = domain
-        if (!domain.startsWith(MATRIX_DOMAIN_PREFIX)) formattedDomain =
-            MATRIX_DOMAIN_PREFIX + domain
-        return "https://$formattedDomain"
-    }
-
-    private fun buildHomeServerConfig(url: String): HomeServerConnectionConfig {
-        return HomeServerConnectionConfig
-            .Builder()
-            .withHomeServerUri(Uri.parse(url))
-            .build()
-    }
 }
