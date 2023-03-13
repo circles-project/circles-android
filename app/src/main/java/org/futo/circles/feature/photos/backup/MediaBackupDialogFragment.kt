b00669c84cd4a04165a7902828c0e6c30b5031b6
Taras Smakula <tarassmakula@gmail.com>
2023-03-13 16:54:26 +0200

    Handle backup click ui behaviour

--- app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDialogFragment.kt
@@ -1,7 +1,10 @@
 package org.futo.circles.feature.photos.backup
 
+import android.os.Bundle
+import android.view.View
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.databinding.DialogFragmentMediaBackupBinding
+import org.futo.circles.extensions.setIsVisible
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MediaBackupDialogFragment :
@@ -11,5 +14,26 @@ class MediaBackupDialogFragment :
 
     private val binding by lazy {
         getBinding() as DialogFragmentMediaBackupBinding
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObservers()
+    }
+
+    private fun setupViews() {
+        with(binding) {
+            lBackupSwitchContainer.setOnClickListener {
+                svBackup.isChecked = !svBackup.isChecked
+            }
+            svBackup.setOnCheckedChangeListener { _, isChecked ->
+                groupBackupFolders.setIsVisible(isChecked)
+            }
+        }
+    }
+
+    private fun setupObservers() {
+
     }
 }
