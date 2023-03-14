a234581339a704fd0c4566945ec61d6f80c86dce
Taras Smakula <tarassmakula@gmail.com>
2023-03-14 17:39:52 +0200

    Implement get media folders list

--- app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupViewModel.kt
@@ -1,6 +1,18 @@
 package org.futo.circles.feature.photos.backup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.futo.circles.extensions.launchBg
+import org.futo.circles.model.MediaFolder
 
-class MediaBackupViewModel : ViewModel() {
+class MediaBackupViewModel(
+    private val dataSource: MediaBackupDataSource
+) : ViewModel() {
+
+    val mediaFolderLiveData = MutableLiveData<List<MediaFolder>>()
+
+    fun getMediaFolders() {
+        launchBg { mediaFolderLiveData.postValue(dataSource.getMediaFolders()) }
+    }
+
 }
