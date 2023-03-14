eb8d78fc1358d207824de8ebaa19a3b3342c2e81
Taras Smakula <tarassmakula@gmail.com>
2023-03-14 18:04:34 +0200

    Add adapter to list

--- app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDialogFragment.kt
@@ -3,14 +3,15 @@ package org.futo.circles.feature.photos.backup
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.DividerItemDecoration
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.core.picker.RuntimePermissionHelper
 import org.futo.circles.databinding.DialogFragmentMediaBackupBinding
 import org.futo.circles.extensions.observeData
 import org.futo.circles.extensions.setIsVisible
+import org.futo.circles.feature.photos.backup.list.MediaFoldersListAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MediaBackupDialogFragment :
@@ -23,14 +24,19 @@ class MediaBackupDialogFragment :
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private val permissionHelper =
+    private val readMediaPermissionHelper =
         RuntimePermissionHelper(this, Manifest.permission.READ_MEDIA_IMAGES)
+
+    private val foldersAdapter by lazy {
+        MediaFoldersListAdapter(
+            onItemCheckChanged = { id -> viewModel.onFolderBackupCheckChanged(id) }
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViews()
         setupObservers()
-        getFoldersList()
     }
 
     private fun setupViews() {
@@ -40,19 +46,24 @@ class MediaBackupDialogFragment :
             }
             svBackup.setOnCheckedChangeListener { _, isChecked ->
                 groupBackupFolders.setIsVisible(isChecked)
+                if (isChecked) getFoldersList()
+            }
+            rvDeviceFolders.apply {
+                adapter = foldersAdapter
+                addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
             }
         }
     }
 
     private fun setupObservers() {
         viewModel.mediaFolderLiveData.observeData(this) {
-            
+            foldersAdapter.submitList(it)
         }
     }
 
     private fun getFoldersList() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
-            permissionHelper.runWithPermission { viewModel.getMediaFolders() }
+            readMediaPermissionHelper.runWithPermission { viewModel.getMediaFolders() }
         else viewModel.getMediaFolders()
     }
 }
