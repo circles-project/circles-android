d9c95e27da169cc7a6a15f102b3b78eba397cf59
Taras Smakula <tarassmakula@gmail.com>
2023-03-14 17:40:06 +0200

    Add runtime permissions

--- app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDialogFragment.kt
@@ -1,9 +1,15 @@
 package org.futo.circles.feature.photos.backup
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.annotation.RequiresApi
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
+import org.futo.circles.core.picker.RuntimePermissionHelper
 import org.futo.circles.databinding.DialogFragmentMediaBackupBinding
+import org.futo.circles.extensions.observeData
 import org.futo.circles.extensions.setIsVisible
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -16,10 +22,15 @@ class MediaBackupDialogFragment :
         getBinding() as DialogFragmentMediaBackupBinding
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val permissionHelper =
+        RuntimePermissionHelper(this, Manifest.permission.READ_MEDIA_IMAGES)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViews()
         setupObservers()
+        getFoldersList()
     }
 
     private fun setupViews() {
@@ -34,6 +45,14 @@ class MediaBackupDialogFragment :
     }
 
     private fun setupObservers() {
+        viewModel.mediaFolderLiveData.observeData(this) {
+            
+        }
+    }
 
+    private fun getFoldersList() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            permissionHelper.runWithPermission { viewModel.getMediaFolders() }
+        else viewModel.getMediaFolders()
     }
 }
