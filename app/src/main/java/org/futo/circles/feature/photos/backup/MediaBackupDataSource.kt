1de716953e9a297f118c548fb7fc8e0f72fef4a2
Taras Smakula <tarassmakula@gmail.com>
2023-03-15 12:21:02 +0200

    Include videos and folder size to query

--- app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDataSource.kt
@@ -1,42 +1,57 @@
 package org.futo.circles.feature.photos.backup
 
 import android.content.Context
-import android.os.Build
 import android.provider.MediaStore
 import org.futo.circles.model.MediaFolderListItem
+import java.io.File
+import kotlin.math.log10
+import kotlin.math.pow
 
 class MediaBackupDataSource(private val context: Context) {
 
     fun getMediaFolders(): List<MediaFolderListItem> {
         val projection = arrayOf(
             MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-            MediaStore.Images.Media.BUCKET_ID
+            MediaStore.Images.Media.BUCKET_ID,
+            MediaStore.Images.Media.DATA
         )
-        val collection =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
-            else MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val collection = MediaStore.Files.getContentUri("external")
+        val selection =
+            "${MediaStore.Files.FileColumns.MEDIA_TYPE} = ? OR ${MediaStore.Files.FileColumns.MEDIA_TYPE} = ?"
+        val selectionArgs = arrayOf(
+            MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE.toString(),
+            MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO.toString()
+        )
+        val sortOrder = "${MediaStore.Images.Media.DATE_MODIFIED} DESC"
 
-        val foldersList = HashSet<MediaFolderListItem>()
+        val foldersList = mutableMapOf<Long, MediaFolderListItem>()
         context.contentResolver.query(
             collection,
             projection,
-            null,
-            null,
-            null
+            selection,
+            selectionArgs,
+            sortOrder
         )?.use { cursor ->
             val bucketNameColumnIndex =
                 cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
             val bucketIdColumnIndex =
                 cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_ID)
+            val mediaDataColumnId = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
 
             while (cursor.moveToNext()) {
                 val bucketId = cursor.getLong(bucketIdColumnIndex)
                 val bucketName = cursor.getString(bucketNameColumnIndex)
-                val mediaFolder = MediaFolderListItem(bucketId, bucketName)
-                foldersList.add(mediaFolder)
+                val filePath = cursor.getString(mediaDataColumnId)
+                val file = File(filePath)
+                val folderPath = file.parent ?: continue
+                val fileSize = file.length()
+                val size = if (foldersList.containsKey(bucketId))
+                    (foldersList[bucketId]?.size ?: 0) + fileSize
+                else fileSize
+                foldersList[bucketId] =
+                    MediaFolderListItem(bucketId, bucketName, folderPath, size)
             }
         }
-        return foldersList.toList()
+        return foldersList.values.toList()
     }
 }
