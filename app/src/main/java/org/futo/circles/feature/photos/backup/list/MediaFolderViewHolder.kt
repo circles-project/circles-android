00535e8937486597a1e64666f62d7e9381c35ae0
Taras Smakula <tarassmakula@gmail.com>
2023-03-15 12:19:45 +0200

    Add file size formatter

--- app/src/main/java/org/futo/circles/feature/photos/backup/list/MediaFolderViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/list/MediaFolderViewHolder.kt
@@ -6,6 +6,8 @@ import org.futo.circles.core.list.ViewBindingHolder
 import org.futo.circles.databinding.ListItemMediaFolderBinding
 import org.futo.circles.extensions.onClick
 import org.futo.circles.model.MediaFolderListItem
+import kotlin.math.log10
+import kotlin.math.pow
 
 class MediaFolderViewHolder(
     parent: ViewGroup,
@@ -26,7 +28,15 @@ class MediaFolderViewHolder(
     fun bind(data: MediaFolderListItem) {
         with(binding) {
             tvFolderName.text = data.displayName
+            tvSize.text = formatFileSize(data.size)
             svFolder.isChecked = data.isSelected
         }
+    }
+
+    private fun formatFileSize(size: Long): String {
+        if (size <= 0) return "0 B"
+        val units = arrayOf("B", "KB", "MB", "GB", "TB")
+        val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
+        return String.format("%.1f %s", size / 1024.0.pow(digitGroups.toDouble()), units[digitGroups])
     }
 }
