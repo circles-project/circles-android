c79a80f24d4a6be5f43bb8909db607ca17b6bb91
Taras Smakula <tarassmakula@gmail.com>
2023-03-16 17:36:44 +0200

    Implement account data logic in datasource

--- app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/MediaBackupDataSource.kt
@@ -2,17 +2,36 @@ package org.futo.circles.feature.photos.backup
 
 import android.content.Context
 import android.provider.MediaStore
+import org.futo.circles.core.ROOM_BACKUP_EVENT_TYPE
+import org.futo.circles.core.utils.getPhotosSpaceId
+import org.futo.circles.extensions.createResult
+import org.futo.circles.model.MediaBackupSettingsData
+import org.futo.circles.model.MediaBackupSettingsData.Companion.foldersKey
+import org.futo.circles.model.MediaBackupSettingsData.Companion.isBackupEnabledKey
 import org.futo.circles.model.MediaFolderListItem
+import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.session.events.model.Content
+import org.matrix.android.sdk.api.session.getRoom
 import java.io.File
-import kotlin.math.log10
-import kotlin.math.pow
 
 class MediaBackupDataSource(private val context: Context) {
 
-    fun getMediaFolders(): List<MediaFolderListItem> {
+    fun getInitialBackupSettings(): MediaBackupSettingsData {
+        val content = MatrixSessionProvider.currentSession?.getRoom(getPhotosSpaceId() ?: "")
+            ?.roomAccountDataService()
+            ?.getAccountDataEvent(ROOM_BACKUP_EVENT_TYPE)?.content
+        return createMediaBackupData(content)
+    }
+
+    suspend fun saveBackupSettings(data: MediaBackupSettingsData) = createResult {
+        MatrixSessionProvider.currentSession?.getRoom(getPhotosSpaceId() ?: "")
+            ?.roomAccountDataService()
+            ?.updateAccountData(ROOM_BACKUP_EVENT_TYPE, data.toMap())
+    }
+
+    fun getMediaFolders(selectedFoldersIds: List<String>): List<MediaFolderListItem> {
         val projection = arrayOf(
             MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-            MediaStore.Images.Media.BUCKET_ID,
             MediaStore.Images.Media.DATA
         )
         val collection = MediaStore.Files.getContentUri("external")
@@ -24,7 +43,7 @@ class MediaBackupDataSource(private val context: Context) {
         )
         val sortOrder = "${MediaStore.Images.Media.DATE_MODIFIED} DESC"
 
-        val foldersList = mutableMapOf<Long, MediaFolderListItem>()
+        val foldersList = mutableMapOf<String, MediaFolderListItem>()
         context.contentResolver.query(
             collection,
             projection,
@@ -34,24 +53,29 @@ class MediaBackupDataSource(private val context: Context) {
         )?.use { cursor ->
             val bucketNameColumnIndex =
                 cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
-            val bucketIdColumnIndex =
-                cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_ID)
             val mediaDataColumnId = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
 
             while (cursor.moveToNext()) {
-                val bucketId = cursor.getLong(bucketIdColumnIndex)
                 val bucketName = cursor.getString(bucketNameColumnIndex)
-                val filePath = cursor.getString(mediaDataColumnId)
-                val file = File(filePath)
+                val file = File(cursor.getString(mediaDataColumnId))
                 val folderPath = file.parent ?: continue
                 val fileSize = file.length()
-                val size = if (foldersList.containsKey(bucketId))
-                    (foldersList[bucketId]?.size ?: 0) + fileSize
+                val size = if (foldersList.containsKey(folderPath))
+                    (foldersList[folderPath]?.size ?: 0) + fileSize
                 else fileSize
-                foldersList[bucketId] =
-                    MediaFolderListItem(bucketId, bucketName, folderPath, size)
+                foldersList[folderPath] =
+                    MediaFolderListItem(
+                        bucketName, folderPath, size,
+                        selectedFoldersIds.contains(folderPath)
+                    )
             }
         }
         return foldersList.values.toList()
     }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun createMediaBackupData(content: Content?) = MediaBackupSettingsData(
+        (content?.get(isBackupEnabledKey) as? Boolean) ?: false,
+        (content?.get(foldersKey) as? List<String>) ?: emptyList()
+    )
 }
