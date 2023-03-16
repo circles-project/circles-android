a0bd0a6f0cbb87e575ec91dfe0b52a9c29003408
Taras Smakula <tarassmakula@gmail.com>
2023-03-16 17:35:35 +0200

    Make folder path as list item id

--- app/src/main/java/org/futo/circles/feature/photos/backup/list/MediaFoldersListAdapter.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/list/MediaFoldersListAdapter.kt
@@ -5,13 +5,13 @@ import org.futo.circles.core.list.BaseRvAdapter
 import org.futo.circles.model.MediaFolderListItem
 
 class MediaFoldersListAdapter(
-    private val onItemCheckChanged: (Long) -> Unit,
+    private val onItemCheckChanged: (String) -> Unit,
 ) : BaseRvAdapter<MediaFolderListItem, MediaFolderViewHolder>(DefaultIdEntityCallback()) {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         MediaFolderViewHolder(parent,
-            onCheckChanged = { position -> onItemCheckChanged(getItemId(position)) }
+            onCheckChanged = { position -> onItemCheckChanged(getItem(position).id) }
         )
 
     override fun onBindViewHolder(holder: MediaFolderViewHolder, position: Int) {
--- app/src/main/java/org/futo/circles/model/MediaFolderListItem.kt
+++ app/src/main/java/org/futo/circles/model/MediaFolderListItem.kt
@@ -3,9 +3,10 @@ package org.futo.circles.model
 import org.futo.circles.core.list.IdEntity
 
 data class MediaFolderListItem(
-    override val id: Long,
     val displayName: String,
     val path: String,
     val size: Long,
-    val isSelected: Boolean = false
-) : IdEntity<Long>
+    val isSelected: Boolean
+) : IdEntity<String> {
+    override val id: String = path
+}
