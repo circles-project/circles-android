b0700aa5398f191dedbd0ccd875f1dcec3a183f4
Taras Smakula <tarassmakula@gmail.com>
2023-04-03 17:32:22 +0300

    Setup views

--- app/src/main/java/org/futo/circles/feature/rageshake/BugReportViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/rageshake/BugReportViewModel.kt
@@ -1,4 +1,18 @@
 package org.futo.circles.feature.rageshake
 
-class BugReportViewModel {
+import androidx.lifecycle.ViewModel
+import org.futo.circles.core.SingleEventLiveData
+import org.futo.circles.extensions.Response
+import org.futo.circles.feature.sign_up.setup_profile.SetupProfileDataSource
+
+class BugReportViewModel(
+    profileDataSource: SetupProfileDataSource
+) : ViewModel() {
+
+    val sendReportLiveData = SingleEventLiveData<Response<Unit>>()
+    val threePidLiveData = profileDataSource.threePidLiveData
+
+    fun sendReport(description: String, email: String, sendLogs: Boolean, sendScreenshot: Boolean) {
+
+    }
 }
