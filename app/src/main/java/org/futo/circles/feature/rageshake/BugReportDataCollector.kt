2c6460f25a96b83f787ec3421efda4ed7f5eab7b
Taras Smakula <tarassmakula@gmail.com>
2023-04-04 17:44:46 +0300

    Build report map

--- app/src/main/java/org/futo/circles/feature/rageshake/BugReportDataCollector.kt
+++ app/src/main/java/org/futo/circles/feature/rageshake/BugReportDataCollector.kt
@@ -3,10 +3,14 @@ package org.futo.circles.feature.rageshake
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.os.Build
 import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentActivity
+import org.futo.circles.BuildConfig
+import org.futo.circles.R
 import org.futo.circles.extensions.getAllChildFragments
+import org.futo.circles.provider.MatrixSessionProvider
 import java.io.File
 import java.io.IOException
 import java.io.OutputStreamWriter
@@ -16,6 +20,35 @@ class BugReportDataCollector(private val context: Context) {
 
     var screenshot: Bitmap? = null
         private set
+
+    fun collectData(
+        description: String,
+        contactInfo: String,
+        sendLogs: Boolean,
+        sendScreenshot: Boolean
+    ): Map<String, Any> {
+        val session = MatrixSessionProvider.currentSession
+        val map = mutableMapOf<String, Any>(
+            "app" to context.getString(R.string.app_name),
+            "label" to context.getString(R.string.rage_shake_report),
+            "platform" to "Android",
+            "version" to BuildConfig.VERSION_NAME,
+            "flavour" to BuildConfig.FLAVOR,
+            "build_type" to BuildConfig.BUILD_TYPE,
+            "user_id" to (session?.myUserId ?: ""),
+            "device_id" to (session?.sessionParams?.deviceId ?: ""),
+            "home_server_url" to (session?.sessionParams?.homeServerUrl ?: ""),
+            "text" to description,
+            "email" to contactInfo,
+            "device" to Build.MODEL.trim(),
+            "os" to Build.VERSION.RELEASE + " (API " + Build.VERSION.SDK_INT + ") " +
+                    Build.VERSION.INCREMENTAL + "-" + Build.VERSION.CODENAME
+
+        )
+        if (sendLogs) saveLogCat()?.let { map["compressed-log"] = it }
+        if (sendScreenshot) saveScreenshot()?.let { map["file"] = it }
+        return map
+    }
 
     private fun saveLogCat(): File? {
         val logCatErrFile = File(context.cacheDir.absolutePath, LOG_CAT_FILENAME)
@@ -62,7 +95,23 @@ class BugReportDataCollector(private val context: Context) {
         return null
     }
 
+    private fun saveScreenshot(): File? {
+        val bitmap = screenshot ?: return null
+        val screenshotFile =
+            File(context.cacheDir.absolutePath, LOG_CAT_SCREENSHOT_FILENAME)
+        if (screenshotFile.exists()) screenshotFile.delete()
+        try {
+            screenshotFile.outputStream().use {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+            }
+        } catch (_: Exception) {
+            return null
+        }
+        return screenshotFile
+    }
+
     fun takeScreenshot(activity: FragmentActivity) {
+        screenshot = null
         val rootView = activity.window?.decorView?.rootView ?: return
         val mainBitmap = getBitmap(rootView) ?: return
         return try {
--- app/src/main/java/org/futo/circles/feature/rageshake/BugReportDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/rageshake/BugReportDataSource.kt
@@ -3,4 +3,14 @@ package org.futo.circles.feature.rageshake
 class BugReportDataSource(private val bugReportDataCollector: BugReportDataCollector) {
 
     fun getScreenShot() = bugReportDataCollector.screenshot
+
+    fun sendReport(
+        description: String,
+        contactInfo: String,
+        sendLogs: Boolean,
+        sendScreenshot: Boolean
+    ) {
+        val data =
+            bugReportDataCollector.collectData(description, contactInfo, sendLogs, sendScreenshot)
+    }
 }
