f7d22746e2944759db844be7b124e8c63cb7c8da
Taras Smakula <tarassmakula@gmail.com>
2023-04-05 14:30:02 +0300

    Change return type to any

--- app/src/main/java/org/futo/circles/io/BugreportApiService.kt
+++ app/src/main/java/org/futo/circles/io/BugreportApiService.kt
@@ -2,7 +2,6 @@ package org.futo.circles.io
 
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
-import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PartMap
@@ -15,5 +14,5 @@ interface BugreportApiService {
     suspend fun sendBugReport(
         @Url url: String,
         @PartMap map: Map<String, RequestBody>
-    ): Response<ResponseBody>
+    ): ResponseBody
 }
