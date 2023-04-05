16aea5e30c11cb5373f9d36ef0460b12b0787db5
Taras Smakula <tarassmakula@gmail.com>
2023-04-05 14:30:24 +0300

    Handle bug report api call

--- app/src/main/java/org/futo/circles/feature/rageshake/BugReportViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/rageshake/BugReportViewModel.kt
@@ -2,20 +2,26 @@ package org.futo.circles.feature.rageshake
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import okhttp3.ResponseBody
 import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.extensions.Response
+import org.futo.circles.extensions.launchBg
 import org.futo.circles.feature.sign_up.setup_profile.SetupProfileDataSource
 
 class BugReportViewModel(
     profileDataSource: SetupProfileDataSource,
-    bugReportDataSource: BugReportDataSource
+    private val bugReportDataSource: BugReportDataSource
 ) : ViewModel() {
 
-    val sendReportLiveData = SingleEventLiveData<Response<Unit>>()
+    val sendReportLiveData = SingleEventLiveData<Response<ResponseBody>>()
     val threePidLiveData = profileDataSource.threePidLiveData
     val screenshotLiveData = MutableLiveData(bugReportDataSource.getScreenShot())
 
     fun sendReport(description: String, email: String, sendLogs: Boolean, sendScreenshot: Boolean) {
-
+        launchBg {
+            val result =
+                bugReportDataSource.sendReport(description, email, sendLogs, sendScreenshot)
+            sendReportLiveData.postValue(result)
+        }
     }
 }
