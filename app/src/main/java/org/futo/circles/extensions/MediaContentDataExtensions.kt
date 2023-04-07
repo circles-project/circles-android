5096d66bc9104ebe60976c89a6c47588b7f29b86
Taras Smakula <tarassmakula@gmail.com>
2023-04-07 17:56:01 +0300

    Add thumhash placeholder to loadEncryptedImage

--- app/src/main/java/org/futo/circles/extensions/MediaContentDataExtensions.kt
+++ app/src/main/java/org/futo/circles/extensions/MediaContentDataExtensions.kt
@@ -4,14 +4,18 @@ import android.util.Size
 import android.widget.ImageView
 import org.futo.circles.model.MediaFileData
 
-fun MediaFileData.loadEncryptedIntoWithAspect(imageView: ImageView, aspectRatio: Float) {
+fun MediaFileData.loadEncryptedIntoWithAspect(
+    imageView: ImageView,
+    aspectRatio: Float,
+    thumbHash: String? = null
+) {
     imageView.post {
         if (fileUrl.startsWith(UriContentScheme)) {
             imageView.loadImage(fileUrl)
         } else {
             val imageWith = imageView.width
             val size = Size(imageWith, (imageWith / aspectRatio).toInt())
-            imageView.loadEncryptedImage(this, size)
+            imageView.loadEncryptedImage(this, size, thumbHash = thumbHash)
         }
     }
 }
