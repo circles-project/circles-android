63560f73010521a389fb3a46eec086f6535954c0
Taras Smakula <tarassmakula@gmail.com>
2023-04-12 17:16:43 +0300

    Configure notification settings screen

--- app/src/main/java/org/futo/circles/feature/notifications/settings/PushNotificationsSettingsDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/settings/PushNotificationsSettingsDialogFragment.kt
@@ -1,25 +1,42 @@
 package org.futo.circles.feature.notifications.settings
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationManagerCompat
-import org.futo.circles.R
+import androidx.navigation.fragment.findNavController
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.databinding.DialogFragmentPushNotificationsSettingsBinding
+import org.futo.circles.extensions.openNotificationSettings
 
 class PushNotificationsSettingsDialogFragment :
     BaseFullscreenDialogFragment(DialogFragmentPushNotificationsSettingsBinding::inflate) {
 
     private val binding by lazy { getBinding() as DialogFragmentPushNotificationsSettingsBinding }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
     override fun onResume() {
         super.onResume()
         updatePushNotificationStatus()
     }
 
+    private fun setupViews() {
+        with(binding) {
+            lPushNotifications.setOnClickListener { openNotificationSettings() }
+            tvNotificationsTest.setOnClickListener {
+                findNavController()
+                    .navigate(PushNotificationsSettingsDialogFragmentDirections.toNotificationTestDialogFragment())
+            }
+        }
+    }
+
     private fun updatePushNotificationStatus() {
         val isPushNotificationsAllowed =
             NotificationManagerCompat.from(requireContext()).areNotificationsEnabled()
-        binding.tvPushNotificationsStatus.text =
-            getString(if (isPushNotificationsAllowed) R.string.enabled else R.string.disabled)
+        binding.svPushNotificationsStatus.isChecked = isPushNotificationsAllowed
     }
 
 }
