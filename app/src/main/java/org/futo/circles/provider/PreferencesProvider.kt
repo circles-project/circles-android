9273cf6286f28c5f9fdfe96d0caad9bbdbdb95bb
Taras Smakula <tarassmakula@gmail.com>
2023-04-13 15:58:30 +0300

    Add endpoint and gateway to preferences

--- app/src/main/java/org/futo/circles/provider/PreferencesProvider.kt
+++ app/src/main/java/org/futo/circles/provider/PreferencesProvider.kt
@@ -16,21 +16,36 @@ class PreferencesProvider(
     }
 
     fun isDeveloperModeEnabled(): Boolean = getSharedPreferences().getBoolean(DEV_MODE_KEY, false)
-    
+
     fun getFcmToken(): String? {
         return getSharedPreferences().getString(FCM_TOKEN, null)
     }
 
     fun storeFcmToken(token: String?) {
-        getSharedPreferences().edit {
-            putString(FCM_TOKEN, token)
-        }
+        getSharedPreferences().edit { putString(FCM_TOKEN, token) }
+    }
+
+    fun getEndpoint(): String? {
+        return getSharedPreferences().getString(ENDPOINT_OR_TOKEN, null)
+    }
+
+    fun storeUpEndpoint(endpoint: String?) {
+        getSharedPreferences().edit { putString(ENDPOINT_OR_TOKEN, endpoint) }
+    }
+
+    fun getPushGateway(): String? {
+        return getSharedPreferences().getString(PUSH_GATEWAY, null)
+    }
+
+    fun storePushGateway(gateway: String?) {
+        getSharedPreferences().edit { putString(PUSH_GATEWAY, gateway) }
     }
 
     companion object {
         private const val PREFERENCES_NAME = "circles_preferences"
         private const val DEV_MODE_KEY = "developer_mode"
-        private const val FDROID_BACKGROUND_SYNC_MODE = "fdroid_background_sync_mode"
         private const val FCM_TOKEN = "fcm_token"
+        private const val ENDPOINT_OR_TOKEN = "UNIFIED_PUSH_ENDPOINT_OR_TOKEN"
+        private const val PUSH_GATEWAY = "PUSH_GATEWAY"
     }
 }
