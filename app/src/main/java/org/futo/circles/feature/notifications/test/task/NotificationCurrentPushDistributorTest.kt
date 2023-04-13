e394b5499e16698ab8caeb0f35d91d11b5a815b9
Taras Smakula <tarassmakula@gmail.com>
2023-04-13 17:55:17 +0300

    Update notifications test

--- app/src/main/java/org/futo/circles/feature/notifications/test/task/NotificationCurrentPushDistributorTest.kt
+++ app/src/main/java/org/futo/circles/feature/notifications/test/task/NotificationCurrentPushDistributorTest.kt
@@ -2,26 +2,21 @@ package org.futo.circles.feature.notifications.test.task
 
 import android.content.Context
 import org.futo.circles.R
-import org.futo.circles.feature.notifications.FcmHelper
+import org.futo.circles.feature.notifications.UnifiedPushHelper
 import org.futo.circles.model.NotificationTestStatus
 
 
 class NotificationCurrentPushDistributorTest(
     private val context: Context,
-    private val fcmHelper: FcmHelper,
+    private val unifiedPushHelper: UnifiedPushHelper
 ) : BaseNotificationTest(R.string.settings_troubleshoot_test_current_distributor_title) {
 
     override fun perform() {
         description = context.getString(
             R.string.settings_troubleshoot_test_current_distributor,
-            getCurrentDistributorName()
+            unifiedPushHelper.getCurrentDistributorName()
         )
         status = NotificationTestStatus.SUCCESS
     }
-
-    private fun getCurrentDistributorName(): String =
-        if (fcmHelper.isFirebaseAvailable()) context.getString(R.string.unifiedpush_distributor_fcm_fallback)
-        else context.getString(R.string.unifiedpush_distributor_background_sync)
-
 
 }
