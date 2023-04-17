2f3dd4b7a4ed85986b2059acd2e5036666cc9f5b
Taras Smakula <tarassmakula@gmail.com>
2023-04-17 18:06:18 +0300

    Create NotificationsEndpointAsTokenRegistrationTest

---
+++ app/src/main/java/org/futo/circles/feature/notifications/test/task/NotificationsEndpointAsTokenRegistrationTest.kt
@@ -0,0 +1,39 @@
+package org.futo.circles.feature.notifications.test.task
+
+import android.content.Context
+import org.futo.circles.R
+import org.futo.circles.feature.notifications.PushersManager
+import org.futo.circles.model.NotificationTestStatus
+import org.futo.circles.provider.MatrixSessionProvider
+import org.matrix.android.sdk.api.session.pushers.PusherState
+
+class NotificationsEndpointAsTokenRegistrationTest(
+    private val context: Context,
+    private val pushersManager: PushersManager
+) : BaseNotificationTest(R.string.settings_troubleshoot_test_endpoint_registration_title) {
+
+    override fun perform() {
+        val endpoint = pushersManager.getEndpointOrToken() ?: run {
+            status = NotificationTestStatus.FAILED
+            return
+        }
+        val session = MatrixSessionProvider.currentSession ?: run {
+            status = NotificationTestStatus.FAILED
+            return
+        }
+        val pushers = session.pushersService().getPushers().filter {
+            it.pushKey == endpoint && it.state == PusherState.REGISTERED
+        }
+        if (pushers.isEmpty()) {
+            description = context.getString(
+                R.string.settings_troubleshoot_test_endpoint_registration_failed,
+                context.getString(R.string.unexpected_error)
+            )
+            status = NotificationTestStatus.FAILED
+        } else {
+            description =
+                context.getString(R.string.settings_troubleshoot_test_endpoint_registration_success)
+            status = NotificationTestStatus.SUCCESS
+        }
+    }
+}
