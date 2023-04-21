2365a643d50bc806561bfa6a625a96c6ff7aae80
Taras Smakula <tarassmakula@gmail.com>
2023-04-21 17:14:24 +0300

    Change to work manager implementation

--- app/src/main/java/org/futo/circles/feature/photos/backup/service/MediaBackupService.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/service/MediaBackupService.kt
@@ -1,7 +1,5 @@
 package org.futo.circles.feature.photos.backup.service
 
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -11,20 +9,32 @@ import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.provider.MediaStore
+import androidx.work.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.futo.circles.feature.photos.backup.MediaBackupDataSource
 import org.futo.circles.feature.timeline.data_source.SendMessageDataSource
 import org.koin.android.ext.android.inject
-import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class MediaBackupService : Service() {
 
+    private val job = Job()
+    private val backupScope = CoroutineScope(Dispatchers.IO + job)
     private val sendMessageDataSource: SendMessageDataSource by inject()
     private val mediaBackupDataSource: MediaBackupDataSource by inject()
+    private var isBackupRunning = false
 
     private val contentObserver = object : ContentObserver(Handler(Looper.getMainLooper())) {
         override fun onChange(selfChange: Boolean, uri: Uri?) {
+            if (isBackupRunning) return
+            val path = uri?.path ?: return
+            if (mediaBackupDataSource.needToBackup(path)) {
 
+            }
         }
     }
 
@@ -38,41 +48,40 @@ class MediaBackupService : Service() {
         )
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-    }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        backupScope.launch {
+            isBackupRunning = true
+            val media = mediaBackupDataSource.getAllMediasToBackup()
+        }
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        job.cancel()
         contentResolver.unregisterContentObserver(contentObserver);
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
     }
 
     companion object {
         fun getIntent(context: Context) = Intent(context, MediaBackupService::class.java)
 
-        fun scheduleBackup(context: Context) {
-            val pendingIntent =
-                PendingIntent.getService(
-                    context,
-                    0,
-                    getIntent(context),
-                    PendingIntent.FLAG_IMMUTABLE
-                )
-            val calendar = Calendar.getInstance().apply {
-                set(Calendar.HOUR_OF_DAY, 0)
-                set(Calendar.MINUTE, 0)
-                set(Calendar.SECOND, 0)
-                add(Calendar.DAY_OF_YEAR, 1)
-            }
-            (context.getSystemService(Context.ALARM_SERVICE) as AlarmManager).setInexactRepeating(
-                AlarmManager.RTC_WAKEUP,
-                calendar.timeInMillis,
-                AlarmManager.INTERVAL_DAY,
-                pendingIntent
+        private fun scheduleBackup(context: Context) {
+            val backupRequest = OneTimeWorkRequestBuilder<MediaBackupWorker>()
+                .setInitialDelay(1, TimeUnit.DAYS)
+                .setConstraints(
+                    Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.CONNECTED)
+                        .setRequiresCharging(false)
+                        .build()
+                ).build()
+            WorkManager.getInstance(context.applicationContext).enqueueUniqueWork(
+                "media_backup",
+                ExistingWorkPolicy.REPLACE,
+                backupRequest
             )
         }
     }
