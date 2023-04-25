5fb23c25101fca749e13766fcb1cac43d4315374
Taras Smakula <tarassmakula@gmail.com>
2023-04-25 16:25:31 +0300

    Add uri to MediaToBackupItem

--- app/src/main/java/org/futo/circles/model/MediaToBackupItem.kt
+++ app/src/main/java/org/futo/circles/model/MediaToBackupItem.kt
@@ -1,16 +1,20 @@
 package org.futo.circles.model
 
+import android.content.Context
+import android.net.Uri
 import org.futo.circles.core.list.IdEntity
+import org.futo.circles.extensions.getUri
 import java.io.File
 
 data class MediaToBackupItem(
     override val id: String,
     val displayName: String,
-    val path: String,
+    val uri: Uri,
     val size: Long
 ) : IdEntity<String>
 
-fun File.toMediaToBackupItem() = MediaToBackupItem(getMediaFileUniqueId(this), name, path, length())
+fun File.toMediaToBackupItem(context: Context) =
+    MediaToBackupItem(getMediaFileUniqueId(this), name, getUri(context), length())
 
 private fun getMediaFileUniqueId(file: File): String {
     val fileName = file.name
