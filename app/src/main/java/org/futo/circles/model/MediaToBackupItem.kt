7ef2c26d4ef862cd133f9f4d9690e32552e56499
Taras Smakula <tarassmakula@gmail.com>
2023-05-01 16:31:08 +0300

    Remove unique id for media file

--- app/src/main/java/org/futo/circles/model/MediaToBackupItem.kt
+++ app/src/main/java/org/futo/circles/model/MediaToBackupItem.kt
@@ -7,25 +7,14 @@ import org.futo.circles.extensions.getUri
 import java.io.File
 
 data class MediaToBackupItem(
-    override val id: String,
     val displayName: String,
     val uri: Uri,
     val size: Long,
     val dateModified: Long
-) : IdEntity<String>
-
-fun File.toMediaToBackupItem(context: Context) =
-    MediaToBackupItem(
-        getMediaFileUniqueId(this),
-        name,
-        getUri(context),
-        length(),
-        lastModified()
-    )
-
-private fun getMediaFileUniqueId(file: File): String {
-    val fileName = file.name
-    val fileSize = file.length()
-    val fileLastModified = file.lastModified()
-    return "$fileName$fileSize$fileLastModified".hashCode().toString()
+) : IdEntity<Uri> {
+    override val id: Uri = uri
 }
+
+fun File.toMediaToBackupItem(context: Context) = MediaToBackupItem(
+    name, getUri(context), length(), lastModified()
+)
