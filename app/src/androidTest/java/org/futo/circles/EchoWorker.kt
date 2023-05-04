500b568e3d2035c8904a48a6c5502b61cadd3dbe
Taras Smakula <tarassmakula@gmail.com>
2023-05-04 18:00:26 +0300

    Create echo worker

---
+++ app/src/androidTest/java/org/futo/circles/EchoWorker.kt
@@ -0,0 +1,74 @@
+package org.futo.circles
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.delay
+import org.koin.core.component.KoinComponent
+
+class EchoWorker(context: Context, params: WorkerParameters) :
+    CoroutineWorker(context, params), KoinComponent {
+
+    private val notificationManager = NotificationManagerCompat.from(context)
+
+    override suspend fun doWork(): Result {
+        try {
+            setForeground(createForegroundInfo())
+            delay(2000)
+        } catch (_: Exception) {
+            return Result.failure()
+        }
+        return Result.success()
+    }
+
+    private fun createForegroundInfo(): ForegroundInfo {
+        val id = MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID
+        val cancelIntent =
+            WorkManager.getInstance(applicationContext).createCancelPendingIntent(getId())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) createChannel()
+
+        val notification = NotificationCompat.Builder(applicationContext, id)
+            .setContentTitle(applicationContext.getString(R.string.notification_media_backup))
+            .setTicker(applicationContext.getString(R.string.notification_media_backup))
+            .setContentText(applicationContext.getString(R.string.uploading))
+            .setSmallIcon(R.drawable.ic_push_notification)
+            .setOngoing(true)
+            .addAction(
+                R.drawable.ic_close,
+                applicationContext.getString(R.string.cancel),
+                cancelIntent
+            )
+            .build()
+
+        return ForegroundInfo(MEDIA_BACKUP_NOTIFICATION_ID, notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID,
+                applicationContext.getString(R.string.notification_media_backup),
+                NotificationManager.IMPORTANCE_MIN
+            ).apply {
+                description = applicationContext.getString(R.string.notification_media_backup)
+                setSound(null, null)
+                setShowBadge(false)
+            })
+    }
+
+
+    companion object {
+        private const val MEDIA_BACKUP_NOTIFICATION_ID = 35
+        private const val MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID =
+            "MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID"
+    }
+}
