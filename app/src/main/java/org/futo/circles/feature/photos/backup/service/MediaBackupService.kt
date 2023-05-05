639ebbf52001441729827407f69475bf2f1a4e3b
Taras Smakula <tarassmakula@gmail.com>
2023-05-05 15:55:05 +0300

    Add foreground implementation for service

--- app/src/main/java/org/futo/circles/feature/photos/backup/service/MediaBackupService.kt
+++ app/src/main/java/org/futo/circles/feature/photos/backup/service/MediaBackupService.kt
@@ -1,21 +1,28 @@
 package org.futo.circles.feature.photos.backup.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.database.ContentObserver
 import android.net.Uri
 import android.os.Binder
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.provider.MediaStore
-import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import org.futo.circles.R
 import org.futo.circles.feature.photos.backup.MediaBackupDataSource
 import org.koin.android.ext.android.inject
 
@@ -47,6 +54,15 @@ class MediaBackupService : Service() {
         startBackup()
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notification = createNotification()
+        startForeground(MEDIA_BACKUP_NOTIFICATION_ID, notification)
+        startBackup()
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+        return START_NOT_STICKY
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         job.cancel()
@@ -67,11 +83,40 @@ class MediaBackupService : Service() {
         }
     }
 
+
+    private fun createNotification(): Notification {
+        val id = MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) createChannel()
+        return NotificationCompat.Builder(applicationContext, id)
+            .setContentTitle(applicationContext.getString(R.string.notification_media_backup))
+            .setTicker(applicationContext.getString(R.string.notification_media_backup))
+            .setContentText(applicationContext.getString(R.string.uploading))
+            .setSmallIcon(R.drawable.ic_push_notification)
+            .build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        NotificationManagerCompat.from(applicationContext).createNotificationChannel(
+            NotificationChannel(
+                MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID,
+                applicationContext.getString(R.string.notification_media_backup),
+                NotificationManager.IMPORTANCE_MIN
+            ).apply {
+                description = applicationContext.getString(R.string.notification_media_backup)
+                setSound(null, null)
+                setShowBadge(false)
+            })
+    }
+
     inner class MediaBackupServiceBinder : Binder() {
         fun getService(): MediaBackupService = this@MediaBackupService
     }
 
     companion object {
         fun getIntent(context: Context) = Intent(context, MediaBackupService::class.java)
+        private const val MEDIA_BACKUP_NOTIFICATION_ID = 35
+        private const val MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID =
+            "MEDIA_BACKUP_NOTIFICATION_CHANNEL_ID"
     }
 }
