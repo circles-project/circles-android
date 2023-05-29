8c36403002492c342eefe71f3ec58da1158a566d
Taras Smakula <tarassmakula@gmail.com>
2023-05-29 16:29:53 +0300

    Fix file utils

--- core/src/main/java/org/futo/circles/core/utils/FileUtils.kt
+++ core/src/main/java/org/futo/circles/core/utils/FileUtils.kt
@@ -1,10 +1,16 @@
 package org.futo.circles.core.utils
 
+import android.content.ContentValues
 import android.content.Context
 import android.net.Uri
 import android.os.Environment
+import android.provider.MediaStore
+import okio.buffer
+import okio.sink
+import okio.source
 import org.futo.circles.core.extensions.getUri
 import org.futo.circles.core.model.MediaFileData
+import org.futo.circles.core.picker.MediaType
 import org.futo.circles.core.provider.MatrixSessionProvider
 import java.io.File
 import java.io.IOException
@@ -52,5 +58,31 @@ object FileUtils {
         context: Context,
         contentData: MediaFileData
     ): Uri? = downloadEncryptedFile(contentData)?.getUri(context)
+
+    suspend fun saveMediaFileToDevice(
+        context: Context,
+        mediaFileData: MediaFileData,
+        mediaType: MediaType
+    ) {
+        val localFile = downloadEncryptedFile(mediaFileData) ?: return
+
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.TITLE, mediaFileData.fileName)
+            put(MediaStore.Images.Media.DISPLAY_NAME, mediaFileData.fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, mediaFileData.mimeType)
+        }
+
+        val externalContentUri = when (mediaType) {
+            MediaType.Image -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            MediaType.Video -> MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val uri = context.contentResolver.insert(externalContentUri, values) ?: return
+
+        val source = localFile.inputStream().source().buffer()
+        context.contentResolver.openOutputStream(uri)?.sink()?.buffer()?.let { sink ->
+            source.use { input -> sink.use { output -> output.writeAll(input) } }
+        }
+    }
 
 }
