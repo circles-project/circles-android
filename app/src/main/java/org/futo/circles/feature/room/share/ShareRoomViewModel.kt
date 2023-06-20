5f06e6367e3de665786ce4a0741d06129a8fc5a9
Taras Smakula <tarassmakula@gmail.com>
2023-06-20 17:37:52 +0300

    Create share room view model

---
+++ app/src/main/java/org/futo/circles/feature/room/share/ShareRoomViewModel.kt
@@ -0,0 +1,26 @@
+package org.futo.circles.feature.room.share
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.futo.circles.core.extensions.getOrThrow
+import org.futo.circles.core.model.ShareableContent
+import org.futo.circles.core.model.TextShareable
+import org.futo.circles.core.provider.MatrixSessionProvider
+import javax.inject.Inject
+
+@HiltViewModel
+class ShareRoomViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+
+    private val roomId: String = savedStateHandle.getOrThrow("roomId")
+    val roomLiveData =
+        MatrixSessionProvider.currentSession?.roomService()?.getRoomSummaryLive(roomId)
+
+
+    fun createSharableUrl(): ShareableContent {
+        return TextShareable(roomId)
+    }
+}
