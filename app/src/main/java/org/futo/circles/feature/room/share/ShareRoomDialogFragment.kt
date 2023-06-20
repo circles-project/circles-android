fa49474f1baf735abd37330d1834db4b3a897cc9
Taras Smakula <tarassmakula@gmail.com>
2023-06-20 17:37:41 +0300

    Create share room dialog fragment

---
+++ app/src/main/java/org/futo/circles/feature/room/share/ShareRoomDialogFragment.kt
@@ -0,0 +1,61 @@
+package org.futo.circles.feature.room.share
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.futo.circles.R
+import org.futo.circles.core.extensions.gone
+import org.futo.circles.core.extensions.observeData
+import org.futo.circles.core.extensions.showError
+import org.futo.circles.core.extensions.visible
+import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
+import org.futo.circles.core.mapping.nameOrId
+import org.futo.circles.core.share.ShareProvider
+import org.futo.circles.databinding.DialogFragmentShareRoomBinding
+import org.matrix.android.sdk.api.session.room.model.RoomSummary
+
+@AndroidEntryPoint
+class ShareRoomDialogFragment :
+    BaseFullscreenDialogFragment(DialogFragmentShareRoomBinding::inflate) {
+
+    private val viewModel by viewModels<ShareRoomViewModel>()
+
+    private val binding by lazy {
+        getBinding() as DialogFragmentShareRoomBinding
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObServers()
+    }
+
+    private fun setupViews() {
+        binding.btnShare.setOnClickListener {
+            ShareProvider.share(requireContext(), viewModel.createSharableUrl())
+        }
+    }
+
+    private fun setupObServers() {
+        viewModel.roomLiveData?.observeData(this) {
+            it.getOrNull()?.let { summary ->
+                handelQrReady(summary)
+            } ?: kotlin.run {
+                binding.vLoading.gone()
+                showError(getString(R.string.room_not_found))
+            }
+        }
+    }
+
+    private fun handelQrReady(roomSummary: RoomSummary) {
+        with(binding) {
+            vLoading.gone()
+            ivQr.visible()
+            ivQr.setData(roomSummary.roomId)
+            tvRoomName.text = roomSummary.nameOrId()
+            tvRoomId.text = roomSummary.roomId
+            btnShare.visible()
+        }
+    }
+}
