04ddc6559887b93bb14f1ef89626e1d07f299a35
Taras Smakula <tarassmakula@gmail.com>
2023-06-22 17:51:18 +0300

    Bind room info for Room well known fragment

--- app/src/main/java/org/futo/circles/feature/room/well_known/RoomWellKnownDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/room/well_known/RoomWellKnownDialogFragment.kt
@@ -2,13 +2,18 @@ package org.futo.circles.feature.room.well_known
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import org.futo.circles.R
+import org.futo.circles.core.extensions.gone
+import org.futo.circles.core.extensions.loadProfileIcon
 import org.futo.circles.core.extensions.observeResponse
-import org.futo.circles.core.extensions.showError
-import org.futo.circles.core.extensions.showSuccess
+import org.futo.circles.core.extensions.setIsVisible
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.databinding.DialogFragmentRoomWellKnownBinding
+import org.futo.circles.model.RoomPublicInfo
+import org.matrix.android.sdk.api.session.room.model.Membership
 
 @AndroidEntryPoint
 class RoomWellKnownDialogFragment :
@@ -27,12 +32,30 @@ class RoomWellKnownDialogFragment :
 
     private fun setupObservers() {
         viewModel.roomPublicInfoLiveData.observeResponse(this,
-            success = {
-                      showSuccess("sucees")
-            },
-            error = {
-                showError(it)
-            }
+            success = { roomInfo -> bindRoomData(roomInfo) },
+            onRequestInvoked = { binding.vLoading.gone() },
+            error = { bindError(it) }
         )
+    }
+
+    private fun bindError(message: String) {
+        binding.ivCover.apply {
+            setImageResource(R.drawable.ic_error)
+            setColorFilter(ContextCompat.getColor(requireContext(), R.color.red))
+        }
+        binding.tvRoomName.text = message
+    }
+
+    private fun bindRoomData(roomInfo: RoomPublicInfo) {
+        with(binding) {
+            ivCover.loadProfileIcon(roomInfo.avatarUrl, roomInfo.displayName)
+            toolbar.title = roomInfo.displayName
+            tvRoomName.text = roomInfo.displayName
+            tvRoomId.text = roomInfo.id
+            tvMembersCount.text = getString(R.string.joined_members_count, roomInfo.memberCount)
+            btnRequest.setIsVisible(roomInfo.membership == Membership.NONE)
+            tvTopic.setIsVisible(roomInfo.topic != null)
+            tvTopic.text = roomInfo.topic ?: ""
+        }
     }
 }
--- app/src/main/java/org/futo/circles/model/RoomPublicInfo.kt
+++ app/src/main/java/org/futo/circles/model/RoomPublicInfo.kt
@@ -1,35 +1,33 @@
 package org.futo.circles.model
 
+import org.futo.circles.core.mapping.nameOrId
 import org.matrix.android.sdk.api.session.room.model.Membership
 import org.matrix.android.sdk.api.session.room.model.RoomSummary
 import org.matrix.android.sdk.api.session.room.peeking.PeekResult
 
 data class RoomPublicInfo(
     val id: String,
-    val displayName: String?,
+    val displayName: String,
     val avatarUrl: String?,
-    val topic: String,
-    val memberCount: Int?,
-    val alias: String?,
+    val topic: String?,
+    val memberCount: Int,
     val membership: Membership
 )
 
 fun RoomSummary.toRoomPublicInfo() = RoomPublicInfo(
     id = roomId,
-    displayName = name,
+    displayName = nameOrId(),
     avatarUrl = avatarUrl,
     topic = topic,
-    memberCount = joinedMembersCount,
-    alias = canonicalAlias,
+    memberCount = joinedMembersCount ?: 0,
     membership = membership
 )
 
 fun PeekResult.Success.toRoomPublicInfo() = RoomPublicInfo(
     id = roomId,
-    displayName = name,
+    displayName = name?.takeIf { it.isNotEmpty() } ?: roomId,
     avatarUrl = avatarUrl,
-    topic = topic ?: "",
-    memberCount = numJoinedMembers,
-    alias = alias,
+    topic = topic,
+    memberCount = numJoinedMembers ?: 0,
     membership = Membership.NONE
 )
