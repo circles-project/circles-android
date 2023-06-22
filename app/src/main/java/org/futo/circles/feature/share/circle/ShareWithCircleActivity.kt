62c3cb11e93b785b1b74d41c1d6ddede54a0ba10
Taras Smakula <tarassmakula@gmail.com>
2023-06-22 14:40:18 +0300

    Fix di for share activities

--- app/src/main/java/org/futo/circles/feature/share/circle/ShareWithCircleActivity.kt
+++ app/src/main/java/org/futo/circles/feature/share/circle/ShareWithCircleActivity.kt
@@ -1,5 +1,6 @@
 package org.futo.circles.feature.share.circle
 
+import dagger.hilt.android.AndroidEntryPoint
 import org.futo.circles.R
 import org.futo.circles.core.model.CircleRoomTypeArg
 import org.futo.circles.core.room.select.RoomsPicker
@@ -7,6 +8,7 @@ import org.futo.circles.core.share.BaseShareActivity
 import org.futo.circles.core.utils.getTimelineRoomFor
 import org.futo.circles.feature.room.select.SelectRoomsFragment
 
+@AndroidEntryPoint
 class ShareWithCircleActivity : BaseShareActivity() {
 
     override val titleResId: Int = R.string.share_with_circle
--- app/src/main/java/org/futo/circles/feature/share/group/ShareWithGroupActivity.kt
+++ app/src/main/java/org/futo/circles/feature/share/group/ShareWithGroupActivity.kt
@@ -1,11 +1,13 @@
 package org.futo.circles.feature.share.group
 
+import dagger.hilt.android.AndroidEntryPoint
 import org.futo.circles.R
 import org.futo.circles.core.model.CircleRoomTypeArg
 import org.futo.circles.core.room.select.RoomsPicker
 import org.futo.circles.core.share.BaseShareActivity
 import org.futo.circles.feature.room.select.SelectRoomsFragment
 
+@AndroidEntryPoint
 class ShareWithGroupActivity : BaseShareActivity() {
 
     override val titleResId: Int = R.string.share_with_group
