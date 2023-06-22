b3f3ff48c22ae465045cb9f364028ef7c1620575
Taras Smakula <tarassmakula@gmail.com>
2023-06-22 17:09:40 +0300

    Add messages for peek room errors

--- app/src/main/java/org/futo/circles/feature/room/well_known/RoomWellKnownDataSource.kt
+++ app/src/main/java/org/futo/circles/feature/room/well_known/RoomWellKnownDataSource.kt
@@ -1,5 +1,8 @@
 package org.futo.circles.feature.room.well_known
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.futo.circles.R
 import org.futo.circles.core.extensions.Response
 import org.futo.circles.core.extensions.createResult
 import org.futo.circles.core.provider.MatrixSessionProvider
@@ -9,17 +12,20 @@ import org.matrix.android.sdk.api.session.getRoom
 import org.matrix.android.sdk.api.session.room.peeking.PeekResult
 import javax.inject.Inject
 
-class RoomWellKnownDataSource @Inject constructor() {
+class RoomWellKnownDataSource @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
 
     suspend fun resolveRoomById(roomId: String): Response<RoomPublicInfo> = createResult {
         val session = MatrixSessionProvider.currentSession
-            ?: throw IllegalArgumentException("session is not created")
+            ?: throw IllegalArgumentException(context.getString(R.string.session_is_not_created))
 
         session.getRoom(roomId)?.roomSummary()?.toRoomPublicInfo()?.let { return@createResult it }
 
         when (val peekResult = session.roomService().peekRoom(roomId)) {
             is PeekResult.Success -> peekResult.toRoomPublicInfo()
-            else -> throw IllegalArgumentException("room not found")
+            is PeekResult.PeekingNotAllowed -> throw IllegalArgumentException(context.getString(R.string.not_allowed_to_view_room_info))
+            PeekResult.UnknownAlias -> throw IllegalArgumentException(context.getString(R.string.room_not_found))
         }
     }
 
