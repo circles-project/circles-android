f50bcb297f6a17f8c3db0a98997b2434eabf7adc
Taras Smakula <tarassmakula@gmail.com>
2023-06-27 14:55:47 +0300

    Add membership message for well known

--- app/src/main/java/org/futo/circles/feature/room/well_known/RoomWellKnownDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/room/well_known/RoomWellKnownDialogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import org.futo.circles.R
@@ -16,7 +15,6 @@ import org.futo.circles.core.extensions.setIsVisible
 import org.futo.circles.core.extensions.showSuccess
 import org.futo.circles.core.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.databinding.DialogFragmentRoomWellKnownBinding
-import org.futo.circles.gallery.feature.UpdateGalleryDialogFragmentArgs
 import org.futo.circles.model.RoomPublicInfo
 import org.matrix.android.sdk.api.session.room.model.Membership
 
@@ -73,9 +71,25 @@ class RoomWellKnownDialogFragment :
             toolbar.title = roomInfo.displayName
             tvRoomName.text = roomInfo.displayName
             tvMembersCount.text = getString(R.string.joined_members_count, roomInfo.memberCount)
-            btnRequest.setIsVisible(roomInfo.membership == Membership.NONE)
-            tvTopic.setIsVisible(roomInfo.topic != null)
+            btnRequest.setIsVisible(shouldShowKnockButton(roomInfo.membership))
+            tvTopic.setIsVisible(roomInfo.topic?.isNotEmpty() == true)
             tvTopic.text = roomInfo.topic ?: ""
+            tvMembersip.text = getString(
+                when (roomInfo.membership) {
+                    Membership.NONE,
+                    Membership.KNOCK,
+                    Membership.LEAVE -> R.string.request_to_become_member
+
+                    Membership.INVITE -> R.string.you_have_pending_invitation
+                    Membership.JOIN -> R.string.you_are_already_member
+                    Membership.BAN -> R.string.you_are_banned
+                }
+            )
         }
     }
+
+    private fun shouldShowKnockButton(membership: Membership): Boolean =
+        membership == Membership.NONE || membership == Membership.KNOCK || membership == Membership.LEAVE
+
+
 }
