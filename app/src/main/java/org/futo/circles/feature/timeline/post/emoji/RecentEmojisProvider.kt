bea0a1510ab2f5d15c2b83a511d3849155492fa1
Taras Smakula <tarassmakula@gmail.com>
2023-07-04 19:41:16 +0300

    New default emojis list and logic

--- app/src/main/java/org/futo/circles/feature/timeline/post/emoji/RecentEmojisProvider.kt
+++ app/src/main/java/org/futo/circles/feature/timeline/post/emoji/RecentEmojisProvider.kt
@@ -3,27 +3,29 @@ package org.futo.circles.feature.timeline.post.emoji
 import android.content.Context
 import com.vanniktech.emoji.Emoji
 import com.vanniktech.emoji.recent.RecentEmojiManager
-import com.vanniktech.emoji.search.SearchEmojiManager
 
 object RecentEmojisProvider {
 
     private var manager: RecentEmojiManager? = null
 
+    private val defaultEmojisKeys = listOf(
+        "👍", "👎", "❤️", "🔥", "😀", "🤣", "🎉", "🚀", "💯"
+    )
+
+    private const val RECENT_EMOJIS_PREF_NAME = "emoji-recent-manager"
+    private const val RECENT_EMOJIS_PREF_KEY = "recent-emojis"
+    private const val TIME_DELIMITER = ";"
+    private const val EMOJI_DELIMITER = "~"
+
     fun get(context: Context) = manager ?: RecentEmojiManager(context).also { manager = it }
 
-    fun saveDefaultRecentEmojis(context: Context) {
-        val recentManager = get(context)
-        val recent = recentManager.getRecentEmojis()
-        if (recent.isNotEmpty()) return
-        recentManager.apply {
-            SearchEmojiManager().apply {
-                search("100").firstOrNull()?.emoji?.let { emoji -> addEmoji(emoji) }
-                search("tada").firstOrNull()?.emoji?.let { emoji -> addEmoji(emoji) }
-                search("grinning").firstOrNull()?.emoji?.let { emoji -> addEmoji(emoji) }
-                search("thumbsup").firstOrNull()?.emoji?.let { emoji -> addEmoji(emoji) }
-            }
-            persist()
-        }
+    fun initWithDefaultRecentEmojis(context: Context) {
+        val defaultEmojisString =
+            defaultEmojisKeys.joinToString(TIME_DELIMITER + "0" + EMOJI_DELIMITER)
+        context.applicationContext.getSharedPreferences(
+            RECENT_EMOJIS_PREF_NAME, Context.MODE_PRIVATE
+        ).edit().putString(RECENT_EMOJIS_PREF_KEY, defaultEmojisString).apply()
+        manager = RecentEmojiManager(context)
     }
 
     fun addNewEmoji(context: Context, emoji: Emoji) {
