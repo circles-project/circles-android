edf673e4f65fda5a5101cd4f7a5a174cfee0d6a5
Taras Smakula <tarassmakula@gmail.com>
2023-07-06 18:21:05 +0300

    Create FullScreenListPreviewFragment

---
+++ gallery/src/main/java/org/futo/circles/gallery/feature/gallery/full_screen/FullScreenListPreviewFragment.kt
@@ -0,0 +1,48 @@
+package org.futo.circles.gallery.feature.gallery.full_screen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import org.futo.circles.core.extensions.observeData
+import org.futo.circles.gallery.R
+import org.futo.circles.gallery.databinding.FragmentFullScreenListPreviewBinding
+import org.futo.circles.gallery.feature.gallery.full_screen.list.FullScreenGalleryListAdapter
+import org.futo.circles.gallery.feature.gallery.grid.GalleryViewModel
+
+@AndroidEntryPoint
+class FullScreenListPreviewFragment : Fragment(R.layout.fragment_full_screen_list_preview) {
+
+    private val binding by viewBinding(FragmentFullScreenListPreviewBinding::bind)
+
+    private val viewModel by viewModels<GalleryViewModel>({ requireParentFragment() })
+
+    private val listAdapter by lazy {
+        FullScreenGalleryListAdapter(onLoadMore = { viewModel.loadMore() })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObservers()
+    }
+
+    private fun setupViews() {
+        binding.rvMediaList.apply {
+            layoutManager = LinearLayoutManager(requireContext()).apply {
+                orientation = LinearLayoutManager.HORIZONTAL
+            }
+            adapter = listAdapter
+        }
+    }
+
+
+    private fun setupObservers() {
+        viewModel.galleryItemsLiveData.observeData(this) {
+            listAdapter.submitList(it)
+        }
+    }
+}
