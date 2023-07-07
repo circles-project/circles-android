d06946ce84702a2d5f698a4ca7628b2ab0551fd0
Taras Smakula <tarassmakula@gmail.com>
2023-07-07 15:19:15 +0300

    Add view types

--- gallery/src/main/java/org/futo/circles/gallery/feature/gallery/full_screen/list/FullScreenGalleryListAdapter.kt
+++ gallery/src/main/java/org/futo/circles/gallery/feature/gallery/full_screen/list/FullScreenGalleryListAdapter.kt
@@ -1,16 +1,23 @@
 package org.futo.circles.gallery.feature.gallery.full_screen.list
 
 import android.view.ViewGroup
+import com.google.android.exoplayer2.ExoPlayer
 import org.futo.circles.core.list.BaseRvAdapter
+import org.futo.circles.core.model.PostContentType
 import org.futo.circles.gallery.model.GalleryContentListItem
 
 
 class FullScreenGalleryListAdapter(
+    private val videoPlayer: ExoPlayer,
     private val onLoadMore: () -> Unit
 ) : BaseRvAdapter<GalleryContentListItem, FullScreenGalleryItemViewHolder>(DefaultIdEntityCallback()) {
 
+    override fun getItemViewType(position: Int) = getItem(position).mediaContent.type.ordinal
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        FullScreenGalleryItemViewHolder(parent)
+        if (viewType == PostContentType.VIDEO_CONTENT.ordinal)
+            FullScreenVideoItemViewHolder(parent, videoPlayer)
+        else FullScreenImageItemViewHolder(parent)
 
     override fun onBindViewHolder(holder: FullScreenGalleryItemViewHolder, position: Int) {
         holder.bind(getItem(position))
