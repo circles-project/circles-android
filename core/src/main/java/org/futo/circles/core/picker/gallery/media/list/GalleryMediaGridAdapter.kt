d25e6df2a81ce9246a47e5bc8c9c847e4c87a710
Taras Smakula <tarassmakula@gmail.com>
2023-07-23 20:01:29 +0300

    Update adapter

--- core/src/main/java/org/futo/circles/core/picker/gallery/media/list/GalleryMediaGridAdapter.kt
+++ core/src/main/java/org/futo/circles/core/picker/gallery/media/list/GalleryMediaGridAdapter.kt
@@ -5,16 +5,22 @@ import org.futo.circles.core.list.BaseRvAdapter
 import org.futo.circles.core.model.GalleryContentListItem
 
 class GalleryMediaGridAdapter(
+    private val isMultiSelect: Boolean,
     private val onMediaItemClicked: (item: GalleryContentListItem) -> Unit,
     private val onLoadMore: () -> Unit
-) : BaseRvAdapter<GalleryContentListItem, GalleryMediaItemViewHolder>(DefaultIdEntityCallback()) {
+) : BaseRvAdapter<GalleryContentListItem, GridMediaItemViewHolder>(DefaultIdEntityCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = GalleryMediaItemViewHolder(
-        parent,
-        onItemClicked = { position, _ -> onMediaItemClicked(getItem(position)) }
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = if (isMultiSelect)
+        MultiSelectGalleryMediaItemViewHolder(
+            parent,
+            onItemClicked = { position -> onMediaItemClicked(getItem(position)) })
+    else
+        GalleryMediaItemViewHolder(
+            parent,
+            onItemClicked = { position, _ -> onMediaItemClicked(getItem(position)) }
+        )
 
-    override fun onBindViewHolder(holder: GalleryMediaItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: GridMediaItemViewHolder, position: Int) {
         holder.bind(getItem(position))
         if (position >= itemCount - LOAD_MORE_THRESHOLD) onLoadMore()
     }
