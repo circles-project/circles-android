bab962b89a40965ae8ef318d3beecb345e8e6111
Taras Smakula <tarassmakula@gmail.com>
2023-07-23 20:01:09 +0300

    Create response models

--- core/src/main/java/org/futo/circles/core/model/GalleryContentListItem.kt
+++ core/src/main/java/org/futo/circles/core/model/GalleryContentListItem.kt
@@ -1,11 +1,10 @@
 package org.futo.circles.core.model
 
 import org.futo.circles.core.list.IdEntity
-import org.futo.circles.core.model.MediaContent
-import org.futo.circles.core.model.PostInfo
 
 data class GalleryContentListItem(
     override val id: String,
     val postInfo: PostInfo,
-    val mediaContent: MediaContent
+    val mediaContent: MediaContent,
+    val isSelected: Boolean = false
 ) : IdEntity<String>
