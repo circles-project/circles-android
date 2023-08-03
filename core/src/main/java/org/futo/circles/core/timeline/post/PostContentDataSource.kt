002bbef540aeb03bd78d7531fa84fea9d464688b
Taras Smakula <tarassmakula@gmail.com>
2023-08-03 16:42:24 +0300

    Create getPostContentType for Timeline event

--- core/src/main/java/org/futo/circles/core/timeline/post/PostContentDataSource.kt
+++ core/src/main/java/org/futo/circles/core/timeline/post/PostContentDataSource.kt
@@ -1,15 +1,13 @@
 package org.futo.circles.core.timeline.post
 
 import dagger.hilt.android.scopes.ViewModelScoped
+import org.futo.circles.core.extensions.getPostContentType
 import org.futo.circles.core.mapping.toPost
 import org.futo.circles.core.model.Post
 import org.futo.circles.core.model.PostContent
-import org.futo.circles.core.model.PostContentType
 import org.futo.circles.core.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.getRoom
 import org.matrix.android.sdk.api.session.room.getTimelineEvent
-import org.matrix.android.sdk.api.session.room.timeline.TimelineEvent
-import org.matrix.android.sdk.api.session.room.timeline.getLastMessageContent
 import javax.inject.Inject
 
 
@@ -21,16 +19,10 @@ class PostContentDataSource @Inject constructor() {
     fun getPost(roomId: String, eventId: String): Post? {
         val roomForMessage = session?.getRoom(roomId)
         val timelineEvent = roomForMessage?.getTimelineEvent(eventId) ?: return null
-        return getPostContentTypeFor(timelineEvent)?.let { timelineEvent.toPost(it) }
+        return timelineEvent.getPostContentType()?.let { timelineEvent.toPost(it) }
     }
 
     fun getPostContent(roomId: String, eventId: String): PostContent? =
         getPost(roomId, eventId)?.content
-
-
-    private fun getPostContentTypeFor(event: TimelineEvent): PostContentType? {
-        val messageType = event.getLastMessageContent()?.msgType
-        return PostContentType.values().firstOrNull { it.typeKey == messageType }
-    }
 
 }
