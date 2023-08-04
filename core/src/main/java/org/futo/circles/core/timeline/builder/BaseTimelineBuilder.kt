26f2254bc8eeaa2151b61fabf4bcbca10dd905cc
Taras Smakula <tarassmakula@gmail.com>
2023-08-04 15:36:40 +0300

    Create base timeline bilder

---
+++ core/src/main/java/org/futo/circles/core/timeline/builder/BaseTimelineBuilder.kt
@@ -0,0 +1,32 @@
+package org.futo.circles.core.timeline.builder
+
+import org.futo.circles.core.model.Post
+import org.matrix.android.sdk.api.session.events.model.EventType
+import org.matrix.android.sdk.api.session.room.timeline.TimelineEvent
+import org.matrix.android.sdk.api.session.room.timeline.isEdition
+import org.matrix.android.sdk.api.session.room.timeline.isReply
+
+abstract class BaseTimelineBuilder {
+
+    private val supportedTimelineEvens: List<String> =
+        listOf(EventType.MESSAGE, EventType.POLL_START.stable, EventType.POLL_START.unstable)
+
+    abstract fun List<TimelineEvent>.processSnapshot(isThread: Boolean): List<Post>
+
+    fun build(snapshot: List<TimelineEvent>, isThread: Boolean): List<Post> = snapshot
+        .filterTimelineEvents(isThread)
+        .processSnapshot(isThread)
+
+    protected fun sortList(list: List<Post>, isThread: Boolean) =
+        if (isThread) list.sortedBy { it.postInfo.timestamp }
+        else list.sortedByDescending { it.postInfo.timestamp }
+
+    private fun List<TimelineEvent>.filterTimelineEvents(isThread: Boolean): List<TimelineEvent> =
+        filter {
+            val isSupportedEvent = it.root.getClearType() in supportedTimelineEvens
+            val isNotRemovedEvent = !it.isEdition() && !it.root.isRedacted()
+
+            if (isThread) isSupportedEvent && isNotRemovedEvent
+            else isSupportedEvent && isNotRemovedEvent && !it.isReply()
+        }
+}
