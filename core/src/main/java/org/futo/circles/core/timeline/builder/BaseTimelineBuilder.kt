84fdda43d9067918af66b50ccaa5168a36cbdbbb
Taras Smakula <tarassmakula@gmail.com>
2023-08-14 18:12:32 +0300

    Update mark as read logic

--- core/src/main/java/org/futo/circles/core/timeline/builder/BaseTimelineBuilder.kt
+++ core/src/main/java/org/futo/circles/core/timeline/builder/BaseTimelineBuilder.kt
@@ -1,7 +1,11 @@
 package org.futo.circles.core.timeline.builder
 
 import org.futo.circles.core.model.Post
+import org.futo.circles.core.provider.MatrixSessionProvider
 import org.matrix.android.sdk.api.session.events.model.EventType
+import org.matrix.android.sdk.api.session.getRoomSummary
+import org.matrix.android.sdk.api.session.room.Room
+import org.matrix.android.sdk.api.session.room.getTimelineEvent
 import org.matrix.android.sdk.api.session.room.timeline.TimelineEvent
 import org.matrix.android.sdk.api.session.room.timeline.isEdition
 import org.matrix.android.sdk.api.session.room.timeline.isReply
@@ -20,6 +24,13 @@ abstract class BaseTimelineBuilder {
     protected fun sortList(list: List<Post>, isThread: Boolean) =
         if (isThread) list.sortedBy { it.postInfo.timestamp }
         else list.sortedByDescending { it.postInfo.timestamp }
+
+    protected fun getReadReceipts(room: Room): List<Long> =
+        MatrixSessionProvider.currentSession?.getRoomSummary(room.roomId)?.otherMemberIds?.map {
+            val eventId = room.readService().getUserReadReceipt(it)
+                ?: return@map System.currentTimeMillis()
+            room.getTimelineEvent(eventId)?.root?.originServerTs ?: System.currentTimeMillis()
+        } ?: emptyList()
 
     private fun List<TimelineEvent>.filterTimelineEvents(isThread: Boolean): List<TimelineEvent> =
         filter {
