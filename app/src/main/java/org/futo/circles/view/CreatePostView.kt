14502d069cd237b4139286bf6846471860bdeb44
Taras Smakula <tarassmakula@gmail.com>
2023-08-17 16:38:33 +0300

    Change create post scroll behaviour

--- app/src/main/java/org/futo/circles/view/CreatePostView.kt
+++ app/src/main/java/org/futo/circles/view/CreatePostView.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import org.futo.circles.R
 import org.futo.circles.core.extensions.gone
 import org.futo.circles.core.extensions.loadProfileIcon
 import org.futo.circles.core.extensions.notEmptyDisplayName
@@ -31,7 +30,12 @@ class CreatePostView(
     private var listener: CreatePostViewListener? = null
 
     init {
-        setBackgroundColor(ContextCompat.getColor(context, org.futo.circles.core.R.color.post_card_background_color))
+        setBackgroundColor(
+            ContextCompat.getColor(
+                context,
+                org.futo.circles.core.R.color.post_card_background_color
+            )
+        )
         setOnClickListener { listener?.onCreatePost() }
         binding.ivCreatePoll.setOnClickListener { listener?.onCreatePoll() }
     }
@@ -54,10 +58,6 @@ class CreatePostView(
         recycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                if (!recyclerView.canScrollVertically(1)) {
-                    visible()
-                    return
-                }
                 if (dy > 0) gone() else if (dy < 0) visible()
             }
         })
