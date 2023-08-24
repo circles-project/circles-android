95aae5ed43e2a7b7b81abc5c68e3e7a4a552e32a
Taras Smakula <tarassmakula@gmail.com>
2023-08-24 17:08:48 +0300

    Create BSSpekeClientProvider

---
+++ auth/src/main/java/org/futo/circles/auth/bsspeke/BSSpekeClientProvider.kt
@@ -0,0 +1,23 @@
+package org.futo.circles.auth.bsspeke
+
+object BSSpekeClientProvider {
+
+    private var clientInstance: BSSpekeClient? = null
+
+    fun getClientOrThrow() =
+        clientInstance ?: throw IllegalArgumentException("BsSpeke client is not initialized")
+
+    fun initClient(userId: String, password: String) {
+        val serverId = userId.substringAfter(":")
+        clientInstance = BSSpekeClient(userId, serverId, password)
+    }
+
+    fun initClient(userPart: String, domain: String, password: String) {
+        clientInstance = BSSpekeClient("@$userPart:$domain", domain, password)
+    }
+
+    fun clear() {
+        clientInstance = null
+    }
+
+}
