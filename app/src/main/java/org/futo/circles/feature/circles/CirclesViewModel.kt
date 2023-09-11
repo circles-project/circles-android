96044a184a9a36da1ef74052775b701831f52e1c
Taras Smakula <tarassmakula@gmail.com>
2023-09-11 12:48:13 +0300

    Add createTimeLineIfNotExist

--- app/src/main/java/org/futo/circles/feature/circles/CirclesViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/circles/CirclesViewModel.kt
@@ -1,23 +1,35 @@
 package org.futo.circles.feature.circles
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import org.futo.circles.R
 import org.futo.circles.core.SingleEventLiveData
 import org.futo.circles.core.extensions.Response
+import org.futo.circles.core.extensions.createResult
 import org.futo.circles.core.extensions.launchBg
+import org.futo.circles.core.model.LoadingData
+import org.futo.circles.core.provider.MatrixSessionProvider
+import org.futo.circles.core.room.CreateRoomDataSource
 import org.futo.circles.core.room.invite.InviteRequestsDataSource
+import org.futo.circles.core.utils.getTimelineRoomFor
 import org.futo.circles.model.RequestCircleListItem
+import org.matrix.android.sdk.api.session.getRoomSummary
 import javax.inject.Inject
 
 @HiltViewModel
 class CirclesViewModel @Inject constructor(
     dataSource: CirclesDataSource,
-    private val inviteRequestsDataSource: InviteRequestsDataSource
+    private val inviteRequestsDataSource: InviteRequestsDataSource,
+    private val createRoomDataSource: CreateRoomDataSource
 ) : ViewModel() {
 
     val roomsLiveData = dataSource.getCirclesFlow().asLiveData()
     val inviteResultLiveData = SingleEventLiveData<Response<Unit?>>()
+    val navigateToCircleLiveData = SingleEventLiveData<Response<String>>()
+    val createTimelineLoadingLiveData = MutableLiveData<LoadingData>()
 
     fun rejectInvite(roomId: String) {
         launchBg {
@@ -35,6 +47,25 @@ class CirclesViewModel @Inject constructor(
 
     fun kickUser(room: RequestCircleListItem) {
         launchBg { inviteRequestsDataSource.kickUser(room.id, room.requesterId) }
+    }
+
+    fun createTimeLineIfNotExist(circleId: String) {
+        launchBg {
+            val result = createResult {
+                if (getTimelineRoomFor(circleId) == null) {
+                    createTimelineLoadingLiveData.postValue(
+                        LoadingData(R.string.creating_timeline, total = 0)
+                    )
+                    val name =
+                        MatrixSessionProvider.getSessionOrThrow().getRoomSummary(circleId)?.name
+                    createRoomDataSource.createCircleTimeline(circleId, name)
+                    delay(500L)
+                }
+                circleId
+            }
+            createTimelineLoadingLiveData.postValue(LoadingData(isLoading = false))
+            navigateToCircleLiveData.postValue(result)
+        }
     }
 
 }
