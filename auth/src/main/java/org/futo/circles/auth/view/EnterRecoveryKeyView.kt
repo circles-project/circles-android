453e08f2dfddba88b2d960e3ad028316c4e47b9b
Taras Smakula <tarassmakula@gmail.com>
2023-09-12 16:01:04 +0300

    Create enter recovery view key

---
+++ auth/src/main/java/org/futo/circles/auth/view/EnterRecoveryKeyView.kt
@@ -0,0 +1,60 @@
+package org.futo.circles.auth.view
+
+import android.content.Context
+import android.net.Uri
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import org.futo.circles.auth.databinding.ViewEnterRecoveryKeyBinding
+import org.futo.circles.core.extensions.getFilename
+import org.futo.circles.core.extensions.gone
+import org.futo.circles.core.extensions.visible
+
+class EnterRecoveryKeyView(
+    context: Context,
+    attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+
+    private val binding = ViewEnterRecoveryKeyBinding.inflate(LayoutInflater.from(context), this)
+
+    private var selectedFileUri: Uri? = null
+    private var onFileUploadListener: (() -> Unit)? = null
+    private var onInputChangedListener: (() -> Unit)? = null
+
+    init {
+        binding.ivRemoveFile.setOnClickListener {
+            selectedFileUri = null
+            binding.passPhraseGroup.visible()
+            binding.fileNameGroup.gone()
+            handleOnInputChanged()
+        }
+        binding.btnUploadFile.setOnClickListener {
+            onFileUploadListener?.invoke()
+        }
+    }
+
+    fun setup(
+        onUploadFileListener: () -> Unit,
+        onInputChanged: () -> Unit
+    ) {
+        onFileUploadListener = onUploadFileListener
+        onInputChangedListener = onInputChanged
+    }
+
+    fun getRawKey(): String? = binding.tilRecoveryKey.editText?.text?.toString()
+    fun getFileUri(): Uri? = selectedFileUri
+
+    fun selectFile(uri: Uri) {
+        selectedFileUri = uri
+        binding.tvFileName.text = uri.getFilename(context) ?: uri.toString()
+        binding.passPhraseGroup.gone()
+        binding.fileNameGroup.visible()
+        handleOnInputChanged()
+    }
+
+    private fun handleOnInputChanged() {
+        onInputChangedListener?.invoke()
+    }
+
+
+}
