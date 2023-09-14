faaafb389246f433f84c52b1a2a2c84f8a5a8def
Taras Smakula <tarassmakula@gmail.com>
2023-09-14 16:01:33 +0300

    Setup Explanation dialog ui

--- auth/src/main/java/org/futo/circles/auth/explanation/CirclesExplanationDialog.kt
+++ auth/src/main/java/org/futo/circles/auth/explanation/CirclesExplanationDialog.kt
@@ -6,11 +6,20 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.InsetDrawable
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Space
 import androidx.appcompat.app.AppCompatDialog
+import org.futo.circles.auth.R
 import org.futo.circles.auth.databinding.DialogCirclesExplanationBinding
+import org.futo.circles.core.model.CircleRoomTypeArg
+import org.futo.circles.core.model.CircleRoomTypeArg.Group
 
-class CirclesExplanationDialog(context: Context) : AppCompatDialog(context) {
+
+class CirclesExplanationDialog(context: Context, private val roomType: CircleRoomTypeArg) :
+    AppCompatDialog(context) {
 
     private val binding = DialogCirclesExplanationBinding.inflate(LayoutInflater.from(context))
 
@@ -22,6 +31,64 @@ class CirclesExplanationDialog(context: Context) : AppCompatDialog(context) {
             setBackgroundDrawable(InsetDrawable(ColorDrawable(Color.TRANSPARENT), 20))
             setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT)
         }
-
+        setupViews()
     }
+
+    private fun setupViews() {
+        with(binding) {
+            btnDone.setOnClickListener { this@CirclesExplanationDialog.dismiss() }
+            when (roomType) {
+                Group -> {
+                    tvTitle.setText(R.string.group)
+                    tvDescription.text = Html.fromHtml(
+                        context.getString(R.string.group_explanation),
+                        Html.FROM_HTML_MODE_COMPACT
+                    )
+                    setupImages(listOf(R.drawable.explanation_groups))
+                }
+
+                else -> {
+                    tvTitle.setText(R.string.circle)
+                    tvDescription.text = Html.fromHtml(
+                        context.getString(R.string.circle_explanation),
+                        Html.FROM_HTML_MODE_COMPACT
+                    )
+                    setupImages(
+                        listOf(
+                            R.drawable.explanation_circles_1,
+                            R.drawable.explanation_circles_2,
+                            R.drawable.explanation_circles_3,
+                            R.drawable.explanation_circles_4
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun setupImages(images: List<Int>) {
+        if (images.isEmpty()) return
+        val spaceWeight = if (images.size == 1) 0f else images.size.toFloat() / (images.size - 1)
+        val imageWeight = (100f - images.size * spaceWeight) / images.size.toFloat()
+        images.forEachIndexed { index, i ->
+            binding.lImages.addView(ImageView(context).apply {
+                adjustViewBounds = true
+                layoutParams =
+                    LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+                        weight = imageWeight
+                    }
+                setImageResource(i)
+            })
+            if (index != images.size - 1) {
+                binding.lImages.addView(Space(context).apply {
+                    layoutParams =
+                        LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+                            weight = spaceWeight
+                        }
+                })
+            }
+        }
+    }
+
+
 }
