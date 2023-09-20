0f8c0bc372c9d3cb9f64112a9e6eaa8426fb2cd6
Taras Smakula <tarassmakula@gmail.com>
2023-09-20 17:32:51 +0300

    Create ConfigureWorkspaceViewModel

---
+++ core/src/main/java/org/futo/circles/core/workspace/ConfigureWorkspaceViewModel.kt
@@ -0,0 +1,11 @@
+package org.futo.circles.core.workspace
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ConfigureWorkspaceViewModel @Inject constructor(
+) : ViewModel() {
+
+}
