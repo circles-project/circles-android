2bda15fd43f5da66e70d4aafefe76fe02fcc56c4
Taras Smakula <tarassmakula@gmail.com>
2023-09-22 15:06:28 +0300

    Use room name as title

--- core/src/main/java/org/futo/circles/core/model/WorkspaceTask.kt
+++ core/src/main/java/org/futo/circles/core/model/WorkspaceTask.kt
@@ -4,26 +4,24 @@ import org.futo.circles.core.list.IdEntity
 
 sealed class WorkspaceTask(
     open val room: CirclesRoom,
-    open val titleResId: Int,
     open val descriptionResId: Int,
     open val status: TaskStatus
 ) : IdEntity<Int>
 
+
 data class MandatoryWorkspaceTask(
     override val room: CirclesRoom,
-    override val titleResId: Int,
     override val descriptionResId: Int,
     override val status: TaskStatus = TaskStatus.IDLE
-) : WorkspaceTask(room, titleResId, descriptionResId, status) {
-    override val id: Int = titleResId
+) : WorkspaceTask(room, descriptionResId, status) {
+    override val id: Int = descriptionResId
 }
 
 data class OptionalWorkspaceTask(
     override val room: CirclesRoom,
-    override val titleResId: Int,
     override val descriptionResId: Int,
     override val status: TaskStatus = TaskStatus.IDLE,
     val isSelected: Boolean = true
-) : WorkspaceTask(room, titleResId, descriptionResId, status) {
-    override val id: Int = titleResId
+) : WorkspaceTask(room, descriptionResId, status) {
+    override val id: Int = descriptionResId
 }
