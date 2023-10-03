1a11131caca7efb159253b94631e13d2bc290ed0
Taras Smakula <tarassmakula@gmail.com>
2023-10-03 17:20:25 +0300

    show no internet for activity

--- core/src/main/java/org/futo/circles/core/BaseActivity.kt
+++ core/src/main/java/org/futo/circles/core/BaseActivity.kt
@@ -1,9 +1,13 @@
 package org.futo.circles.core
 
+import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.internal.findRootView
 import org.futo.circles.core.rageshake.BugReportDataCollector
 import org.futo.circles.core.rageshake.RageShake
 import javax.inject.Inject
+
 
 abstract class BaseActivity(contentLayoutId: Int) : AppCompatActivity(contentLayoutId) {
 
@@ -11,6 +15,12 @@ abstract class BaseActivity(contentLayoutId: Int) : AppCompatActivity(contentLay
     lateinit var bugReportDataCollector: BugReportDataCollector
 
     private val rageShake by lazy { RageShake(this, bugReportDataCollector) }
+    private val noInternetConnectionPresenter = NoInternetConnectionViewPresenter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        noInternetConnectionPresenter.register(this, findRootView(this) as? ViewGroup)
+    }
 
     override fun onResume() {
         super.onResume()
@@ -20,5 +30,10 @@ abstract class BaseActivity(contentLayoutId: Int) : AppCompatActivity(contentLay
     override fun onPause() {
         super.onPause()
         if (CirclesAppConfig.isRageshakeEnabled) rageShake.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        noInternetConnectionPresenter.unregister(this)
     }
 }
