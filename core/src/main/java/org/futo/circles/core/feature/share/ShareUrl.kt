e2359539afcf38ede4f9e9ceec4ccb3f6a31a845
Taras Smakula <tarassmakula@gmail.com>
2023-10-27 15:32:30 +0300

    Change url building

--- core/src/main/java/org/futo/circles/core/feature/share/ShareUrl.kt
+++ core/src/main/java/org/futo/circles/core/feature/share/ShareUrl.kt
@@ -1,15 +1,8 @@
 package org.futo.circles.core.feature.share
 
-import org.futo.circles.core.provider.MatrixSessionProvider
+import org.futo.circles.core.model.ShareUrlTypeArg
 
 private const val BASE_SHARE_URL = "https://circu.li/"
-const val SHARE_ROOM_URL_PREFIX = "https://circu.li/room/"
-const val SHARE_PROFILE_URL_PREFIX = "https://circu.li/profile/"
 
-fun buildShareRoomUrl(roomId: String, roomName: String, topic: String?) =
-    SHARE_ROOM_URL_PREFIX + roomId + "/$roomName" + if (topic.isNullOrEmpty()) "" else "/$topic"
-
-fun buildShareProfileUrl(sharedSpaceId: String) =
-    MatrixSessionProvider.currentSession?.myUserId?.let { userId ->
-        "$SHARE_PROFILE_URL_PREFIX$userId/$sharedSpaceId"
-    } ?: ""
+fun buildShareRoomUrl(type: ShareUrlTypeArg, roomId: String) =
+    BASE_SHARE_URL + type.typeKey + "/$roomId"
