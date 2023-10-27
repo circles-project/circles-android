d7602a05b90b1e8567966665f707e3672c61a785
Taras Smakula <tarassmakula@gmail.com>
2023-10-27 15:32:43 +0300

    Create ShareUrlTypeArg

---
+++ core/src/main/java/org/futo/circles/core/model/ShareUrlTypeArg.kt
@@ -0,0 +1,9 @@
+package org.futo.circles.core.model
+
+enum class ShareUrlTypeArg(val typeKey: String) {
+    ROOM("room"),
+    PROFILE("profile"),
+    GALLERY("galley"),
+    GROUP("group"),
+    TIMELINE("timeline")
+}
