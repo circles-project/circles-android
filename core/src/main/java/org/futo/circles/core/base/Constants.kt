08af2cf5cba430a0d0eedd250d61a1631ace27df
Taras Smakula <tarassmakula@gmail.com>
2023-11-14 12:09:23 +0200

    Room version 11

--- core/src/main/java/org/futo/circles/core/base/Constants.kt
+++ core/src/main/java/org/futo/circles/core/base/Constants.kt
@@ -6,6 +6,7 @@ import org.futo.circles.core.provider.MatrixSessionProvider
 const val FILE_PROVIDER_AUTHORITY_EXTENSION = ".provider"
 const val MediaCaptionFieldKey = "caption"
 const val READ_ONLY_ROLE = -10
+const val DEFAULT_ROOM_VERSION = "11"
 
 fun getCirclesDomain(): String {
     if (BuildConfig.DEBUG) return CirclesAppConfig.usServerDomain
