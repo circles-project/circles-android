0b5055895113e6ae663c03a0ee3c30cbeaee516e
Taras Smakula <tarassmakula@gmail.com>
2023-11-28 16:10:55 +0200

    Change composer menu

--- app/src/main/java/org/futo/circles/view/RichTextComposerLayout.kt
+++ app/src/main/java/org/futo/circles/view/RichTextComposerLayout.kt
@@ -9,18 +9,15 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.cardview.widget.CardView
 import androidx.core.view.isVisible
-import androidx.core.widget.doOnTextChanged
 import io.element.android.wysiwyg.EditorEditText
-import io.element.android.wysiwyg.display.KeywordDisplayHandler
 import io.element.android.wysiwyg.display.LinkDisplayHandler
 import io.element.android.wysiwyg.display.TextDisplay
 import io.element.android.wysiwyg.view.models.InlineFormat
-import io.element.android.wysiwyg.view.models.LinkAction
 import org.futo.circles.R
 import org.futo.circles.databinding.ComposerRichTextLayoutBinding
 import org.futo.circles.databinding.ViewRichTextMenuButtonBinding
@@ -32,66 +29,46 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr), MessageComposerView {
+) : LinearLayout(context, attrs, defStyleAttr) {
 
     private val views: ComposerRichTextLayoutBinding
 
-    override var callback: Callback? = null
-
-    override val text: Editable?
+    private val text: Editable?
         get() = editText.text
-    override val formattedText: String?
-        get() = (editText as? EditorEditText)?.getContentAsMessageHtml()
-    override val editText: EditText
+
+    private val editText: EditText
         get() = views.richTextComposerEditText
 
-    override val emojiButton: ImageButton?
-        get() = null
 
-    var pillDisplayHandler: PillDisplayHandler? = null
+    private var pillDisplayHandler: PillDisplayHandler? = null
 
     init {
         inflate(context, R.layout.composer_rich_text_layout, this)
         views = ComposerRichTextLayoutBinding.bind(this)
 
-        // Workaround to avoid cut-off text caused by padding in scrolled TextView (there is no clipToPadding).
-        // In TextView, clipTop = padding, but also clipTop -= shadowRadius. So if we set the shadowRadius to padding, they cancel each other
         views.richTextComposerEditText.setShadowLayer(
-            views.richTextComposerEditText.paddingBottom.toFloat(),
-            0f,
-            0f,
-            0
+            views.richTextComposerEditText.paddingBottom.toFloat(), 0f, 0f, 0
         )
-
-        views.richTextComposerEditText.doOnTextChanged { s, _, _, _ ->
-            callback?.onTextChanged(s ?: "")
-        }
 
         disallowParentInterceptTouchEvent(views.richTextComposerEditText)
 
         setupRichTextMenu()
+
         views.richTextComposerEditText.linkDisplayHandler = LinkDisplayHandler { text, url ->
             pillDisplayHandler?.resolveLinkDisplay(text, url) ?: TextDisplay.Plain
-        }
-        views.richTextComposerEditText.keywordDisplayHandler = object : KeywordDisplayHandler {
-            override val keywords: List<String>
-                get() = pillDisplayHandler?.keywords.orEmpty()
-
-            override fun resolveKeywordDisplay(text: String): TextDisplay =
-                pillDisplayHandler?.resolveKeywordDisplay(text) ?: TextDisplay.Plain
         }
     }
 
     private fun setupRichTextMenu() {
         addRichTextMenuItem(
-            R.drawable.ic_composer_bold,
+            R.drawable.ic_bold,
             R.string.rich_text_editor_format_bold,
             ComposerAction.BOLD
         ) {
             views.richTextComposerEditText.toggleInlineFormat(InlineFormat.Bold)
         }
         addRichTextMenuItem(
-            R.drawable.ic_composer_italic,
+            R.drawable.ic_italic,
             R.string.rich_text_editor_format_italic,
             ComposerAction.ITALIC
         ) {
@@ -105,21 +82,21 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
             views.richTextComposerEditText.toggleInlineFormat(InlineFormat.Underline)
         }
         addRichTextMenuItem(
-            R.drawable.ic_composer_strikethrough,
+            R.drawable.ic_strikethrough,
             R.string.rich_text_editor_format_strikethrough,
             ComposerAction.STRIKE_THROUGH
         ) {
             views.richTextComposerEditText.toggleInlineFormat(InlineFormat.StrikeThrough)
         }
         addRichTextMenuItem(
-            R.drawable.ic_composer_bullet_list,
+            R.drawable.ic_bullet_list,
             R.string.rich_text_editor_bullet_list,
             ComposerAction.UNORDERED_LIST
         ) {
             views.richTextComposerEditText.toggleList(ordered = false)
         }
         addRichTextMenuItem(
-            R.drawable.ic_composer_numbered_list,
+            R.drawable.ic_number_list,
             R.string.rich_text_editor_numbered_list,
             ComposerAction.ORDERED_LIST
         ) {
@@ -147,37 +124,11 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
             views.richTextComposerEditText.toggleQuote()
         }
         addRichTextMenuItem(
-            R.drawable.ic_composer_inline_code,
-            R.string.rich_text_editor_inline_code,
-            ComposerAction.INLINE_CODE
-        ) {
-            views.richTextComposerEditText.toggleInlineFormat(InlineFormat.InlineCode)
-        }
-        addRichTextMenuItem(
             R.drawable.ic_composer_code_block,
             R.string.rich_text_editor_code_block,
             ComposerAction.CODE_BLOCK
         ) {
             views.richTextComposerEditText.toggleCodeBlock()
-        }
-        addRichTextMenuItem(
-            R.drawable.ic_composer_link,
-            R.string.rich_text_editor_link,
-            ComposerAction.LINK
-        ) {
-            views.richTextComposerEditText.getLinkAction()?.let {
-                when (it) {
-                    LinkAction.InsertLink -> callback?.onSetLink(
-                        isTextSupported = true,
-                        initialLink = null
-                    )
-
-                    is LinkAction.SetLink -> callback?.onSetLink(
-                        isTextSupported = false,
-                        initialLink = it.currentUrl
-                    )
-                }
-            }
         }
     }
 
@@ -210,12 +161,9 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-
         views.richTextComposerEditText.actionStatesChangedListener =
             EditorEditText.OnActionStatesChangedListener { state ->
-                for (action in state.keys) {
-                    updateMenuStateFor(action, state)
-                }
+                for (action in state.keys) { updateMenuStateFor(action, state) }
             }
     }
 
@@ -227,14 +175,12 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
         onClick: () -> Unit
     ) {
         val inflater = LayoutInflater.from(context)
-        val button = ViewRichTextMenuButtonBinding.inflate(inflater, views.richTextMenu, true)
-        button.root.tag = action
-        with(button.root) {
+        val item = ViewRichTextMenuButtonBinding.inflate(inflater, views.richTextMenu, true)
+        item.root.tag = action
+        with(item.root) {
             contentDescription = resources.getString(description)
-            setImageResource(iconId)
-            setOnClickListener {
-                onClick()
-            }
+            item.ivIcon.setImageResource(iconId)
+            setOnClickListener { onClick() }
         }
     }
 
@@ -242,7 +188,7 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
         action: ComposerAction,
         menuState: Map<ComposerAction, ActionState>
     ) {
-        val button = findViewWithTag<ImageButton>(action) ?: return
+        val button = findViewWithTag<CardView>(action) ?: return
         val stateForAction = menuState[action]
         button.isEnabled = stateForAction != ActionState.DISABLED
         button.isSelected = stateForAction == ActionState.REVERSED
@@ -255,7 +201,7 @@ internal class RichTextComposerLayout @JvmOverloads constructor(
         }
     }
 
-    override fun setTextIfDifferent(text: CharSequence?): Boolean {
+    private fun setTextIfDifferent(text: CharSequence?): Boolean {
         return editText.setTextIfDifferent(text)
     }
 
