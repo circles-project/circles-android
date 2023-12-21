a7faf61c86d2d6a6095655f1d5b918418730a315
Taras Smakula <tarassmakula@gmail.com>
2023-12-21 15:10:18 +0200

    Add ignored users to settings

--- app/src/main/java/org/futo/circles/feature/settings/SettingsFragment.kt
+++ app/src/main/java/org/futo/circles/feature/settings/SettingsFragment.kt
@@ -76,6 +76,7 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
             tvVersion.setOnLongClickListener { toggleDeveloperMode(); true }
             tvPushNotifications.setOnClickListener { navigator.navigateToPushSettings() }
             ivShareProfile.setOnClickListener { navigator.navigateToShareProfile(viewModel.getSharedCircleSpaceId()) }
+            tvIgnoredUsers.setOnClickListener { navigator.navigateToIgnoredUsers() }
         }
         setVersion()
     }
