aaa2834ed1bc5b7faddeb5d20adcd0d5bf7aed3c
Taras Smakula <tarassmakula@gmail.com>
2024-01-25 12:00:21 +0200

    Set circles.futo.org for deeplinks

--- app/src/main/java/org/futo/circles/model/UrlData.kt
+++ app/src/main/java/org/futo/circles/model/UrlData.kt
@@ -1,6 +1,7 @@
 package org.futo.circles.model
 
 import org.futo.circles.core.feature.share.BASE_SHARE_URL
+import org.futo.circles.core.feature.share.LEGACY_BASE_SHARE_URL
 import org.futo.circles.core.model.ShareUrlTypeArg
 import org.futo.circles.core.model.shareUrlTypeArgFromType
 
@@ -10,7 +11,11 @@ data class RoomUrlData(
 )
 
 fun parseUrlData(url: String): RoomUrlData? {
-    val data = url.removePrefix(BASE_SHARE_URL).split("/")
+    val data = if (url.startsWith(BASE_SHARE_URL)) url.removePrefix(BASE_SHARE_URL).split("/")
+    else if (url.startsWith(LEGACY_BASE_SHARE_URL)) url.removePrefix(LEGACY_BASE_SHARE_URL)
+        .split("/")
+    else return null
+
     val typeString = data.getOrNull(0) ?: return null
     val type = shareUrlTypeArgFromType(typeString) ?: return null
     val roomId = data.getOrNull(1) ?: return null
