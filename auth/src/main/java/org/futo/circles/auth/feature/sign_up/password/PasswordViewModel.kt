9d20ecc06890721ae6783eebb0fc417b0b2653e0
Taras Smakula <tarassmakula@gmail.com>
2024-02-07 14:32:18 +0200

    Show warning on password stage

--- auth/src/main/java/org/futo/circles/auth/feature/sign_up/password/PasswordViewModel.kt
+++ auth/src/main/java/org/futo/circles/auth/feature/sign_up/password/PasswordViewModel.kt
@@ -13,12 +13,18 @@ class PasswordViewModel @Inject constructor(
     private val passwordDataSource: PasswordDataSource
 ) : ViewModel() {
 
+    private var isPasswordWarningConfirmed: Boolean = false
     val passwordResponseLiveData = SingleEventLiveData<Response<Unit>>()
 
     fun loginWithPassword(password: String) {
         launchBg {
             passwordResponseLiveData.postValue(passwordDataSource.processPasswordStage(password))
         }
+    }
+
+    fun isPasswordWarningConfirmed() = isPasswordWarningConfirmed
+    fun confirmPasswordWarning() {
+        isPasswordWarningConfirmed = true
     }
 
 }
