d2571ca5a532d74374b708b3b04662aed7329bf9
Taras Smakula <tarassmakula@gmail.com>
2024-02-21 16:51:42 +0200

    Title and list for category screen

--- app/src/main/java/org/futo/circles/feature/people/category/PeopleCategoryDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/people/category/PeopleCategoryDialogFragment.kt
@@ -5,8 +5,10 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
+import org.futo.circles.R
 import org.futo.circles.core.base.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.core.extensions.navigateSafe
 import org.futo.circles.core.extensions.observeData
@@ -17,12 +19,15 @@ import org.futo.circles.core.utils.LauncherActivityUtils
 import org.futo.circles.core.view.EmptyTabPlaceholderView
 import org.futo.circles.databinding.DialogFragmentPeopleCategoryBinding
 import org.futo.circles.feature.people.list.PeopleAdapter
+import org.futo.circles.model.PeopleCategoryTypeArg
 
 @AndroidEntryPoint
 class PeopleCategoryDialogFragment :
     BaseFullscreenDialogFragment(DialogFragmentPeopleCategoryBinding::inflate) {
 
     private val viewModel by viewModels<PeopleCategoryViewModel>()
+
+    private val args: PeopleCategoryDialogFragmentArgs by navArgs()
 
     private val binding by lazy {
         getBinding() as DialogFragmentPeopleCategoryBinding
@@ -48,19 +53,29 @@ class PeopleCategoryDialogFragment :
     }
 
     private fun setupViews() {
-        binding.rvUsers.apply {
-            setEmptyView(EmptyTabPlaceholderView(requireContext()).apply {
-                //setText(getString(R.string.ignored_users_empty_message))
-            })
-            adapter = usersAdapter
-            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        with(binding) {
+            val title = getString(
+                when (args.categoryType) {
+                    PeopleCategoryTypeArg.Connections -> R.string.my_connections
+                    PeopleCategoryTypeArg.Followers -> org.futo.circles.core.R.string.my_followers
+                    PeopleCategoryTypeArg.Following -> org.futo.circles.core.R.string.people_i_m_following
+                    PeopleCategoryTypeArg.Other -> org.futo.circles.core.R.string.other_known_users
+                    PeopleCategoryTypeArg.Ignored -> R.string.ignored_users
+                }
+            )
+            toolbar.title = title
+            rvUsers.apply {
+                setEmptyView(EmptyTabPlaceholderView(requireContext()).apply {
+                    setText(getString(R.string.people_category_empty_message, title))
+                })
+                adapter = usersAdapter
+                addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+            }
         }
     }
 
     private fun setupObservers() {
-        viewModel.ignoredUsersLiveData.observeData(this) {
-            //usersAdapter.submitList(it)
-        }
+        viewModel.usersLiveData.observeData(this) { usersAdapter.submitList(it) }
         viewModel.unIgnoreUserLiveData.observeResponse(this,
             success = {
                 (activity as? AppCompatActivity)?.let {
--- app/src/main/java/org/futo/circles/feature/people/category/PeopleCategoryViewModel.kt
+++ app/src/main/java/org/futo/circles/feature/people/category/PeopleCategoryViewModel.kt
@@ -1,26 +1,29 @@
 package org.futo.circles.feature.people.category
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.map
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.futo.circles.core.base.SingleEventLiveData
 import org.futo.circles.core.extensions.Response
+import org.futo.circles.core.extensions.getOrThrow
 import org.futo.circles.core.extensions.launchBg
 import org.futo.circles.core.feature.user.UserOptionsDataSource
-import org.futo.circles.core.mapping.toCirclesUserSummary
-import org.futo.circles.core.provider.MatrixSessionProvider
+import org.futo.circles.model.PeopleCategoryTypeArg
 import javax.inject.Inject
 
 @HiltViewModel
 class PeopleCategoryViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    peopleCategoryDataSource: PeopleCategoryDataSource,
     private val userOptionsDataSource: UserOptionsDataSource
 ) : ViewModel() {
 
-    val unIgnoreUserLiveData = SingleEventLiveData<Response<Unit?>>()
+    private val categoryType: PeopleCategoryTypeArg = savedStateHandle.getOrThrow("categoryType")
+    val usersLiveData =
+        peopleCategoryDataSource.getUsersListByCategoryFlow(categoryType).asLiveData()
 
-    val ignoredUsersLiveData =
-        MatrixSessionProvider.getSessionOrThrow().userService().getIgnoredUsersLive()
-            .map { it.map { it.toCirclesUserSummary() } }
+    val unIgnoreUserLiveData = SingleEventLiveData<Response<Unit?>>()
 
     fun unIgnoreUser(userId: String) {
         launchBg {
