00ddff5e2961dd7016834ef7ae0991080a12ecef
Taras Smakula <tarassmakula@gmail.com>
2024-02-27 17:59:15 +0200

    Update validate email ds

--- auth/src/main/java/org/futo/circles/auth/feature/sign_up/validate_email/ValidateEmailDataSource.kt
+++ auth/src/main/java/org/futo/circles/auth/feature/sign_up/validate_email/ValidateEmailDataSource.kt
@@ -1,21 +1,22 @@
 package org.futo.circles.auth.feature.sign_up.validate_email
 
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource.Companion.REGISTRATION_EMAIL_REQUEST_TOKEN_TYPE
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource.Companion.REGISTRATION_EMAIL_SUBMIT_TOKEN_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.REGISTRATION_EMAIL_REQUEST_TOKEN_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.REGISTRATION_EMAIL_SUBMIT_TOKEN_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.TYPE_PARAM_KEY
+import org.futo.circles.auth.feature.uia.UIADataSourceProvider
 import org.futo.circles.core.extensions.Response
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
 import org.matrix.android.sdk.api.auth.registration.Stage
 import javax.inject.Inject
 
-class ValidateEmailDataSource @Inject constructor(
-    private val signUpDataSource: SignUpDataSource
-) {
+class ValidateEmailDataSource @Inject constructor() {
+
+    private val uiaDataSource = UIADataSourceProvider.getDataSourceOrThrow()
 
     suspend fun sendValidationCode(
         email: String,
         subscribeToUpdates: Boolean
-    ): Response<RegistrationResult> = signUpDataSource.performRegistrationStage(
+    ): Response<RegistrationResult> = uiaDataSource.performUIAStage(
         mapOf(
             TYPE_PARAM_KEY to REGISTRATION_EMAIL_REQUEST_TOKEN_TYPE,
             EMAIL_PARAM_KEY to email,
@@ -24,7 +25,7 @@ class ValidateEmailDataSource @Inject constructor(
     )
 
     suspend fun validateEmail(code: String): Response<RegistrationResult> =
-        signUpDataSource.performRegistrationStage(
+        uiaDataSource.performUIAStage(
             mapOf(
                 TYPE_PARAM_KEY to REGISTRATION_EMAIL_SUBMIT_TOKEN_TYPE,
                 TOKEN_PARAM_KEY to code
@@ -32,7 +33,7 @@ class ValidateEmailDataSource @Inject constructor(
         )
 
     fun shouldShowSubscribeToEmail(): Boolean =
-        (signUpDataSource.currentStage as? Stage.Other)?.params?.get(OFFER_LIST_SUBSCRIPTION_KEY) as? Boolean
+        (uiaDataSource.currentStage as? Stage.Other)?.params?.get(OFFER_LIST_SUBSCRIPTION_KEY) as? Boolean
             ?: false
 
     companion object {
