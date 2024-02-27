4a41bd335f5883820257f05e26214be7b124612d
Taras Smakula <tarassmakula@gmail.com>
2024-02-27 17:53:54 +0200

    Update validate token

--- auth/src/main/java/org/futo/circles/auth/feature/sign_up/validate_token/ValidateTokenDataSource.kt
+++ auth/src/main/java/org/futo/circles/auth/feature/sign_up/validate_token/ValidateTokenDataSource.kt
@@ -1,17 +1,18 @@
 package org.futo.circles.auth.feature.sign_up.validate_token
 
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource.Companion.REGISTRATION_TOKEN_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.REGISTRATION_TOKEN_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.TYPE_PARAM_KEY
+import org.futo.circles.auth.feature.uia.UIADataSourceProvider
 import org.futo.circles.core.extensions.Response
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
 import javax.inject.Inject
 
-class ValidateTokenDataSource @Inject constructor(
-    private val signUpDataSource: SignUpDataSource
-) {
+class ValidateTokenDataSource @Inject constructor() {
+
+    private val uiaDataSource = UIADataSourceProvider.getDataSourceOrThrow()
 
     suspend fun validateToken(token: String): Response<RegistrationResult> =
-        signUpDataSource.performRegistrationStage(
+        uiaDataSource.performUIAStage(
             mapOf(
                 TYPE_PARAM_KEY to REGISTRATION_TOKEN_TYPE,
                 TOKEN_PARAM_KEY to token
