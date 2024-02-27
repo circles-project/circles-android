fc59fcc40de550aa629ee0f80e161bc04c77e0c3
Taras Smakula <tarassmakula@gmail.com>
2024-02-27 18:00:33 +0200

    Update username ds

--- auth/src/main/java/org/futo/circles/auth/feature/sign_up/username/UsernameDataSource.kt
+++ auth/src/main/java/org/futo/circles/auth/feature/sign_up/username/UsernameDataSource.kt
@@ -1,20 +1,21 @@
 package org.futo.circles.auth.feature.sign_up.username
 
 import androidx.lifecycle.MutableLiveData
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource
-import org.futo.circles.auth.feature.sign_up.SignUpDataSource.Companion.REGISTRATION_USERNAME_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.REGISTRATION_USERNAME_TYPE
+import org.futo.circles.auth.feature.uia.UIADataSource.Companion.TYPE_PARAM_KEY
+import org.futo.circles.auth.feature.uia.UIADataSourceProvider
 import org.futo.circles.core.extensions.Response
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
 import javax.inject.Inject
 
-class UsernameDataSource @Inject constructor(
-    private val signUpDataSource: SignUpDataSource
-) {
+class UsernameDataSource @Inject constructor() {
 
-    val domainLiveData = MutableLiveData(signUpDataSource.domain)
+    private val uiaDataSource = UIADataSourceProvider.getDataSourceOrThrow()
+
+    val domainLiveData = MutableLiveData(uiaDataSource.domain)
 
     suspend fun processUsernameStage(username: String): Response<RegistrationResult> =
-        signUpDataSource.performRegistrationStage(
+        uiaDataSource.performUIAStage(
             mapOf(
                 TYPE_PARAM_KEY to REGISTRATION_USERNAME_TYPE,
                 USERNAME_PARAM_KEY to username
