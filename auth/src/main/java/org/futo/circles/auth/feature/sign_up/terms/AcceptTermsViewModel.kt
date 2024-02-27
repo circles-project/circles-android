c40e13a69f16230202e2bae37b851f9fc9efd4c5
Taras Smakula <tarassmakula@gmail.com>
2024-02-27 18:09:24 +0200

    Update accept terms

--- auth/src/main/java/org/futo/circles/auth/feature/sign_up/terms/AcceptTermsViewModel.kt
+++ auth/src/main/java/org/futo/circles/auth/feature/sign_up/terms/AcceptTermsViewModel.kt
@@ -1,23 +1,18 @@
 package org.futo.circles.auth.feature.sign_up.terms
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import org.futo.circles.auth.base.BaseAcceptTermsDataSource
 import org.futo.circles.auth.model.TermsListItem
 import org.futo.circles.core.base.SingleEventLiveData
 import org.futo.circles.core.extensions.Response
-import org.futo.circles.core.extensions.getOrThrow
 import org.futo.circles.core.extensions.launchBg
 import javax.inject.Inject
 
 @HiltViewModel
 class AcceptTermsViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-    dataSourceFactory: BaseAcceptTermsDataSource.Factory
+    private val dataSource: AcceptTermsDataSource
 ) : ViewModel() {
 
-    private val dataSource = dataSourceFactory.create(savedStateHandle.getOrThrow("mode"))
 
     val acceptTermsLiveData = SingleEventLiveData<Response<Unit>>()
     val termsListLiveData = dataSource.termsListLiveData
