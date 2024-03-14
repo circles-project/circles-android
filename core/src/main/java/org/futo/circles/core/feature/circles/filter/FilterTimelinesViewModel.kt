a7135c85a38b7c24610181f3324b40d97ca7cb00
Taras Smakula <tarassmakula@gmail.com>
2024-03-14 15:45:46 +0200

    Implement view model

--- core/src/main/java/org/futo/circles/core/feature/circles/filter/FilterTimelinesViewModel.kt
+++ core/src/main/java/org/futo/circles/core/feature/circles/filter/FilterTimelinesViewModel.kt
@@ -2,14 +2,29 @@ package org.futo.circles.core.feature.circles.filter
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.futo.circles.core.base.SingleEventLiveData
+import org.futo.circles.core.extensions.Response
+import org.futo.circles.core.extensions.launchBg
 import javax.inject.Inject
 
 @HiltViewModel
 class FilterTimelinesViewModel @Inject constructor(
-    dataSource: FilterTimelinesDataSource
+    private val dataSource: FilterTimelinesDataSource
 ) : ViewModel() {
 
     val circleInfoLiveData = dataSource.circleSummaryLiveData
+    val timelinesLiveData = dataSource.timelinesLiveData
+    val updateFilterResultLiveData = SingleEventLiveData<Response<Unit?>>()
 
+    fun saveFilter() {
+        launchBg {
+            val result = dataSource.applyFilter()
+            updateFilterResultLiveData.postValue(result)
+        }
+    }
+
+    fun toggleItemSelected(id: String) {
+        dataSource.toggleItemSelected(id)
+    }
 
 }
