7333cb83771c5afc5014476aac7b035b21ba9436
Taras Smakula <tarassmakula@gmail.com>
2024-03-18 14:37:28 +0200

    Change create post to BottomSheetDialogFragment

--- app/src/main/java/org/futo/circles/feature/timeline/post/create/CreatePostDialogFragment.kt
+++ app/src/main/java/org/futo/circles/feature/timeline/post/create/CreatePostDialogFragment.kt
@@ -1,17 +1,24 @@
 package org.futo.circles.feature.timeline.post.create
 
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.futo.circles.R
 import org.futo.circles.core.base.NetworkObserver
-import org.futo.circles.core.base.fragment.BaseFullscreenDialogFragment
 import org.futo.circles.core.extensions.navigateSafe
 import org.futo.circles.core.extensions.observeData
 import org.futo.circles.core.extensions.showError
@@ -23,17 +30,13 @@ import org.futo.circles.core.model.TextContent
 import org.futo.circles.databinding.DialogFragmentCreatePostBinding
 import org.futo.circles.feature.timeline.post.emoji.EmojiPickerListener
 import org.futo.circles.model.CreatePostContent
-import java.util.*
 
 @AndroidEntryPoint
-class CreatePostDialogFragment :
-    BaseFullscreenDialogFragment(DialogFragmentCreatePostBinding::inflate),
-    PreviewPostListener, EmojiPickerListener {
+class CreatePostDialogFragment : BottomSheetDialogFragment(), PreviewPostListener,
+    EmojiPickerListener {
 
+    private var binding: DialogFragmentCreatePostBinding? = null
     private val args: CreatePostDialogFragmentArgs by navArgs()
-    private val binding by lazy {
-        getBinding() as DialogFragmentCreatePostBinding
-    }
     private val viewModel by viewModels<CreatePostViewModel>()
 
     private val mediaPickerHelper = MediaPickerHelper(this, isVideoAvailable = true)
@@ -43,6 +46,40 @@ class CreatePostDialogFragment :
         super.onAttach(context)
         createPostListener =
             parentFragmentManager.fragments.lastOrNull { it is CreatePostListener } as? CreatePostListener
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogFragmentCreatePostBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener { setupBottomSheet(it) }
+        return dialog
+    }
+
+    private fun setupBottomSheet(dialogInterface: DialogInterface) {
+        val bottomSheetDialog = dialogInterface as BottomSheetDialog
+        val bottomSheet = bottomSheetDialog.findViewById<View>(
+            com.google.android.material.R.id.design_bottom_sheet
+        ) ?: return
+        bottomSheet.setBackgroundColor(Color.TRANSPARENT)
+        bottomSheet.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+        BottomSheetBehavior.from(bottomSheet).apply {
+            peekHeight = resources.displayMetrics.heightPixels
+            state = BottomSheetBehavior.STATE_EXPANDED
+            addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    if (newState == BottomSheetBehavior.STATE_HALF_EXPANDED) dismiss()
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+
+            })
+        }
     }
 
     @Suppress("DEPRECATION")
@@ -55,31 +92,30 @@ class CreatePostDialogFragment :
 
     private fun setupViews() {
         setToolbarTitle()
-        with(binding) {
-            vPostPreview.setup(this@CreatePostDialogFragment, args.roomId, args.isEdit)
-        }
+        binding?.btnClose?.setOnClickListener { dismiss() }
+        binding?.vPostPreview?.setup(this@CreatePostDialogFragment, args.roomId, args.isEdit)
     }
 
     private fun setupObservers() {
         viewModel.postToEditContentLiveData.observeData(this) {
             when (it.type) {
                 PostContentType.IMAGE_CONTENT, PostContentType.VIDEO_CONTENT ->
-                    binding.vPostPreview.setMediaFromExistingPost(it as MediaContent)
+                    binding?.vPostPreview?.setMediaFromExistingPost(it as MediaContent)
 
-                else -> binding.vPostPreview.setText((it as TextContent).message)
+                else -> binding?.vPostPreview?.setText((it as TextContent).message)
             }
         }
     }
 
     private fun setToolbarTitle() {
-        binding.toolbar.title = when {
+        binding?.tvTitle?.text = when {
             args.isEdit -> getString(R.string.edit_post)
             else -> getString(R.string.create_post)
         }
     }
 
     private fun onMediaSelected(uri: Uri, type: MediaType) {
-        binding.vPostPreview.setMedia(uri, type)
+        binding?.vPostPreview?.setMedia(uri, type)
     }
 
     private fun sendPost(content: CreatePostContent) {
@@ -109,7 +145,7 @@ class CreatePostDialogFragment :
 
     override fun onAddLinkClicked() {
         AddLinkDialog(requireContext()) { title, link ->
-            binding.vPostPreview.insertLink(title, link)
+            binding?.vPostPreview?.insertLink(title, link)
         }.show()
     }
 
@@ -123,6 +159,11 @@ class CreatePostDialogFragment :
     }
 
     override fun onEmojiSelected(roomId: String?, eventId: String?, emoji: String) {
-        binding.vPostPreview.insertEmoji(emoji)
+        binding?.vPostPreview?.insertEmoji(emoji)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
