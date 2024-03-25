c018df6e324f445e0b5390794df675fadd7921ba
Taras Smakula <tarassmakula@gmail.com>
2024-03-25 16:28:20 +0200

    For poll

---
+++ app/src/main/java/org/futo/circles/feature/timeline/list/holder/PollPostViewHolder.kt
@@ -0,0 +1,34 @@
+package org.futo.circles.feature.timeline.list.holder
+
+import android.view.ViewGroup
+import org.futo.circles.core.base.list.ViewBindingHolder
+import org.futo.circles.core.model.PollContent
+import org.futo.circles.core.model.Post
+import org.futo.circles.databinding.ViewPollPostBinding
+import org.futo.circles.view.PostLayout
+import org.futo.circles.view.PostOptionsListener
+
+class PollPostViewHolder(
+    parent: ViewGroup,
+    private val postOptionsListener: PostOptionsListener,
+    isThread: Boolean
+) : PostViewHolder(inflate(parent, ViewPollPostBinding::inflate), isThread) {
+
+    private companion object : ViewBindingHolder
+
+    private val binding = baseBinding as ViewPollPostBinding
+    override val postLayout: PostLayout = binding.lPollPost
+
+    init {
+        binding.lPollPost.setListener(postOptionsListener)
+    }
+
+    override fun bind(post: Post) {
+        super.bind(post)
+        (post.content as? PollContent)?.let {
+            binding.pollContentView.setup(it) { optionId ->
+                postOptionsListener.onPollOptionSelected(post.postInfo.roomId, post.id, optionId)
+            }
+        }
+    }
+}
