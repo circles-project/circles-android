e5b7c01fca241b06feca27c1acf0e0f2edc4f49b
Taras Smakula <tarassmakula@gmail.com>
2024-03-26 13:33:33 +0200

    Remove post layout for poll post

--- app/src/main/java/org/futo/circles/feature/timeline/list/holder/PollPostViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/timeline/list/holder/PollPostViewHolder.kt
@@ -1,7 +1,9 @@
 package org.futo.circles.feature.timeline.list.holder
 
 import android.view.ViewGroup
+import org.futo.circles.R
 import org.futo.circles.core.base.list.ViewBindingHolder
+import org.futo.circles.core.feature.markdown.MarkdownParser
 import org.futo.circles.core.model.PollContent
 import org.futo.circles.core.model.Post
 import org.futo.circles.databinding.ViewPollPostBinding
@@ -25,10 +27,16 @@ class PollPostViewHolder(
 
     override fun bind(post: Post) {
         super.bind(post)
-        (post.content as? PollContent)?.let {
-            binding.pollContentView.setup(it) { optionId ->
-                postOptionsListener.onPollOptionSelected(post.postInfo.roomId, post.id, optionId)
-            }
+        val content = (post.content as? PollContent) ?: return
+        binding.pollContentView.setup(content) { optionId ->
+            postOptionsListener.onPollOptionSelected(post.postInfo.roomId, post.id, optionId)
         }
+        setMentionBorder(content)
+    }
+
+    private fun setMentionBorder(content: PollContent) {
+        val hasMention = MarkdownParser.hasCurrentUserMention(content.question)
+        if (hasMention) binding.lCard.setBackgroundResource(R.drawable.bg_mention_highlight)
+        else binding.lCard.background = null
     }
 }
