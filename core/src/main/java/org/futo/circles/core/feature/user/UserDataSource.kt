5bc2122c530901cb5b031b464f70e3cf76558817
Taras Smakula <tarassmakula@gmail.com>
2024-04-02 16:37:13 +0300

    Add mutual users section

--- core/src/main/java/org/futo/circles/core/feature/user/UserDataSource.kt
+++ core/src/main/java/org/futo/circles/core/feature/user/UserDataSource.kt
@@ -12,6 +12,8 @@ import kotlinx.coroutines.flow.map
 import org.futo.circles.core.extensions.getOrThrow
 import org.futo.circles.core.extensions.getRoomOwner
 import org.futo.circles.core.feature.workspace.SharedCircleDataSource
+import org.futo.circles.core.mapping.toCircleUserSummary
+import org.futo.circles.core.model.MutualFriendListItem
 import org.futo.circles.core.model.TimelineHeaderItem
 import org.futo.circles.core.model.TimelineListItem
 import org.futo.circles.core.model.TimelineRoomListItem
@@ -19,9 +21,13 @@ import org.futo.circles.core.model.toTimelineRoomListItem
 import org.futo.circles.core.provider.MatrixSessionProvider
 import org.futo.circles.core.utils.getTimelinesLiveData
 import org.futo.circles.core.utils.spaceType
+import org.matrix.android.sdk.api.session.getRoom
 import org.matrix.android.sdk.api.session.getUserOrDefault
+import org.matrix.android.sdk.api.session.room.members.roomMemberQueryParams
 import org.matrix.android.sdk.api.session.room.model.Membership
+import org.matrix.android.sdk.api.session.room.model.RoomMemberSummary
 import org.matrix.android.sdk.api.session.room.model.RoomSummary
+import org.matrix.android.sdk.api.session.user.model.User
 import javax.inject.Inject
 
 @ViewModelScoped
@@ -39,35 +45,43 @@ class UserDataSource @Inject constructor(
     }
 
     suspend fun getTimelinesFlow() = combine(
-        getAllFollowingTimelinesFlow(),
-        getAllSharedTimelinesFlow()
-    ) { followingTimelines, sharedTimelines ->
-        buildList(followingTimelines, sharedTimelines)
+        getAllTimelinesFlow(),
+        getAllSharedTimelinesFlow(),
+        session.userService().getIgnoredUsersLive().asFlow()
+    ) { allTimelines, sharedTimelines, ignoredUsers ->
+        buildList(allTimelines, sharedTimelines, ignoredUsers)
     }.flowOn(Dispatchers.IO).distinctUntilChanged()
 
     private fun buildList(
-        followingTimelines: List<TimelineRoomListItem>,
-        sharedTimelines: List<TimelineRoomListItem>
+        allTimelines: List<TimelineRoomListItem>,
+        sharedTimelines: List<TimelineRoomListItem>,
+        ignoredUsers: List<User>
     ): List<TimelineListItem> = mutableListOf<TimelineListItem>().apply {
-        val allItems = mutableListOf<TimelineRoomListItem>().apply {
-            addAll(followingTimelines)
+
+        val timelinesOwnedByUser = mutableListOf<TimelineRoomListItem>().apply {
+            addAll(allTimelines.filter { isUsersCircleTimeline(it.id) })
             addAll(sharedTimelines)
         }.distinctBy { it.id }
 
-        val following = allItems.filter { it.isJoined }
+        val following = timelinesOwnedByUser.filter { it.isJoined }
         if (following.isNotEmpty()) {
             add(TimelineHeaderItem.followingHeader)
             addAll(following)
         }
-        val others = allItems - following.toSet()
+        val others = timelinesOwnedByUser - following.toSet()
         if (others.isNotEmpty()) {
             add(TimelineHeaderItem.othersHeader)
             addAll(others)
         }
+        val mutualFriends = getMutualFriends(allTimelines, ignoredUsers)
+        if (mutualFriends.isNotEmpty()) {
+            add(TimelineHeaderItem.mutualFriends)
+            addAll(mutualFriends)
+        }
     }
 
-    private fun getAllFollowingTimelinesFlow() = getTimelinesLiveData(listOf(Membership.JOIN))
-        .map { list -> filterUsersTimelines(list) }.asFlow()
+    private fun getAllTimelinesFlow() = getTimelinesLiveData(listOf(Membership.JOIN))
+        .map { list -> list.map { it.toTimelineRoomListItem() } }.asFlow()
 
     private suspend fun getAllSharedTimelinesFlow() = session.roomService().getRoomSummaryLive(
         sharedCircleDataSource.getSharedCircleFor(userId)?.roomId ?: ""
@@ -81,14 +95,26 @@ class UserDataSource @Inject constructor(
             else it.toTimelineRoomListItem()
         }
 
-    private fun filterUsersTimelines(list: List<RoomSummary>): List<TimelineRoomListItem> {
-        return list.mapNotNull { summary ->
-            if (isUsersCircleTimeline(summary)) summary.toTimelineRoomListItem()
+
+    private fun getMutualFriends(
+        following: List<TimelineRoomListItem>,
+        ignoredUsers: List<User>
+    ): List<MutualFriendListItem> {
+        val ignoredUsersIds = ignoredUsers.map { it.userId }
+        val mutualFriendsSet = mutableSetOf<RoomMemberSummary>()
+        following.forEach {
+            val members = session.getRoom(it.id)?.membershipService()?.getRoomMembers(
+                roomMemberQueryParams { memberships = listOf(Membership.JOIN) }
+            ) ?: emptyList()
+            mutualFriendsSet.addAll(members)
+        }
+        return mutualFriendsSet.mapNotNull {
+            if (it.userId != userId && it.userId != session.myUserId)
+                MutualFriendListItem(it.toCircleUserSummary(), ignoredUsersIds.contains(it.userId))
             else null
         }
     }
 
-    private fun isUsersCircleTimeline(summary: RoomSummary) =
-        getRoomOwner(summary.roomId)?.userId == userId
+    private fun isUsersCircleTimeline(roomId: String) = getRoomOwner(roomId)?.userId == userId
 
 }
