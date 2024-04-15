66d7653086f1f392ea0ccc3b28338e0976ac4c40
Taras Smakula <tarassmakula@gmail.com>
2024-04-15 18:06:52 +0300

    Update publishing script

--- modules_release_clean.sh
+++ modules_release_clean.sh
@@ -2,10 +2,12 @@
 # Remove .aar and pom.xml from the root directory if they exist
 rm -f auth-gplay-release.aar
 rm -f auth-fdroid-release.aar
-rm -f core-release.aar
+rm -f core-gplay-release.aar
+rm -f core-fdroid-release.aar
 rm -f gallery-release.aar
 
 rm -f pom_auth_gplay.xml
 rm -f pom_auth_fdroid.xml
-rm -f pom_core.xml
+rm -f pom_core_gplay.xml
+rm -f pom_core_fdroid.xml
 rm -f pom_gallery.xml
