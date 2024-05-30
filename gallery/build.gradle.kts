e3da7802c4e43075f78816a16de3cfac51d10ad6
Taras Smakula <tarassmakula@gmail.com>
2024-05-30 13:42:52 +0300

    Gallery to kts

---
+++ gallery/build.gradle.kts
@@ -0,0 +1,122 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("maven-publish")
+}
+
+android {
+    namespace = "org.futo.circles.gallery"
+    compileSdk = rootProject.ext["sdk_version"] as Int
+
+    defaultConfig {
+        minSdk = rootProject.ext["min_sdk_version"] as Int
+        targetSdk = rootProject.ext["sdk_version"] as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+
+    val flavorDimensionName = "store"
+    flavorDimensions.add(flavorDimensionName)
+    productFlavors {
+        create("gplay") {
+            dimension = flavorDimensionName
+        }
+        create("fdroid") {
+            dimension = flavorDimensionName
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+        freeCompilerArgs += listOf(
+                "-opt-in=kotlinx.coroutines.FlowPreview",
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+        )
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    // Image zoom
+    implementation("com.jsibbold:zoomage:1.3.1")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:${rootProject.ext["hilt_version"]}")
+    kapt("com.google.dagger:hilt-compiler:${rootProject.ext["hilt_version"]}")
+    implementation("androidx.hilt:hilt-work:1.2.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+afterEvaluate {
+    android.libraryVariants.forEach { variant ->
+        if (variant.buildType.name != "release") {
+            return@forEach
+        }
+        publishing.publications.create(variant.name, MavenPublication::class) {
+            groupId = rootProject.ext["modules_groupId"] as String
+            artifactId = "gallery_${variant.flavorName}"
+            version = rootProject.ext["modules_version"] as String
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                fun addDependency(dep: Dependency, scope: String) {
+                    if (dep.group == null || dep.name == "unspecified" || dep.version == "unspecified") return
+                    val dependencyNode = dependenciesNode.appendNode("dependency")
+                    dependencyNode.appendNode("groupId", dep.group)
+                    dependencyNode.appendNode("artifactId", dep.name)
+                    dependencyNode.appendNode("version", dep.version)
+                    dependencyNode.appendNode("scope", scope)
+                }
+                configurations.getByName("api").dependencies.forEach { dep -> addDependency(dep, "compile") }
+                configurations.getByName("implementation").dependencies.forEach { dep ->
+                    addDependency(dep, "runtime")
+                }
+                if (variant.flavorName == "gplay") {
+                    configurations.getByName("gplayApi").dependencies.forEach { dep ->
+                        addDependency(dep, "compile")
+                    }
+                    configurations.getByName("gplayImplementation").dependencies.forEach { dep ->
+                        addDependency(dep, "runtime")
+                    }
+                } else if (variant.flavorName == "fdroid") {
+                    configurations.getByName("fdroidApi").dependencies.forEach { dep ->
+                        addDependency(dep, "compile")
+                    }
+                    configurations.getByName("fdroidImplementation").dependencies.forEach { dep ->
+                        addDependency(dep, "runtime")
+                    }
+                }
+            }
+        }
+    }
+}
