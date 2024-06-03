89e26fe2a9a75aa50a642a72b2bbbc0d832e10a3
Taras Smakula <tarassmakula@gmail.com>
2024-06-03 15:51:05 +0300

    Only joined members for circle followers count

--- app/src/main/java/org/futo/circles/mapping/RoomSummaryMapping.kt
+++ app/src/main/java/org/futo/circles/mapping/RoomSummaryMapping.kt
@@ -46,8 +46,12 @@ private fun RoomSummary.getFollowingCount() = spaceChildren?.filter {
             getRoomOwner(it.childRoomId)?.userId != MatrixSessionProvider.currentSession?.myUserId
 }?.size ?: 0
 
-private fun RoomSummary.getFollowersCount(): Int =
-    getTimelineRoomFor(roomId)?.roomSummary()?.otherMemberIds?.size ?: 0
+//Joined members minus room owner
+private fun RoomSummary.getFollowersCount(): Int {
+    val joinedToTimelineCount = getTimelineRoomFor(roomId)?.roomSummary()?.joinedMembersCount ?: 0
+    val followersCount = joinedToTimelineCount.takeIf { it > 0 }?.let { it - 1 } ?: 0
+    return followersCount
+}
 
 
 private fun RoomSummary.getCircleUnreadMessagesCount(): Int {
