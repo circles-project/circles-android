b1fdb506d313af6ece72e386d2907a26ff0ab9b7
Taras Smakula <tarassmakula@gmail.com>
2024-06-06 16:32:54 +0300

    view holder

---
+++ auth/src/main/java/org/futo/circles/auth/feature/setup/circles/SetupCirclesViewModel.kt
@@ -0,0 +1,12 @@
+package org.futo.circles.auth.feature.setup.circles
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SetupCirclesViewModel @Inject constructor(
+) : ViewModel() {
+
+
+}
