c0dcfa55300f46af754adbce9957353b43385b31
Taras Smakula <tarassmakula@gmail.com>
2024-06-17 13:35:35 +0300

    Update dependencies

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     "gplayImplementation"("com.android.billingclient:billing-ktx:7.0.0")
 
     // PasswordManager
-    val credentialsVersion = "1.3.0-beta01"
+    val credentialsVersion = "1.3.0-beta02"
     "gplayImplementation"("androidx.credentials:credentials:$credentialsVersion")
     "gplayImplementation"("androidx.credentials:credentials-play-services-auth:$credentialsVersion")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.1" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 
