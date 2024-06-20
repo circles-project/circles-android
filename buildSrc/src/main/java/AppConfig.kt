ccb47404c84be7cf4b76e6569cc313128862d908
Taras Smakula <tarassmakula@gmail.com>
2024-06-20 15:25:35 +0300

    App config gradle constants

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,14 @@
+object AppConfig {
+    const val compileSdk = 34
+    const val minSdk = 24
+    const val versionCode = 41
+    const val versionName = "1.0.31"
+
+    //output file name
+    val archivesName = "circles-v$versionName"
+
+    //build flavours
+    const val flavourDimension = "store"
+    const val gplayFlavourName = "gplay"
+    const val fdroidFlavourName = "fdroid"
+}
