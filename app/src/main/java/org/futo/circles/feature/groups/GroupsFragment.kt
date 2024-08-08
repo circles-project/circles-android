795b7092d2d6e00e5a2593e7ecaa79633c36bc90
Taras Smakula <tarassmakula@gmail.com>
2024-08-08 18:35:00 +0300

    Groups toolbar ui

--- app/src/main/java/org/futo/circles/feature/groups/GroupsFragment.kt
+++ app/src/main/java/org/futo/circles/feature/groups/GroupsFragment.kt
@@ -1,29 +1,22 @@
 package org.futo.circles.feature.groups
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.view.menu.MenuBuilder
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
 import org.futo.circles.R
 import org.futo.circles.core.base.fragment.BaseBindingFragment
-import org.futo.circles.core.databinding.FragmentRoomsBinding
 import org.futo.circles.core.extensions.navigateSafe
 import org.futo.circles.core.extensions.observeData
 import org.futo.circles.core.view.EmptyTabPlaceholderView
+import org.futo.circles.databinding.FragmentGroupsBinding
 import org.futo.circles.feature.groups.list.GroupsListAdapter
 import org.futo.circles.model.GroupListItem
 
 @AndroidEntryPoint
-class GroupsFragment : BaseBindingFragment<FragmentRoomsBinding>(FragmentRoomsBinding::inflate),
-    MenuProvider {
+class GroupsFragment : BaseBindingFragment<FragmentGroupsBinding>(FragmentGroupsBinding::inflate) {
 
     private val viewModel by viewModels<GroupsViewModel>()
     private val listAdapter by lazy {
@@ -39,23 +32,8 @@ class GroupsFragment : BaseBindingFragment<FragmentRoomsBinding>(FragmentRoomsBi
         super.onViewCreated(view, savedInstanceState)
         setupViews()
         setupObservers()
-        activity?.addMenuProvider(this, viewLifecycleOwner)
     }
 
-
-    @SuppressLint("RestrictedApi")
-    override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
-        (menu as? MenuBuilder)?.setOptionalIconsVisible(true)
-        menu.clear()
-        inflater.inflate(R.menu.circles_tab_menu, menu)
-    }
-
-    override fun onMenuItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.help -> findNavController().navigateSafe(GroupsFragmentDirections.toCirclesExplanationDialogFragment())
-        }
-        return true
-    }
 
     private fun setupViews() {
         binding.rvRooms.apply {
@@ -65,9 +43,8 @@ class GroupsFragment : BaseBindingFragment<FragmentRoomsBinding>(FragmentRoomsBi
             })
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
             adapter = listAdapter
-            bindToFab(binding.fbAddRoom)
         }
-        binding.fbAddRoom.setOnClickListener { navigateToCreateRoom() }
+        binding.ivCreateGroup.setOnClickListener { navigateToCreateRoom() }
     }
 
     private fun setupObservers() {
