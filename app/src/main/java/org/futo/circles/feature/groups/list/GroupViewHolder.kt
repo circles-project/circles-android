826974b28c63222ade0bf4ac87f442938deb69e9
Taras Smakula <tarassmakula@gmail.com>
2024-08-09 14:52:48 +0300

    Update view holder binding

--- app/src/main/java/org/futo/circles/feature/groups/list/GroupViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/groups/list/GroupViewHolder.kt
@@ -1,9 +1,7 @@
 package org.futo.circles.feature.groups.list
 
-import android.text.format.DateUtils
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.futo.circles.R
@@ -12,8 +10,6 @@ import org.futo.circles.core.base.list.context
 import org.futo.circles.core.databinding.ListItemInviteNotificationBinding
 import org.futo.circles.core.extensions.loadRoomProfileIcon
 import org.futo.circles.core.extensions.onClick
-import org.futo.circles.core.extensions.setIsEncryptedIcon
-import org.futo.circles.core.extensions.setIsVisible
 import org.futo.circles.core.utils.TextFormatUtils
 import org.futo.circles.databinding.ListItemJoinedGroupBinding
 import org.futo.circles.model.GroupInvitesNotificationListItem
@@ -43,58 +39,24 @@ class JoinedGroupViewHolder(
         if (data !is JoinedGroupListItem) return
 
         binding.ivGroup.loadRoomProfileIcon(data.info.avatarUrl, data.info.title)
-        setIsEncrypted(binding.ivLock, data.isEncrypted)
         setTitle(binding.tvGroupTitle, data.info.title)
-        setTopic(data.topic)
-        setMembersCount(data.membersCount, data.knockRequestsCount)
-        setUpdateTime(data.timestamp)
+        setMembersCount(data.membersCount)
         setUnreadCount(data.unreadCount)
     }
 
     fun bindPayload(data: GroupListItemPayload) {
-        data.isEncrypted?.let { setIsEncrypted(binding.ivLock, it) }
-        data.topic?.let { setTopic(it) }
-        data.membersCount?.let { setMembersCount(it, data.knocksCount ?: 0) }
-        data.knocksCount?.let { setMembersCount(data.membersCount ?: 0, it) }
-        data.timestamp?.let { setUpdateTime(it) }
+        data.membersCount?.let { setMembersCount(it) }
         data.unreadCount?.let { setUnreadCount(it) }
-    }
-
-    private fun setIsEncrypted(lockIcon: ImageView, isEncrypted: Boolean) {
-        lockIcon.setIsEncryptedIcon(isEncrypted)
     }
 
     private fun setTitle(titleView: TextView, title: String) {
         titleView.text = title
     }
 
-    private fun setTopic(topic: String) {
-        binding.tvTopic.setIsVisible(topic.isNotEmpty())
-        binding.tvTopic.text = context.getString(org.futo.circles.core.R.string.topic_format, topic)
-    }
-
-    private fun setMembersCount(membersCount: Int, knocksCount: Int) {
-        val members = context.resources.getQuantityString(
+    private fun setMembersCount(membersCount: Int) {
+        binding.tvMembers.text = context.resources.getQuantityString(
             R.plurals.member_plurals,
             membersCount, membersCount
-        )
-        val knocks = if (knocksCount > 0) " / ${
-            context.resources.getQuantityString(
-                R.plurals.request_plurals,
-                knocksCount, knocksCount
-            )
-        }" else ""
-
-        val membersInfo = members + knocks
-        binding.tvMembers.text = membersInfo
-    }
-
-    private fun setUpdateTime(timestamp: Long) {
-        binding.tvUpdateTime.text = context.getString(
-            org.futo.circles.core.R.string.last_updated_formatter,
-            DateUtils.getRelativeTimeSpanString(
-                timestamp, System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS
-            )
         )
     }
 
