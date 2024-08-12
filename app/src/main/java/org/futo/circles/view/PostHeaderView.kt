f4d7e08f1cf43760e9dd318499582274eff58189
Taras Smakula <tarassmakula@gmail.com>
2024-08-12 19:00:14 +0300

    Set color for circle room indicator on post header

--- app/src/main/java/org/futo/circles/view/PostHeaderView.kt
+++ app/src/main/java/org/futo/circles/view/PostHeaderView.kt
@@ -1,14 +1,17 @@
 package org.futo.circles.view
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.ColorUtils
 import org.futo.circles.R
 import org.futo.circles.core.extensions.getAttributes
 import org.futo.circles.core.extensions.loadUserProfileIcon
 import org.futo.circles.core.extensions.notEmptyDisplayName
 import org.futo.circles.core.extensions.setIsVisible
+import org.futo.circles.core.feature.textDrawable.ColorGenerator
 import org.futo.circles.core.model.Post
 import org.futo.circles.core.provider.MatrixSessionProvider
 import org.futo.circles.core.utils.TimeAgoFormatUtils
@@ -47,6 +50,7 @@ class PostHeaderView(
             sender.notEmptyDisplayName(),
             sender.avatarUrl,
             data.postInfo.getLastModifiedTimestamp(),
+            data.postInfo.roomId,
             data.timelineName,
             data.timelineOwnerName
         )
@@ -57,13 +61,14 @@ class PostHeaderView(
         name: String,
         avatarUrl: String?,
         timestamp: Long,
+        roomId: String? = null,
         roomName: String? = null,
         timelineOwnerName: String? = null
     ) {
         setUserIcon(userId, avatarUrl)
         binding.tvUserName.text = name
         binding.tvTime.text = TimeAgoFormatUtils.getTimeAgoString(context, timestamp)
-        setCircleRoomIndicator(roomName, timelineOwnerName)
+        setCircleRoomIndicator(roomId, roomName, timelineOwnerName)
     }
 
     private fun parseAttributes(attrs: AttributeSet?) {
@@ -87,12 +92,23 @@ class PostHeaderView(
         }
     }
 
-    private fun setCircleRoomIndicator(roomName: String?, timelineOwnerName: String?) {
+    private fun setCircleRoomIndicator(
+        roomId: String?,
+        roomName: String?,
+        timelineOwnerName: String?
+    ) {
         binding.chCircleIndicator.setIsVisible(roomName != null)
-        roomName?.let {
-            val nameFormat =
-                context.getString(R.string.in_timeline_format, timelineOwnerName ?: "", it)
-            binding.chCircleIndicator.text = nameFormat
+        roomName ?: return
+        val nameFormat =
+            context.getString(R.string.in_timeline_format, timelineOwnerName ?: "", roomName)
+        with(binding.chCircleIndicator) {
+            text = nameFormat
+            val textColor = ColorGenerator().getColor(roomId ?: roomName)
+            setTextColor(textColor)
+            chipStrokeColor =
+                ColorStateList.valueOf(ColorUtils.setAlphaComponent(textColor, (255 * 0.3).toInt()))
+            chipBackgroundColor =
+                ColorStateList.valueOf(ColorUtils.setAlphaComponent(textColor, (255 * 0.1).toInt()))
         }
     }
 }
