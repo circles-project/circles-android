7723c5672fb3031ce8d090ab7e1fae6db4f96680
Taras Smakula <tarassmakula@gmail.com>
2024-08-16 14:31:23 +0300

    Create my profile fragment

---
+++ settings/src/main/java/org/futo/circles/settings/feature/profile/tab/MyProfileFragment.kt
@@ -0,0 +1,35 @@
+package org.futo.circles.settings.feature.profile.tab
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import org.futo.circles.core.base.fragment.BaseBindingFragment
+import org.futo.circles.core.extensions.navigateSafe
+import org.futo.circles.settings.databinding.FragmentMyProfileBinding
+
+@AndroidEntryPoint
+class MyProfileFragment :
+    BaseBindingFragment<FragmentMyProfileBinding>(FragmentMyProfileBinding::inflate) {
+
+    private val viewModel by viewModels<MyProfileViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupObservers()
+    }
+
+    private fun setupViews() {
+        with(binding) {
+            btnSettings.setOnClickListener {
+                findNavController().navigateSafe(MyProfileFragmentDirections.toSettingsDialogFragment())
+            }
+        }
+    }
+
+    private fun setupObservers() {
+
+    }
+}
---
+++ settings/src/main/java/org/futo/circles/settings/feature/profile/tab/MyProfileViewModel.kt
@@ -0,0 +1,11 @@
+package org.futo.circles.settings.feature.profile.tab
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MyProfileViewModel @Inject constructor() : ViewModel() {
+
+}
