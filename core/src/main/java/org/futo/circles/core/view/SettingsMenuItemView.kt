76a9b2b7fe1dab2104c1c410e10f4e407b12d85d
Taras Smakula <tarassmakula@gmail.com>
2024-08-19 13:57:34 +0300

    Update settings menu item

--- core/src/main/java/org/futo/circles/core/view/SettingsMenuItemView.kt
+++ core/src/main/java/org/futo/circles/core/view/SettingsMenuItemView.kt
@@ -2,8 +2,9 @@ package org.futo.circles.core.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
-import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.futo.circles.core.R
 import org.futo.circles.core.databinding.ViewSettingsMenuItemBinding
 import org.futo.circles.core.extensions.getAttributes
@@ -13,33 +14,30 @@ import org.futo.circles.core.extensions.setIsVisible
 class SettingsMenuItemView(
     context: Context,
     attrs: AttributeSet? = null,
-) : LinearLayout(context, attrs) {
-
-    init {
-        orientation = VERTICAL
-    }
+) : ConstraintLayout(context, attrs) {
 
     private val binding =
         ViewSettingsMenuItemBinding.inflate(LayoutInflater.from(context), this)
 
 
     init {
+        addBackgroundRipple()
         getAttributes(attrs, R.styleable.SettingsMenuItemView) {
             val isDividerVisible = getBoolean(R.styleable.SettingsMenuItemView_hasDivider, true)
             binding.vBottomDivider.setIsVisible(isDividerVisible)
-
-            binding.tvOptionName.apply {
-                text = getString(R.styleable.SettingsMenuItemView_optionName)
-                    ?.replace(' ', Typography.nbsp)
-                setCompoundDrawablesWithIntrinsicBounds(
-                    getDrawable(R.styleable.SettingsMenuItemView_optionIcon), null, null, null
-                )
-            }
-
+            binding.tvOptionName.text = getString(R.styleable.SettingsMenuItemView_optionName)
+                ?.replace(' ', Typography.nbsp)
+            binding.ivOptionIcon.setImageDrawable(getDrawable(R.styleable.SettingsMenuItemView_optionIcon))
         }
     }
 
     fun setText(text: String) {
         binding.tvOptionName.text = text
+    }
+
+    private fun addBackgroundRipple() {
+        val outValue = TypedValue()
+        context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+        setBackgroundResource(outValue.resourceId)
     }
 }
