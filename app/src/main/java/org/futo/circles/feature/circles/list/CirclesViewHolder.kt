3030fd0c279b2540ef440b41fdddcbb37d578538
Taras Smakula <tarassmakula@gmail.com>
2024-08-20 17:30:09 +0300

    Add owner indicator on circles item

--- app/src/main/java/org/futo/circles/feature/circles/list/CirclesViewHolder.kt
+++ app/src/main/java/org/futo/circles/feature/circles/list/CirclesViewHolder.kt
@@ -1,17 +1,23 @@
 package org.futo.circles.feature.circles.list
 
+import android.content.res.ColorStateList
 import android.text.format.DateUtils
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.graphics.ColorUtils
 import androidx.recyclerview.widget.RecyclerView
 import org.futo.circles.R
 import org.futo.circles.core.base.list.ViewBindingHolder
 import org.futo.circles.core.base.list.context
 import org.futo.circles.core.databinding.ListItemCircleAllPostsBinding
 import org.futo.circles.core.databinding.ListItemInviteNotificationBinding
+import org.futo.circles.core.extensions.gone
 import org.futo.circles.core.extensions.loadRoomProfileIcon
 import org.futo.circles.core.extensions.onClick
+import org.futo.circles.core.extensions.setIsVisible
+import org.futo.circles.core.feature.textDrawable.ColorGenerator
+import org.futo.circles.core.model.CirclesUserSummary
+import org.futo.circles.core.provider.MatrixSessionProvider
 import org.futo.circles.core.utils.TextFormatUtils
 import org.futo.circles.databinding.ListItemCirclesHeaderBinding
 import org.futo.circles.databinding.ListItemJoinedCircleBinding
@@ -45,7 +51,8 @@ class JoinedCircleViewHolder(
 
         with(binding) {
             ivCircle.loadRoomProfileIcon(data.info.avatarUrl, data.info.title)
-            setTitle(tvCircleTitle, data.info.title, data.owner?.name)
+            binding.tvCircleTitle.text = data.info.title
+            setOwnerIndicator(data.owner)
             setFollowersCount(data.followersCount, data.knockRequestsCount)
             setUpdateTime(data.timestamp)
             setUnreadCount(data.unreadCount)
@@ -59,11 +66,29 @@ class JoinedCircleViewHolder(
         data.timestamp?.let { setUpdateTime(it) }
     }
 
-    private fun setTitle(titleView: TextView, roomName: String, ownerName: String?) {
-        val title = ownerName?.let {
-            "$ownerName - $roomName"
-        } ?: roomName
-        titleView.text = title
+
+    private fun setOwnerIndicator(owner: CirclesUserSummary?) {
+        owner ?: run { binding.chOwnerIndicator.gone(); return }
+        with(binding.chOwnerIndicator) {
+            setIsVisible(owner.id != MatrixSessionProvider.currentSession?.myUserId)
+            text = owner.name
+            val textColor = ColorGenerator().getColor(owner.id)
+            setTextColor(textColor)
+            chipStrokeColor =
+                ColorStateList.valueOf(
+                    ColorUtils.setAlphaComponent(
+                        textColor,
+                        (255 * 0.3).toInt()
+                    )
+                )
+            chipBackgroundColor =
+                ColorStateList.valueOf(
+                    ColorUtils.setAlphaComponent(
+                        textColor,
+                        (255 * 0.1).toInt()
+                    )
+                )
+        }
     }
 
     private fun setFollowersCount(followersCount: Int, knocksCount: Int) {
