ff281b4e273ba147820b924fb4e499a9cf1fba9b
Taras Smakula <tarassmakula@gmail.com>
2024-09-02 17:09:24 +0300

    New items design for select galleries

--- gallery/src/main/java/org/futo/circles/gallery/feature/select/SelectGalleriesFragment.kt
+++ gallery/src/main/java/org/futo/circles/gallery/feature/select/SelectGalleriesFragment.kt
@@ -1,17 +1,22 @@
 package org.futo.circles.gallery.feature.select
 
 import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import org.futo.circles.core.base.fragment.BaseBindingFragment
+import org.futo.circles.core.base.list.GridOffsetDecoration
+import org.futo.circles.core.extensions.dpToPx
 import org.futo.circles.core.extensions.observeData
 import org.futo.circles.core.feature.room.select.interfaces.RoomsPicker
 import org.futo.circles.core.feature.room.select.interfaces.SelectRoomsListener
 import org.futo.circles.core.model.SelectableRoomListItem
 import org.futo.circles.gallery.databinding.FragmentSelectGalleriesBinding
 import org.futo.circles.gallery.feature.select.list.SelectGalleryAdapter
+import org.futo.circles.gallery.feature.select.list.SelectGalleryViewHolder
 
 @AndroidEntryPoint
 class SelectGalleriesFragment :
@@ -44,7 +49,11 @@ class SelectGalleriesFragment :
         viewModel.galleriesLiveData.value?.filter { it.isSelected } ?: emptyList()
 
     private fun setupViews() {
-        binding.rvGalleries.adapter = listAdapter
+        binding.rvGalleries.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            addItemDecoration(getGalleriesGridOffsetDecoration())
+            adapter = listAdapter
+        }
     }
 
     private fun setupObservers() {
@@ -56,6 +65,24 @@ class SelectGalleriesFragment :
 
     private fun onGallerySelected(gallery: SelectableRoomListItem) {
         viewModel.toggleGallerySelect(gallery)
+    }
+
+    private fun getGalleriesGridOffsetDecoration() = GridOffsetDecoration { holder, column ->
+        if (holder !is SelectGalleryViewHolder) return@GridOffsetDecoration Rect()
+
+        val offsetOfParent = requireContext().dpToPx(16)
+        val itemsOffset = requireContext().dpToPx(6)
+        val left: Int
+        val right: Int
+
+        if (column == 0) {
+            left = offsetOfParent
+            right = itemsOffset
+        } else {
+            left = itemsOffset
+            right = offsetOfParent
+        }
+        Rect(left, itemsOffset, right, itemsOffset)
     }
 
 }
--- gallery/src/main/java/org/futo/circles/gallery/feature/select/list/SelectGalleryViewHolder.kt
+++ gallery/src/main/java/org/futo/circles/gallery/feature/select/list/SelectGalleryViewHolder.kt
@@ -2,10 +2,11 @@ package org.futo.circles.gallery.feature.select.list
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import org.futo.circles.core.extensions.loadRoomProfileIcon
-import org.futo.circles.core.extensions.onClick
-import org.futo.circles.core.extensions.setIsVisible
 import org.futo.circles.core.base.list.ViewBindingHolder
+import org.futo.circles.core.extensions.loadMatrixImage
+import org.futo.circles.core.extensions.onClick
+import org.futo.circles.core.feature.textDrawable.ColorGenerator
+import org.futo.circles.core.feature.textDrawable.TextDrawable
 import org.futo.circles.core.model.SelectableRoomListItem
 import org.futo.circles.gallery.databinding.ListItemSelectGalleryBinding
 
@@ -24,13 +25,19 @@ class SelectGalleryViewHolder(
 
     fun bind(data: SelectableRoomListItem) {
         with(binding) {
-            baseGalleryItem.ivGalleryImage.loadRoomProfileIcon(data.info.avatarUrl, "")
+            val placeholder = TextDrawable.Builder()
+                .setShape(TextDrawable.SHAPE_ROUND_RECT)
+                .setColor(ColorGenerator().getColor(data.id))
+                .build()
+            baseGalleryItem.ivGalleryImage.loadMatrixImage(
+                url = data.info.avatarUrl,
+                placeholder = placeholder
+            )
             baseGalleryItem.tvGalleryName.text = data.info.title
             ivSelect.setImageResource(
                 if (data.isSelected) org.futo.circles.core.R.drawable.ic_check_circle
                 else org.futo.circles.core.R.drawable.ic_unselected
             )
-            vSelectBackground.setIsVisible(data.isSelected)
         }
     }
 }
