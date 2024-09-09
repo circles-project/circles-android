7e8e9d80eb4e7c3918cfcd529ea5cc956cbd6de4
Taras Smakula <tarassmakula@gmail.com>
2024-09-09 15:52:32 +0300

    Dm message input color

--- app/src/main/java/org/futo/circles/view/SendMessageView.kt
+++ app/src/main/java/org/futo/circles/view/SendMessageView.kt
@@ -35,7 +35,7 @@ class SendMessageView(
         setBackgroundColor(
             ContextCompat.getColor(
                 context,
-                org.futo.circles.core.R.color.post_card_background_color
+                org.futo.circles.core.R.color.grey_cool_200
             )
         )
         with(binding) {
