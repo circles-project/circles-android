cf4ab6251dd05faad2e2a6bfb46fa1d7cda3e461
Taras Smakula <tarassmakula@gmail.com>
2024-09-09 16:48:28 +0300

    Bottom sheet bg color

--- app/src/main/java/org/futo/circles/feature/timeline/post/emoji/EmojiBottomSheet.kt
+++ app/src/main/java/org/futo/circles/feature/timeline/post/emoji/EmojiBottomSheet.kt
@@ -53,7 +53,7 @@ class EmojiBottomSheet : TransparentBackgroundBottomSheetDialogFragment() {
                     EmojiTheming(
                         backgroundColor = ContextCompat.getColor(
                             requireContext(),
-                            org.futo.circles.core.R.color.post_card_background_color
+                            org.futo.circles.core.R.color.grey_cool_200
                         ),
                         primaryColor = ContextCompat.getColor(
                             requireContext(),
